e93457a41f19a993e2efa2a19a83c0fd651b690b
snowleo <snowleo@e251c2fe-e539-e718-e476-b85c1f46cddb>
2011-03-21 15:39:57 +0000

    [trunk] Fix for ticket #161. Reset the color to white after away.
    git-svn-id: https://svn.java.net/svn/essentials~svn/trunk@995 e251c2fe-e539-e718-e476-b85c1f46cddb

--- Essentials/src/com/earth2me/essentials/commands/Commandwhois.java
+++ Essentials/src/com/earth2me/essentials/commands/Commandwhois.java
@@ -36,7 +36,7 @@ public class Commandwhois extends EssentialsCommand
 			sender.sendMessage(ChatColor.BLUE + " - Health: " + u.getHealth() + "/20");
 			sender.sendMessage(ChatColor.BLUE + " - Location: (" + u.getLocation().getBlockX() + ", " + u.getLocation().getBlockY() + ", " + u.getLocation().getBlockZ() + ")");
 			if (!parent.getConfiguration().getBoolean("disable-eco", false)) sender.sendMessage(ChatColor.BLUE + " - Money: $" + u.getMoney());
-			sender.sendMessage(ChatColor.BLUE + " - Status: " + (parent.away.contains(u) ? "§cAway" : "Available"));
+			sender.sendMessage(ChatColor.BLUE + " - Status: " + (parent.away.contains(u) ? "§cAway§f" : "Available"));
 			sender.sendMessage(ChatColor.BLUE + " - IP Address: " + u.getAddress().getAddress().toString());
 
 		}
