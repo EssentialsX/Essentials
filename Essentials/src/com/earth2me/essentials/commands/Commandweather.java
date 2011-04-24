1c06e813b069da0abfbe05bed873d76d0f94ec49
zenexer <zenexer@e251c2fe-e539-e718-e476-b85c1f46cddb>
2011-04-24 22:13:14 +0000

    Code cleanup; deprecated /nuble
    git-svn-id: https://svn.java.net/svn/essentials~svn/trunk@1271 e251c2fe-e539-e718-e476-b85c1f46cddb

--- Essentials/src/com/earth2me/essentials/commands/Commandweather.java
+++ Essentials/src/com/earth2me/essentials/commands/Commandweather.java
@@ -5,9 +5,10 @@ import com.earth2me.essentials.User;
 
 import org.bukkit.Server;
 
+
 public class Commandweather extends EssentialsCommand
 {
-        public Commandweather()
+	public Commandweather()
 	{
 		super("weather");
 	}
@@ -15,56 +16,41 @@ public class Commandweather extends EssentialsCommand
 	@Override
 	public void run(Server server, Essentials parent, User user, String commandLabel, String[] args) throws Exception
 	{
-                switch (args.length)
-                {
-                case 0:
-                        if (user.isAuthorized("essentials.weather"))
-                        {
-                             user.sendMessage("§cUsage: /" + commandLabel + " <storm/sun> [duration]");
-                             break;
-                        }
-                        user.sendMessage("§cYou are not allowed to change the weather");
-                        break;
-                case 1:
-                        if (user.isAuthorized("essentials.weather"))
-                        {
-                            if(args[0].equalsIgnoreCase("storm"))
-                            {
-                                user.getWorld().setStorm(true);
-                                user.sendMessage("§7You set the weather in your world to storm");
-                                break;
-                            }
-                            if(args[0].equalsIgnoreCase("sun"))
-                            {
-                                user.getWorld().setStorm(false);
-                                user.sendMessage("§7You set the weather in your world to sun");
-                                break;
-                            }
-                            user.sendMessage("§cUsage: /" + commandLabel + " <storm/sun> [duration]");
-                        }
-                        user.sendMessage("§cYou are not allowed to change the weather");
-                        break;
-                case 2:
-                        if (user.isAuthorized("essentials.weather"))
-                        {
-                            if(args[0].equalsIgnoreCase("storm"))
-                            {
-                                user.getWorld().setStorm(true);
-                                user.getWorld().setWeatherDuration(Integer.parseInt(args[1]) * 20);
-                                user.sendMessage("§7You set the weather in your world to storm for " +  args[1] + " seconds");
-                                break;
-                            }
-                            if(args[0].equalsIgnoreCase("sun"))
-                            {
-                                user.getWorld().setStorm(false);
-                                user.getWorld().setWeatherDuration(Integer.parseInt(args[1]) * 20);
-                                user.sendMessage("§7You set the weather in your world to sun for " +  args[1] + " seconds");
-                                break;
-                            }
-                            user.sendMessage("§cUsage: /" + commandLabel + " <storm/sun> [duration]");
-                        }
-                        user.sendMessage("§cYou are not allowed to change the weather");
-                        break;
-                }
-        }
+		switch (args.length)
+		{
+		case 0:
+			user.sendMessage("§cUsage: /" + commandLabel + " <storm/sun> [duration]");
+			break;
+		case 1:
+			if (args[0].equalsIgnoreCase("storm"))
+			{
+				user.getWorld().setStorm(true);
+				user.sendMessage("§7You set the weather in your world to storm");
+				break;
+			}
+			if (args[0].equalsIgnoreCase("sun"))
+			{
+				user.getWorld().setStorm(false);
+				user.sendMessage("§7You set the weather in your world to sun");
+				break;
+			}
+			user.sendMessage("§cUsage: /" + commandLabel + " <storm/sun> [duration]");
+		case 2:
+			if (args[0].equalsIgnoreCase("storm"))
+			{
+				user.getWorld().setStorm(true);
+				user.getWorld().setWeatherDuration(Integer.parseInt(args[1]) * 20);
+				user.sendMessage("§7You set the weather in your world to storm for " + args[1] + " seconds");
+				break;
+			}
+			if (args[0].equalsIgnoreCase("sun"))
+			{
+				user.getWorld().setStorm(false);
+				user.getWorld().setWeatherDuration(Integer.parseInt(args[1]) * 20);
+				user.sendMessage("§7You set the weather in your world to sun for " + args[1] + " seconds");
+				break;
+			}
+			user.sendMessage("§cUsage: /" + commandLabel + " <storm/sun> [duration]");
+		}
+	}
 }
