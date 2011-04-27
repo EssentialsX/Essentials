29869fa2644c20161b5be180b9a58d9ae739f943
ementalo <ementalo@e251c2fe-e539-e718-e476-b85c1f46cddb>
2011-04-27 00:16:23 +0000

    [trunk] lightning strikes where you look if you do not pass an arg. fixed health taking
    git-svn-id: https://svn.java.net/svn/essentials~svn/trunk@1282 e251c2fe-e539-e718-e476-b85c1f46cddb

--- Essentials/src/com/earth2me/essentials/commands/Commandlightning.java
+++ Essentials/src/com/earth2me/essentials/commands/Commandlightning.java
@@ -1,9 +1,8 @@
 package com.earth2me.essentials.commands;
 
 import com.earth2me.essentials.Essentials;
+import com.earth2me.essentials.TargetBlock;
 import com.earth2me.essentials.User;
-import java.util.List;
-
 import org.bukkit.Server;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
@@ -20,13 +19,14 @@ public class Commandlightning extends EssentialsCommand
 	public void run(Server server, Essentials parent, User user, String commandLabel, String[] args) throws Exception
 	{
 
+		World world = user.getWorld();
 		if (args.length < 1)
 		{
-			user.sendMessage("§cUsage: /" + commandLabel + " [player]");
+			world.strikeLightning(user.getTargetBlock(null, 600).getLocation());
+			user.sendMessage("§7CRACK");
 			return;
 		}
 
-		World world = user.getWorld();
 		if(server.matchPlayer(args[0]).isEmpty())
 		{
 			user.sendMessage("§cPlayer not found");
@@ -35,9 +35,9 @@ public class Commandlightning extends EssentialsCommand
 
 		for (Player p : server.matchPlayer(args[0]))
 		{
-			user.sendMessage("§7Smiting" + p.getDisplayName());
+			user.sendMessage("§7Smiting " + p.getDisplayName());
 			world.strikeLightning(p.getLocation());
-			p.setHealth(p.getHealth() - 5);
+			p.setHealth(p.getHealth() < 5 ? 0 : p.getHealth() -5);
 			p.sendMessage("§7You have just been smited");
 		}
 		user.charge(this);
