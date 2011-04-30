ec18c0096d91f237a27e9aa88a96857403b2ed40
snowleo <snowleo@e251c2fe-e539-e718-e476-b85c1f46cddb>
2011-04-30 18:12:46 +0000

    [trunk/GroupManager] Correct error message on failing to load users file.
    git-svn-id: https://svn.java.net/svn/essentials~svn/trunk@1291 e251c2fe-e539-e718-e476-b85c1f46cddb

--- EssentialsGroupManager/src/org/anjocaido/groupmanager/dataholder/WorldDataHolder.java
+++ EssentialsGroupManager/src/org/anjocaido/groupmanager/dataholder/WorldDataHolder.java
@@ -558,7 +558,7 @@ public class WorldDataHolder {
         Yaml yamlUsers = new Yaml(new SafeConstructor());
         Map<String, Object> usersRootDataNode;
         if (!groupsFile.exists()) {
-            throw new IllegalArgumentException("The file which should contain permissions does not exist!\n" + groupsFile.getPath());
+            throw new IllegalArgumentException("The file which should contain permissions does not exist!\n" + usersFile.getPath());
         }
         FileInputStream usersInputStream = new FileInputStream(usersFile);
         try {
@@ -567,7 +567,7 @@ public class WorldDataHolder {
                 throw new NullPointerException();
             }
         } catch (Exception ex) {
-            throw new IllegalArgumentException("The following file couldn't pass on Parser.\n" + groupsFile.getPath(), ex);
+            throw new IllegalArgumentException("The following file couldn't pass on Parser.\n" + usersFile.getPath(), ex);
         } finally {
             usersInputStream.close();
         }
