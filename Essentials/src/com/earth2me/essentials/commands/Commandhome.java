86dce6e092fb84575f755836f1d3fde3d5f104da
snowleo <snowleo@e251c2fe-e539-e718-e476-b85c1f46cddb>
2011-05-15 02:39:59 +0000

    /home: Allows to go to offline players home.
    git-svn-id: https://svn.java.net/svn/essentials~svn/trunk@1473 e251c2fe-e539-e718-e476-b85c1f46cddb

--- Essentials/src/com/earth2me/essentials/commands/Commandhome.java
+++ Essentials/src/com/earth2me/essentials/commands/Commandhome.java
@@ -2,6 +2,7 @@ package com.earth2me.essentials.commands;
 
 import org.bukkit.Server;
 import com.earth2me.essentials.User;
+import com.earth2me.essentials.Util;
 
 
 public class Commandhome extends EssentialsCommand
@@ -17,7 +18,19 @@ public class Commandhome extends EssentialsCommand
 		user.canAfford(this);
 		if(args.length > 0 && user.isAuthorized("essentials.home.others"))
 		{
-			User u = getPlayer(server, args, 0);
+			User u;
+			try
+			{
+				u = getPlayer(server, args, 0);
+			}
+			catch(NoSuchFieldException ex)
+			{
+				u = ess.getOfflineUser(args[0]);
+			}
+			if (u == null)
+			{
+				throw new Exception(Util.i18n("playerNotFound"));
+			}
 			user.getTeleport().home(u, this.getName());
 			return;
 		}
