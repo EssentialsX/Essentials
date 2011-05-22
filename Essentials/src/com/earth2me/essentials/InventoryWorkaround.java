90fe8172671cec59a0b3361afee06bd989849e46
snowleo <snowleo@e251c2fe-e539-e718-e476-b85c1f46cddb>
2011-05-22 17:32:24 +0000

    Cleanup
    git-svn-id: https://svn.java.net/svn/essentials~svn/trunk@1510 e251c2fe-e539-e718-e476-b85c1f46cddb

--- Essentials/src/com/earth2me/essentials/InventoryWorkaround.java
+++ Essentials/src/com/earth2me/essentials/InventoryWorkaround.java
@@ -2,7 +2,6 @@ package com.earth2me.essentials;
 
 import java.util.HashMap;
 import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.entity.Item;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
