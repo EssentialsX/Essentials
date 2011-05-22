7ed87d696ac257b9a09b32658f78dd1265ab0105
snowleo <snowleo@e251c2fe-e539-e718-e476-b85c1f46cddb>
2011-05-22 20:05:47 +0000

    cleanup
    git-svn-id: https://svn.java.net/svn/essentials~svn/trunk@1519 e251c2fe-e539-e718-e476-b85c1f46cddb

--- EssentialsProtect/src/com/earth2me/essentials/protect/EssentialsProtectWeatherListener.java
+++ EssentialsProtect/src/com/earth2me/essentials/protect/EssentialsProtectWeatherListener.java
@@ -1,18 +1,9 @@
 package com.earth2me.essentials.protect;
 
-import com.earth2me.essentials.Essentials;
-import com.earth2me.essentials.User;
-import org.bukkit.ChatColor;
-import org.bukkit.Material;
-import org.bukkit.block.Block;
-import org.bukkit.block.BlockFace;
-import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.weather.LightningStrikeEvent;
 import org.bukkit.event.weather.ThunderChangeEvent;
 import org.bukkit.event.weather.WeatherChangeEvent;
-import org.bukkit.event.weather.WeatherEvent;
 import org.bukkit.event.weather.WeatherListener;
-import org.bukkit.inventory.ItemStack;
 
 
 public class EssentialsProtectWeatherListener extends WeatherListener
