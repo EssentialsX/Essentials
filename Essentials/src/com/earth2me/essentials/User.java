1b0b0b1b571b6e5cf015dbad819da76ec291ab3a
ementalo <suror@gmx.co.uk>
2011-07-14 23:27:04 +0100

    fix messages when /eco'ing others

--- Essentials/src/com/earth2me/essentials/User.java
+++ Essentials/src/com/earth2me/essentials/User.java
@@ -90,7 +90,7 @@ public class User extends UserData implements Comparable<User>, IReplyTo, IUser
 		sendMessage(Util.format("addedToAccount", Util.formatCurrency(value)));
 		if (initiator != null)
 		{
-			initiator.sendMessage((Util.format("addedToAccount", Util.formatCurrency(value))));
+			initiator.sendMessage((Util.format("addedToOthersAccount", Util.formatCurrency(value), this.getDisplayName())));
 		}
 	}
 	
@@ -127,7 +127,7 @@ public class User extends UserData implements Comparable<User>, IReplyTo, IUser
 		sendMessage(Util.format("takenFromAccount", Util.formatCurrency(value)));
 		if (initiator != null)
 		{
-			initiator.sendMessage((Util.format("takenFromAccount", Util.formatCurrency(value))));
+			initiator.sendMessage((Util.format("takenFromOthersAccount", Util.formatCurrency(value), this.getDisplayName())));
 		}
 	}
 	
