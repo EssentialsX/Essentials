78c376af79f1550941166bcfa43a8d3625111693
snowleo <schneeleo@gmail.com>
2011-07-16 02:38:22 +0200

    Created an interface for settings.

--- Essentials/src/com/earth2me/essentials/Essentials.java
+++ Essentials/src/com/earth2me/essentials/Essentials.java
@@ -48,7 +48,7 @@ public class Essentials extends JavaPlugin implements IEssentials
 {
 	public static final int BUKKIT_VERSION = 974;
 	private static final Logger LOGGER = Logger.getLogger("Minecraft");
-	private transient Settings settings;
+	private transient ISettings settings;
 	private final transient TNTExplodeListener tntListener = new TNTExplodeListener(this);
 	private transient Spawn spawn;
 	private transient Jail jail;
@@ -65,7 +65,7 @@ public class Essentials extends JavaPlugin implements IEssentials
 	private transient final EssentialsErrorHandler errorHandler = new EssentialsErrorHandler();
 	private transient IPermissionsHandler permissionsHandler;
 
-	public Settings getSettings()
+	public ISettings getSettings()
 	{
 		return settings;
 	}
