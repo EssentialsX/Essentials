48230f615d0d3a6f3d98d03960ccf71c337f914a
snowleo <schneeleo@gmail.com>
2011-07-16 17:04:25 +0200

    Fixes to EssentialsProtect that will remove conflicts with WorldGuard and other protection plugins.

--- EssentialsProtect/src/com/earth2me/essentials/protect/EssentialsProtectPlayerListener.java
+++ EssentialsProtect/src/com/earth2me/essentials/protect/EssentialsProtectPlayerListener.java
@@ -24,6 +24,10 @@ public class EssentialsProtectPlayerListener extends PlayerListener
 	@Override
 	public void onPlayerInteract(final PlayerInteractEvent event)
 	{
+		if (event.isCancelled())
+		{
+			return;
+		}
 		final User user = ess.getUser(event.getPlayer());
 
 		if (prot.getSettingBool(ProtectConfig.disable_build) && !user.canBuild())
