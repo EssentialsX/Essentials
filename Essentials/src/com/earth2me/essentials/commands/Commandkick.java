0ab8a2760db34de0b2d184bb7a7edb5f8cf9c23e
KHobbits <rob@khobbits.co.uk>
2011-07-26 14:23:14 +0100

    Adding tempban/kick broadcast permissions and adding a banned by to the broadcast. Adding tempban/kick broadcast permissions and adding a banned by to the broadcast. Merge branch 'master' of github.com:khobbits/Essentials
    Conflicts:
    	Essentials/src/messages.properties
    	Essentials/src/messages_da.properties
    	Essentials/src/messages_en.properties
    	Essentials/src/messages_fr.properties
    	Essentials/src/messages_nl.properties
    Merge branch 'master' of github.com:khobbits/Essentials

--- Essentials/src/com/earth2me/essentials/commands/Commandkick.java
+++ Essentials/src/com/earth2me/essentials/commands/Commandkick.java
@@ -1,9 +1,11 @@
 package com.earth2me.essentials.commands;
 
+import com.earth2me.essentials.Console;
 import org.bukkit.Server;
 import org.bukkit.command.CommandSender;
 import com.earth2me.essentials.User;
 import com.earth2me.essentials.Util;
+import org.bukkit.entity.Player;
 
 
 public class Commandkick extends EssentialsCommand
@@ -21,15 +23,24 @@ public class Commandkick extends EssentialsCommand
 			throw new NotEnoughArgumentsException();
 		}
 		
-		User u = getPlayer(server, args, 0);
-		if (u.isAuthorized("essentials.kick.exempt"))
+		User player = getPlayer(server, args, 0);
+		if (player.isAuthorized("essentials.kick.exempt"))
 		{
 			sender.sendMessage(Util.i18n("kickExempt"));
 			return;
 		}
 		charge(sender);
 		final String kickReason = args.length > 1 ? getFinalArg(args, 1) : Util.i18n("kickDefault");
-		u.kickPlayer(kickReason);
-		server.broadcastMessage(Util.format("playerKicked", u.getName(), kickReason));
+		player.kickPlayer(kickReason);
+		String senderName = sender instanceof Player ? ((Player)sender).getDisplayName() : Console.NAME;
+		
+		for(Player p : server.getOnlinePlayers())
+		{
+			User u = ess.getUser(p);
+			if(u.isAuthorized("essentials.kick.notify"))
+			{
+			p.sendMessage(Util.format("playerKicked", senderName, player.getName(), kickReason));
+			}
+		}
 	}
 }
