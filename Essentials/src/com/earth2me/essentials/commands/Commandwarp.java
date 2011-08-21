d818f705adc30b5f515e49f88e23e870322f3eb3
snowleo <schneeleo@gmail.com>
2011-08-21 21:30:01 +0200

    Cleanup

--- Essentials/src/com/earth2me/essentials/commands/Commandwarp.java
+++ Essentials/src/com/earth2me/essentials/commands/Commandwarp.java
@@ -7,7 +7,6 @@ import com.earth2me.essentials.Util;
 import com.earth2me.essentials.Warps;
 import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 
 
