c0d21b6044724de6f22f8924fa129a640e505faf
okamosy <okamosy@gmail.com>
2011-08-21 20:06:25 +0100

    Removed commented-out code

--- Essentials/src/com/earth2me/essentials/commands/Commandpowertool.java
+++ Essentials/src/com/earth2me/essentials/commands/Commandpowertool.java
@@ -96,46 +96,4 @@ public class Commandpowertool extends EssentialsCommand
 		charge(user);
 		user.setPowertool(is, powertools);
 	}
-
-	private String appendPowerTool(User user, String command, ItemStack is, String itemName) throws Exception
-	{
-		command = command.substring(2); // Ignore the first 2 chars
-		/*String powertools = user.getPowertool(is);
-		if (powertools != null)
-		{
-			if (powertools.contains(command))
-			{
-				throw new Exception((Util.format("powerToolAlreadySet", command, itemName)));
-			}
-
-			StringBuilder newCommand = new StringBuilder();
-			command = newCommand.append(powertools).append("|").append(command).toString();
-		}
-*/
-		return command;
-	}
-
-	private String removePowerTool(User user, String command, ItemStack is, String itemName) throws Exception
-	{
-		/*
-		String powertools = user.getPowertool(is);
-		if (!powertools.contains(command))
-		{
-			throw new Exception((Util.format("powerToolNoSuchCommandAssigned", command, itemName)));
-		}
-
-		command = powertools.replace(command, "").replace("||", "|");
-
-		// Trim off any leading/trailing '|' chars
-		if (command.startsWith("|"))
-		{
-			command = command.substring(1);
-		}
-		if (command.endsWith("|"))
-		{
-			command = command.substring(0, command.length() - 1);
-		}
-*/
-		return command;
-	}
 }
