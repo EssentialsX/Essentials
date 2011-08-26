016dd10060ca19bcf5bfd8d857b12bc34847cbfc
KHobbits <rob@khobbits.co.uk>
2011-08-26 05:12:29 +0100

    Adding essentials.delhome.others to allow people to delete other peoples homes.

--- Essentials/src/com/earth2me/essentials/commands/Commanddelhome.java
+++ Essentials/src/com/earth2me/essentials/commands/Commanddelhome.java
@@ -16,25 +16,31 @@ public class Commanddelhome extends EssentialsCommand
 	@Override
 	public void run(Server server, CommandSender sender, String commandLabel, String[] args) throws Exception
 	{
-		User user;
+		//Allowing both formats /delhome khobbits house | /delhome khobbits:house
+		final String[] nameParts = args[0].split(":");
+		if (nameParts[0].length() != args[0].length())
+		{
+			args = nameParts;
+		}
+
+		User user = ess.getUser(sender);
 		String name;
 		if (args.length < 1)
 		{
 			throw new NotEnoughArgumentsException();
 		}
-		else if (args.length < 2)
+		else if (args.length > 1 && (user == null || user.isAuthorized("essentials.delhome.others")))
 		{
-			user = ess.getUser(sender);
+			user = getPlayer(server, args, 0);
+			name = args[1];
+		}
+		else
+		{
 			if (user == null)
 			{
 				throw new NotEnoughArgumentsException();
 			}
 			name = args[0];
-		}
-		else
-		{
-			user = getPlayer(server, args, 0);
-			name = args[1];
 		}
 		user.delHome(name.toLowerCase());
 		sender.sendMessage(Util.format("deleteHome", name));
