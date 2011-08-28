59e45592cffc4b25e18ddf38fa49e62e2321f419
snowleo <schneeleo@gmail.com>
2011-08-28 06:16:01 +0200

    Allow the build server to build again.

--- Essentials/src/com/earth2me/essentials/yaml/Settings.java
+++ Essentials/src/com/earth2me/essentials/yaml/Settings.java
@@ -3,11 +3,12 @@ package com.earth2me.essentials.yaml;
 import me.snowleo.yaml.YamlClass;
 import me.snowleo.yaml.YamlComment;
 
-@YamlClass
-public class Settings extends SettingsYaml
+//@YamlClass
+public class Settings //extends SettingsYaml
 {
-	@YamlComment(comment = "Hello")
+	//@YamlComment(comment = "Hello")
 	protected General test = new General();
+	protected boolean test3;
 	boolean test2 = true;
 
 	public Settings() {
