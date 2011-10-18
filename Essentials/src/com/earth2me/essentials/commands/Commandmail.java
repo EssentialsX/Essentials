a86a1990cde79f156e45c24f91c7924eff8b7a4d
Tim P <tustin2121@gmail.com>
2011-10-18 12:12:41 -0400

    Made a couple commands available for the console (ones that make sense to be able to access from the command line).

--- Essentials/src/com/earth2me/essentials/commands/Commandmail.java
+++ Essentials/src/com/earth2me/essentials/commands/Commandmail.java
@@ -5,6 +5,7 @@ import org.bukkit.Server;
 import com.earth2me.essentials.User;
 import com.earth2me.essentials.Util;
 import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 
@@ -66,6 +67,38 @@ public class Commandmail extends EssentialsCommand
 			user.setMails(null);
 			user.sendMessage(Util.i18n("mailCleared"));
 			return;
+		}
+		throw new NotEnoughArgumentsException();
+	}
+	
+	@Override protected void run(Server server, CommandSender sender, String commandLabel, String[] args) throws Exception {
+		if (args.length >= 1 && "read".equalsIgnoreCase(args[0]))
+		{
+			throw new Exception(Util.format("onlyPlayers", commandLabel+" read"));
+		}
+		if (args.length >= 3 && "send".equalsIgnoreCase(args[0]))
+		{
+			Player player = server.getPlayer(args[1]);
+			User u;
+			if (player != null)
+			{
+				u = ess.getUser(player);
+			}
+			else
+			{
+				u = ess.getOfflineUser(args[1]);
+			}
+			if (u == null)
+			{
+				throw new Exception(Util.format("playerNeverOnServer", args[1]));
+			}
+			u.addMail("Server: " + getFinalArg(args, 2));
+			sender.sendMessage(Util.i18n("mailSent"));
+			return;
+		}
+		if (args.length >= 1 && "clear".equalsIgnoreCase(args[0]))
+		{
+			throw new Exception(Util.format("onlyPlayers", commandLabel+" clear"));	
 		}
 		throw new NotEnoughArgumentsException();
 	}
