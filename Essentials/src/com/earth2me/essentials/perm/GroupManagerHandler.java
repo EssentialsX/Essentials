809d18575d1f87f48b75478d023ebb85fc79b921
KHobbits <rob@khobbits.co.uk>
2011-10-29 02:09:11 +0100

    File formatting.

--- Essentials/src/com/earth2me/essentials/perm/GroupManagerHandler.java
+++ Essentials/src/com/earth2me/essentials/perm/GroupManagerHandler.java
@@ -15,55 +15,55 @@ public class GroupManagerHandler implements IPermissionsHandler
 
 	public GroupManagerHandler(final Plugin permissionsPlugin)
 	{
-		groupManager = ((GroupManager)permissionsPlugin);	
+		groupManager = ((GroupManager)permissionsPlugin);
 	}
 
 	@Override
 	public String getGroup(final Player base)
 	{
-		final AnjoPermissionsHandler handler = groupManager.getWorldsHolder().getWorldPermissions(base);	
-		return handler.getGroup(base.getName());				
+		final AnjoPermissionsHandler handler = groupManager.getWorldsHolder().getWorldPermissions(base);
+		return handler.getGroup(base.getName());
 	}
 
 	@Override
 	public List<String> getGroups(final Player base)
 	{
 		final AnjoPermissionsHandler handler = groupManager.getWorldsHolder().getWorldPermissions(base);
-		return Arrays.asList(handler.getGroups(base.getName()));		
+		return Arrays.asList(handler.getGroups(base.getName()));
 	}
 
 	@Override
 	public boolean canBuild(final Player base, final String group)
 	{
-		final AnjoPermissionsHandler handler = groupManager.getWorldsHolder().getWorldPermissions(base);	
-		return handler.canUserBuild(base.getName());	
+		final AnjoPermissionsHandler handler = groupManager.getWorldsHolder().getWorldPermissions(base);
+		return handler.canUserBuild(base.getName());
 	}
 
 	@Override
 	public boolean inGroup(final Player base, final String group)
 	{
-		AnjoPermissionsHandler handler = groupManager.getWorldsHolder().getWorldPermissions(base);	
-		return handler.inGroup(base.getName(), group);		
+		AnjoPermissionsHandler handler = groupManager.getWorldsHolder().getWorldPermissions(base);
+		return handler.inGroup(base.getName(), group);
 	}
 
 	@Override
 	public boolean hasPermission(final Player base, final String node)
 	{
-		AnjoPermissionsHandler handler = groupManager.getWorldsHolder().getWorldPermissions(base);	
+		AnjoPermissionsHandler handler = groupManager.getWorldsHolder().getWorldPermissions(base);
 		return handler.has(base, node);
 	}
 
 	@Override
 	public String getPrefix(final Player base)
 	{
-		AnjoPermissionsHandler handler = groupManager.getWorldsHolder().getWorldPermissions(base);	
+		AnjoPermissionsHandler handler = groupManager.getWorldsHolder().getWorldPermissions(base);
 		return handler.getUserPrefix(base.getName());
 	}
 
 	@Override
 	public String getSuffix(final Player base)
 	{
-		AnjoPermissionsHandler handler = groupManager.getWorldsHolder().getWorldPermissions(base);	
+		AnjoPermissionsHandler handler = groupManager.getWorldsHolder().getWorldPermissions(base);
 		return handler.getUserSuffix(base.getName());
 	}
 }
