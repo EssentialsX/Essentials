8591023ad5e50e132dc0dc9e473d74303630783c
snowleo <schneeleo@gmail.com>
2011-12-02 03:25:22 +0100

    Moved block to item conversion to Util class

--- Essentials/src/com/earth2me/essentials/EssentialsBlockListener.java
+++ Essentials/src/com/earth2me/essentials/EssentialsBlockListener.java
@@ -28,71 +28,7 @@ public class EssentialsBlockListener extends BlockListener
 		final User user = ess.getUser(event.getPlayer());
 		// Do not rely on getItemInHand();
 		// http://leaky.bukkit.org/issues/663
-		final ItemStack is = new ItemStack(event.getBlockPlaced().getType(), 1, (short)0, event.getBlockPlaced().getData());
-		switch (is.getType())
-		{
-		case WOODEN_DOOR:
-			is.setType(Material.WOOD_DOOR);
-			is.setDurability((short)0);
-			break;
-		case IRON_DOOR_BLOCK:
-			is.setType(Material.IRON_DOOR);
-			is.setDurability((short)0);
-			break;
-		case SIGN_POST:
-		case WALL_SIGN:
-			is.setType(Material.SIGN);
-			is.setDurability((short)0);
-			break;
-		case CROPS:
-			is.setType(Material.SEEDS);
-			is.setDurability((short)0);
-			break;
-		case CAKE_BLOCK:
-			is.setType(Material.CAKE);
-			is.setDurability((short)0);
-			break;
-		case BED_BLOCK:
-			is.setType(Material.BED);
-			is.setDurability((short)0);
-			break;
-		case REDSTONE_WIRE:
-			is.setType(Material.REDSTONE);
-			is.setDurability((short)0);
-			break;
-		case REDSTONE_TORCH_OFF:
-		case REDSTONE_TORCH_ON:
-			is.setType(Material.REDSTONE_TORCH_ON);
-			is.setDurability((short)0);
-			break;
-		case DIODE_BLOCK_OFF:
-		case DIODE_BLOCK_ON:
-			is.setType(Material.DIODE);
-			is.setDurability((short)0);
-			break;
-		case DOUBLE_STEP:
-			is.setType(Material.STEP);
-			break;
-		case TORCH:
-		case RAILS:
-		case LADDER:
-		case WOOD_STAIRS:
-		case COBBLESTONE_STAIRS:
-		case LEVER:
-		case STONE_BUTTON:
-		case FURNACE:
-		case DISPENSER:
-		case PUMPKIN:
-		case JACK_O_LANTERN:
-		case WOOD_PLATE:
-		case STONE_PLATE:
-		case PISTON_STICKY_BASE:
-		case PISTON_BASE:
-			is.setDurability((short)0);
-			break;
-		case FIRE:
-			return;
-		}
+		final ItemStack is = Util.convertBlockToItem(event.getBlockPlaced());
 		boolean unlimitedForUser = user.hasUnlimited(is);
 		if (unlimitedForUser && user.getGameMode() == GameMode.SURVIVAL)
 		{
