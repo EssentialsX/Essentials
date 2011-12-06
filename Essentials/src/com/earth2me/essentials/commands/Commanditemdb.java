d57367193307c07f4e6924419b7ca16369f4dbf9
Khyperia <khyperia@live.com>
2011-12-07 05:58:44 +0800

    Renamed /find to /itemdb

--- Essentials/src/com/earth2me/essentials/commands/Commanditemdb.java
+++ Essentials/src/com/earth2me/essentials/commands/Commanditemdb.java
@@ -2,12 +2,13 @@ package com.earth2me.essentials.commands;
 
 import org.bukkit.Server;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 
-public class Commandfind extends EssentialsCommand
+public class Commanditemdb extends EssentialsCommand
 {
-	public Commandfind()
+	public Commanditemdb()
 	{
 		super("find");
 	}
@@ -15,11 +16,22 @@ public class Commandfind extends EssentialsCommand
 	@Override
 	protected void run(Server server, CommandSender sender, String commandLabel, String[] args) throws Exception
 	{
+		ItemStack itemStack = null;
 		if (args.length < 1)
 		{
-			throw new NotEnoughArgumentsException();
+			if (sender instanceof Player)
+			{
+				itemStack = ((Player)sender).getItemInHand();
+			}
+			if (itemStack == null)
+			{
+				throw new NotEnoughArgumentsException();
+			}
 		}
-		ItemStack itemStack = ess.getItemDb().get(args[0]);
+		else
+		{
+			itemStack = ess.getItemDb().get(args[0]);
+		}
 		sender.sendMessage(itemStack.getType().toString() + "- " + itemStack.getTypeId() + ":" + Integer.toString(itemStack.getData().getData()));
 	}
 }
