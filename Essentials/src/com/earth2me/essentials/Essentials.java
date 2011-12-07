5002e7a6ad13295ed85d5758da5a739ade116a72
snowleo <schneeleo@gmail.com>
2011-12-08 00:48:53 +0100

    Prevent blockdamage by enderdragon (default true) in EssentialsProtect CB#1566 B#1058

--- Essentials/src/com/earth2me/essentials/Essentials.java
+++ Essentials/src/com/earth2me/essentials/Essentials.java
@@ -61,7 +61,7 @@ import org.yaml.snakeyaml.error.YAMLException;
 
 public class Essentials extends JavaPlugin implements IEssentials
 {
-	public static final int BUKKIT_VERSION = 1560;
+	public static final int BUKKIT_VERSION = 1566;
 	private static final Logger LOGGER = Logger.getLogger("Minecraft");
 	private transient ISettings settings;
 	private final transient TNTExplodeListener tntListener = new TNTExplodeListener(this);
