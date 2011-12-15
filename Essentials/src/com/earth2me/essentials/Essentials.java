64bc8d1133fa28e9895fd4186f4bec761e6a2844
snowleo <schneeleo@gmail.com>
2011-12-15 10:32:04 +0100

    Corrected version match for 1597 build version

--- Essentials/src/com/earth2me/essentials/Essentials.java
+++ Essentials/src/com/earth2me/essentials/Essentials.java
@@ -124,7 +124,7 @@ public class Essentials extends JavaPlugin implements IEssentials
 				LOGGER.log(Level.WARNING, _("versionMismatch", plugin.getDescription().getName()));
 			}
 		}
-		final Matcher versionMatch = Pattern.compile("git-Bukkit-([0-9]+).([0-9]+).([0-9]+)-R[0-9]+-[0-9]+-[0-9a-z]+-b([0-9]+)jnks.*").matcher(getServer().getVersion());
+		final Matcher versionMatch = Pattern.compile("git-Bukkit-([0-9]+).([0-9]+).([0-9]+)-R[0-9]+-b([0-9]+)jnks.*").matcher(getServer().getVersion());
 		if (versionMatch.matches())
 		{
 			final int versionNumber = Integer.parseInt(versionMatch.group(4));
