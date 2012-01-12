390b17f0d8a4b07388619ad7a64e411d33c211cf
snowleo <schneeleo@gmail.com>
2012-01-12 23:43:11 +0100

    Min Bukkit version is 1713 now

--- Essentials/src/com/earth2me/essentials/Essentials.java
+++ Essentials/src/com/earth2me/essentials/Essentials.java
@@ -60,7 +60,7 @@ import org.yaml.snakeyaml.error.YAMLException;
 
 public class Essentials extends JavaPlugin implements IEssentials
 {
-	public static final int BUKKIT_VERSION = 1597;
+	public static final int BUKKIT_VERSION = 1713;
 	private static final Logger LOGGER = Logger.getLogger("Minecraft");
 	private transient ISettings settings;
 	private final transient TNTExplodeListener tntListener = new TNTExplodeListener(this);
