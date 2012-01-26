b8944d0b254130aeb50d582b1ab733c1a6d19160
KHobbits <rob@khobbits.co.uk>
2012-01-26 00:30:33 +0000

    Set min bukkit to 1818 - RB

--- Essentials/src/com/earth2me/essentials/Essentials.java
+++ Essentials/src/com/earth2me/essentials/Essentials.java
@@ -61,7 +61,7 @@ import org.yaml.snakeyaml.error.YAMLException;
 
 public class Essentials extends JavaPlugin implements IEssentials
 {
-	public static final int BUKKIT_VERSION = 1791;
+	public static final int BUKKIT_VERSION = 1818;
 	private static final Logger LOGGER = Logger.getLogger("Minecraft");
 	private transient ISettings settings;
 	private final transient TNTExplodeListener tntListener = new TNTExplodeListener(this);
