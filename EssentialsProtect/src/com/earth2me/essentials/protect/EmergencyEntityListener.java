9b58b2b9c0d29819db903801a51dac91a46627f1
KHobbits <rob@khobbits.co.uk>
2012-01-26 21:21:13 +0000

    Revert emergency listener to old style Stops peoples world world burning down if they throw in 2.8, on a 1.0 server.

---
+++ EssentialsProtect/src/com/earth2me/essentials/protect/EmergencyEntityListener.java
@@ -0,0 +1,22 @@
+package com.earth2me.essentials.protect;
+
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.event.entity.EntityListener;
+
+@Deprecated
+public class EmergencyEntityListener extends EntityListener
+{
+
+	@Override
+	public void onEntityExplode(final EntityExplodeEvent event)
+	{
+		event.setCancelled(true);
+	}
+
+	@Override
+	public void onEntityDamage(final EntityDamageEvent event)
+	{
+		event.setCancelled(true);
+	}
+}
