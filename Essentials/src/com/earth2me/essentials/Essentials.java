b0552019f53df8d76f051ceca26120ae1e524abf
KHobbits <rob@khobbits.co.uk>
2012-03-01 21:47:01 +0000

    Fix version matching.

--- Essentials/src/com/earth2me/essentials/Essentials.java
+++ Essentials/src/com/earth2me/essentials/Essentials.java
@@ -129,10 +129,10 @@ public class Essentials extends JavaPlugin implements IEssentials
 				LOGGER.log(Level.WARNING, _("versionMismatch", plugin.getDescription().getName()));
 			}
 		}
-		final Matcher versionMatch = Pattern.compile("git-Bukkit-([0-9]+).([0-9]+).([0-9]+)-R[0-9]+-(?:[0-9]+-g[0-9a-f]+-)?b([0-9]+)jnks.*").matcher(getServer().getVersion());
+		final Matcher versionMatch = Pattern.compile("git-Bukkit-(?:(?:[0-9]+)\\.)+[0-9]+-R[0-9]+-(?:[0-9]+-g[0-9a-f]+-)?b([0-9]+)jnks.*").matcher(getServer().getVersion());
 		if (versionMatch.matches())
 		{
-			final int versionNumber = Integer.parseInt(versionMatch.group(4));
+			final int versionNumber = Integer.parseInt(versionMatch.group(1));
 			if (versionNumber < BUKKIT_VERSION)
 			{
 				LOGGER.log(Level.SEVERE, _("notRecommendedBukkit"));
