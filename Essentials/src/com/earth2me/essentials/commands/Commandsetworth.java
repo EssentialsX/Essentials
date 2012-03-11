8dcd591beb12bd0bc03f2a3d6092ce1a7654495b
KHobbits <rob@khobbits.co.uk>
2012-03-11 23:59:59 +0000

    Update setworth to allow /setworth <price> ingame Allow /setworth to be used from console.

--- Essentials/src/com/earth2me/essentials/commands/Commandsetworth.java
+++ Essentials/src/com/earth2me/essentials/commands/Commandsetworth.java
@@ -3,6 +3,7 @@ package com.earth2me.essentials.commands;
 import static com.earth2me.essentials.I18n._;
 import com.earth2me.essentials.User;
 import org.bukkit.Server;
+import org.bukkit.command.CommandSender;
 import org.bukkit.inventory.ItemStack;
 
 
@@ -21,8 +22,34 @@ public class Commandsetworth extends EssentialsCommand
 			throw new NotEnoughArgumentsException();
 		}
 
+		ItemStack stack;
+		String price;
+
+		if (args.length == 1)
+		{
+			stack = user.getInventory().getItemInHand();
+			price = args[0];
+		}
+		else
+		{
+			stack = ess.getItemDb().get(args[0]);
+			price = args[1];
+		}
+
+		ess.getWorth().setPrice(stack, Double.parseDouble(price));
+		user.sendMessage(_("worthSet"));
+	}
+
+	@Override
+	public void run(final Server server, final CommandSender sender, final String commandLabel, final String[] args) throws Exception
+	{
+		if (args.length < 2)
+		{
+			throw new NotEnoughArgumentsException();
+		}
+
 		ItemStack stack = ess.getItemDb().get(args[0]);
 		ess.getWorth().setPrice(stack, Double.parseDouble(args[1]));
-		user.sendMessage(_("worthSet"));
+		sender.sendMessage(_("worthSet"));
 	}
 }
