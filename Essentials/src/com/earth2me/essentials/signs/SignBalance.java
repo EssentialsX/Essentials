00033c11bce3c1a960bf0d14c81b90ab8462f64a
KHobbits <rob@khobbits.co.uk>
2012-03-25 14:26:08 +0100

    Update [balance] sign to use currency formatting.

--- Essentials/src/com/earth2me/essentials/signs/SignBalance.java
+++ Essentials/src/com/earth2me/essentials/signs/SignBalance.java
@@ -3,6 +3,7 @@ package com.earth2me.essentials.signs;
 import static com.earth2me.essentials.I18n._;
 import com.earth2me.essentials.IEssentials;
 import com.earth2me.essentials.User;
+import com.earth2me.essentials.Util;
 
 
 public class SignBalance extends EssentialsSign
@@ -15,7 +16,7 @@ public class SignBalance extends EssentialsSign
 	@Override
 	protected boolean onSignInteract(final ISign sign, final User player, final String username, final IEssentials ess) throws SignException
 	{
-		player.sendMessage(_("balance", player.getMoney()));
+		player.sendMessage(_("balance", Util.displayCurrency(player.getMoney(), ess)));
 		return true;
 	}
 }
