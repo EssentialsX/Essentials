bf1e073ef4d5126b1122ad2bca05ffbc7af42359
KHobbits <rob@khobbits.co.uk>
2012-04-02 02:47:10 +0100

    Allow tppos to be used in console.

--- Essentials/src/com/earth2me/essentials/commands/Commandtppos.java
+++ Essentials/src/com/earth2me/essentials/commands/Commandtppos.java
@@ -5,6 +5,7 @@ import com.earth2me.essentials.Trade;
 import com.earth2me.essentials.User;
 import org.bukkit.Location;
 import org.bukkit.Server;
+import org.bukkit.command.CommandSender;
 import org.bukkit.event.player.PlayerTeleportEvent.TeleportCause;
 
 
@@ -40,5 +41,31 @@ public class Commandtppos extends EssentialsCommand
 		user.sendMessage(_("teleporting"));
 		user.getTeleport().teleport(location, charge, TeleportCause.COMMAND);
 		throw new NoChargeException();
+	}
+
+	@Override
+	public void run(final Server server, final CommandSender sender, final String commandLabel, final String[] args) throws Exception
+	{
+		if (args.length < 4)
+		{
+			throw new NotEnoughArgumentsException();
+		}
+
+		User user = ess.getUser(server.getPlayer(args[0]));
+		final int x = Integer.parseInt(args[1]);
+		final int y = Integer.parseInt(args[2]);
+		final int z = Integer.parseInt(args[3]);
+		final Location location = new Location(user.getWorld(), x, y, z);
+		if (args.length > 4)
+		{
+			location.setYaw((Float.parseFloat(args[4]) + 180 + 360) % 360);
+		}
+		if (args.length > 5)
+		{
+			location.setPitch(Float.parseFloat(args[5]));
+		}
+		sender.sendMessage(_("teleporting"));
+		user.sendMessage(_("teleporting"));
+		user.getTeleport().teleport(location, null, TeleportCause.COMMAND);
 	}
 }
