705efc60024167002b2ecf7114d5fae9dcca6c05
md_5 <md_5@bigpond.com>
2012-05-05 20:01:18 +1000

    We can't damage and kill, so we will just kill to make everyone happy, and the event pointless.

--- Essentials/src/com/earth2me/essentials/commands/Commandsuicide.java
+++ Essentials/src/com/earth2me/essentials/commands/Commandsuicide.java
@@ -3,7 +3,6 @@ package com.earth2me.essentials.commands;
 import static com.earth2me.essentials.I18n._;
 import com.earth2me.essentials.User;
 import org.bukkit.Server;
-import org.bukkit.event.entity.EntityDamageEvent;
 
 
 public class Commandsuicide extends EssentialsCommand
@@ -16,9 +15,6 @@ public class Commandsuicide extends EssentialsCommand
 	@Override
 	public void run(final Server server, final User user, final String commandLabel, final String[] args) throws Exception
 	{
-		EntityDamageEvent ede = new EntityDamageEvent(user.getBase(), EntityDamageEvent.DamageCause.SUICIDE, 1000);
-		server.getPluginManager().callEvent(ede);
-		user.damage(1000);
 		user.setHealth(0);
 		user.sendMessage(_("suicideMessage"));
 		user.setDisplayNick();
