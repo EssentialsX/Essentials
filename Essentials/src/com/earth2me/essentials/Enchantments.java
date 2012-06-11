425c548611aa30ab3a78e0ef40393724239f4800
necrodoom <doomed.war@gmail.com>
2012-06-11 15:08:12 +0300

    pull request for expanding enchant database

--- Essentials/src/com/earth2me/essentials/Enchantments.java
+++ Essentials/src/com/earth2me/essentials/Enchantments.java
@@ -19,23 +19,29 @@ public class Enchantments
 		ENCHANTMENTS.put("alldamage", Enchantment.DAMAGE_ALL);
 		ENCHANTMENTS.put("alldmg", Enchantment.DAMAGE_ALL);
 		ENCHANTMENTS.put("sharpness", Enchantment.DAMAGE_ALL);
+		ENCHANTMENTS.put("sharp", Enchantment.DAMAGE_ALL);
 		
 		ENCHANTMENTS.put("arthropodsdamage", Enchantment.DAMAGE_ARTHROPODS);
 		ENCHANTMENTS.put("ardmg", Enchantment.DAMAGE_ARTHROPODS);
 		ENCHANTMENTS.put("baneofarthropods", Enchantment.DAMAGE_ARTHROPODS);
+		ENCHANTMENTS.put("baneofarthropod", Enchantment.DAMAGE_ARTHROPODS);
+		ENCHANTMENTS.put("arthropod", Enchantment.DAMAGE_ARTHROPODS);
 		
 		ENCHANTMENTS.put("undeaddamage", Enchantment.DAMAGE_UNDEAD);
 		ENCHANTMENTS.put("smite", Enchantment.DAMAGE_UNDEAD);
 		
 		ENCHANTMENTS.put("digspeed", Enchantment.DIG_SPEED);
 		ENCHANTMENTS.put("efficiency", Enchantment.DIG_SPEED);
+		ENCHANTMENTS.put("minespeed", Enchantment.DIG_SPEED);
+		ENCHANTMENTS.put("cutspeed", Enchantment.DIG_SPEED);
 		
 		ENCHANTMENTS.put("durability", Enchantment.DURABILITY);
 		ENCHANTMENTS.put("dura", Enchantment.DURABILITY);
 		ENCHANTMENTS.put("unbreaking", Enchantment.DURABILITY);
 		
 		ENCHANTMENTS.put("fireaspect", Enchantment.FIRE_ASPECT);
-		ENCHANTMENTS.put("fire", Enchantment.FIRE_ASPECT);
+		ENCHANTMENTS.put("meleefire", Enchantment.FIRE_ASPECT);
+		ENCHANTMENTS.put("meleeflame", Enchantment.FIRE_ASPECT);
 		
 		ENCHANTMENTS.put("knockback", Enchantment.KNOCKBACK);
 		
@@ -48,13 +54,18 @@ public class Enchantments
 		
 		ENCHANTMENTS.put("oxygen", Enchantment.OXYGEN);
 		ENCHANTMENTS.put("respiration", Enchantment.OXYGEN);
+		ENCHANTMENTS.put("breathing", Enchantment.OXYGEN);
+		ENCHANTMENTS.put("breath", Enchantment.OXYGEN);
 		
 		ENCHANTMENTS.put("protection", Enchantment.PROTECTION_ENVIRONMENTAL);
 		ENCHANTMENTS.put("prot", Enchantment.PROTECTION_ENVIRONMENTAL);
+		ENCHANTMENTS.put("protect", Enchantment.PROTECTION_ENVIRONMENTAL);
 		
-		ENCHANTMENTS.put("explosionsprotection", Enchantment.PROTECTION_EXPLOSIONS);		
+		ENCHANTMENTS.put("explosionsprotection", Enchantment.PROTECTION_EXPLOSIONS);
+		ENCHANTMENTS.put("explosionprotection", Enchantment.PROTECTION_EXPLOSIONS);
 		ENCHANTMENTS.put("expprot", Enchantment.PROTECTION_EXPLOSIONS);
 		ENCHANTMENTS.put("blastprotection", Enchantment.PROTECTION_EXPLOSIONS);
+		ENCHANTMENTS.put("blastprotect", Enchantment.PROTECTION_EXPLOSIONS);
 		
 		ENCHANTMENTS.put("fallprotection", Enchantment.PROTECTION_FALL);
 		ENCHANTMENTS.put("fallprot", Enchantment.PROTECTION_FALL);
@@ -62,29 +73,39 @@ public class Enchantments
 		ENCHANTMENTS.put("featherfalling", Enchantment.PROTECTION_FALL);
 		
 		ENCHANTMENTS.put("fireprotection", Enchantment.PROTECTION_FIRE);
-		ENCHANTMENTS.put("fireprot", Enchantment.PROTECTION_FIRE);
+		ENCHANTMENTS.put("flameprotection", Enchantment.PROTECTION_FIRE);
+		ENCHANTMENTS.put("fireprotect", Enchantment.PROTECTION_FIRE);
+		ENCHANTMENTS.put("flameprotect", Enchantment.PROTECTION_FIRE);
 		
 		ENCHANTMENTS.put("projectileprotection", Enchantment.PROTECTION_PROJECTILE);
 		ENCHANTMENTS.put("projprot", Enchantment.PROTECTION_PROJECTILE);
 		
 		ENCHANTMENTS.put("silktouch", Enchantment.SILK_TOUCH);
+		ENCHANTMENTS.put("softtouch", Enchantment.SILK_TOUCH);
 		
 		ENCHANTMENTS.put("waterworker", Enchantment.WATER_WORKER);
 		ENCHANTMENTS.put("aquaaffinity", Enchantment.WATER_WORKER);
 		
 		ENCHANTMENTS.put("firearrow", Enchantment.ARROW_FIRE);
 		ENCHANTMENTS.put("flame", Enchantment.ARROW_FIRE);
+		ENCHANTMENTS.put("fire", Enchantment.ARROW_FIRE);
+		ENCHANTMENTS.put("flamearrow", Enchantment.ARROW_FIRE);
 		
 		ENCHANTMENTS.put("arrowdamage", Enchantment.ARROW_DAMAGE);
 		ENCHANTMENTS.put("power", Enchantment.ARROW_DAMAGE);
+		ENCHANTMENTS.put("arrowpower", Enchantment.ARROW_DAMAGE);
 		
 		ENCHANTMENTS.put("arrowknockback", Enchantment.ARROW_KNOCKBACK);
 		ENCHANTMENTS.put("arrowkb", Enchantment.ARROW_KNOCKBACK);
 		ENCHANTMENTS.put("punch", Enchantment.ARROW_KNOCKBACK);
+		ENCHANTMENTS.put("arrowpunch", Enchantment.ARROW_KNOCKBACK);
 		
 		ENCHANTMENTS.put("infinitearrows", Enchantment.ARROW_INFINITE);
 		ENCHANTMENTS.put("infarrows", Enchantment.ARROW_INFINITE);
 		ENCHANTMENTS.put("infinity", Enchantment.ARROW_INFINITE);
+		ENCHANTMENTS.put("infinite", Enchantment.ARROW_INFINITE);
+		ENCHANTMENTS.put("unlimited", Enchantment.ARROW_INFINITE);
+		ENCHANTMENTS.put("unlimitedarrows", Enchantment.ARROW_INFINITE);
 	}
 	
 	public static Enchantment getByName(String name) {
