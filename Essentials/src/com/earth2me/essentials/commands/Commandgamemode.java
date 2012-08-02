fc288718b7c321591c0a78965bd8f5a1f5e6aa1b
ementalo <ementalodev@gmx.co.uk>
2012-08-02 17:43:58 +0100

    Switching the syntax of gamemode /gm mode player and adding mode aliases
    Fixing translation keys
    Fix plugin.yml gamemode syntax

--- Essentials/src/com/earth2me/essentials/commands/Commandgamemode.java
+++ Essentials/src/com/earth2me/essentials/commands/Commandgamemode.java
@@ -45,29 +45,29 @@ public class Commandgamemode extends EssentialsCommand
 
 	private void gamemodeOtherPlayers(final Server server, final CommandSender sender, final String[] args)
 	{
-		for (Player matchPlayer : server.matchPlayer(args[0]))
+		for (Player matchPlayer : server.matchPlayer(args[1]))
 		{
 			final User player = ess.getUser(matchPlayer);
 			if (player.isHidden())
 			{
 				continue;
 			}		
-			performSetMode(args[1], player);
+			performSetMode(args[0], player);
 			sender.sendMessage(_("gameMode", _(player.getGameMode().toString().toLowerCase(Locale.ENGLISH)), player.getDisplayName()));
 		}
 	}
 	
 	private void performSetMode(String mode, Player player)
 	{
-			if (mode.contains("survi") || mode.equalsIgnoreCase("0"))
+			if (mode.contains("survi") || mode.equalsIgnoreCase("0") || mode.equalsIgnoreCase("s"))
 			{
 				player.setGameMode(GameMode.SURVIVAL);
 			}
-			else if (mode.contains("creat") || mode.equalsIgnoreCase("1"))
+			else if (mode.contains("creat") || mode.equalsIgnoreCase("1") || mode.equalsIgnoreCase("c"))
 			{
 				player.setGameMode(GameMode.CREATIVE);
 			}
-			else if (mode.contains("advent") || mode.equalsIgnoreCase("2"))
+			else if (mode.contains("advent") || mode.equalsIgnoreCase("2") || mode.equalsIgnoreCase("a"))
 			{
 				player.setGameMode(GameMode.ADVENTURE);
 			}
