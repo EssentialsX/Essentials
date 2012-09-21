3e80bf479e7f311dfa31bd8d00b0cf7564cad3a7
md_5 <md_5@bigpond.com>
2012-09-21 19:53:20 +1000

    Check for explicit * permission when using PEX.

--- Essentials/src/com/earth2me/essentials/perm/PermissionsExHandler.java
+++ Essentials/src/com/earth2me/essentials/perm/PermissionsExHandler.java
@@ -66,7 +66,7 @@ public class PermissionsExHandler extends SuperpermsHandler
 	@Override
 	public boolean hasPermission(final Player base, final String node)
 	{
-		return super.hasPermission(base, node);
+		return base.hasPermission("*") || super.hasPermission(base, node);
 	}
 
 	@Override
