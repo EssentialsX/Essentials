ef608c75954b74e54543da86ababe960ce95a41e
KHobbits <rob@khobbits.co.uk>
2012-10-20 16:09:54 +0100

    Remove extra home check.

--- Essentials/src/com/earth2me/essentials/commands/Commandhome.java
+++ Essentials/src/com/earth2me/essentials/commands/Commandhome.java
@@ -23,7 +23,6 @@ public class Commandhome extends EssentialsCommand
 	public void run(final Server server, final User user, final String commandLabel, final String[] args) throws Exception
 	{
 		final Trade charge = new Trade(this.getName(), ess);
-		charge.isAffordableFor(user);
 		User player = user;
 		String homeName = "";
 		String[] nameParts;
