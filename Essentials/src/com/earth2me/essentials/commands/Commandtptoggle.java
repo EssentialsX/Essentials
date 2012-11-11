fbb3d7344f46ae813c6e8f3ec9d36329d785af86
Iaccidentally <coryhuckaby@gmail.com>
2012-11-11 14:34:56 -0500

    update tptoggle to allow toggling other players :: new permission: essentials.tptoggle.others

--- Essentials/src/com/earth2me/essentials/commands/Commandtptoggle.java
+++ Essentials/src/com/earth2me/essentials/commands/Commandtptoggle.java
@@ -15,8 +15,26 @@ public class Commandtptoggle extends EssentialsCommand
 	@Override
 	public void run(final Server server, final User user, final String commandLabel, final String[] args) throws Exception
 	{
+		User otherUser = null;
+		if (args.length > 0 && user.isAuthorized("essentials.tptoggle.others"))
+		{
+			otherUser = ess.getUser(server.getPlayer(args[0]));
+				if (otherUser == null)
+				{
+					throw new Exception(_("playerNotFound"));
+				}
+				else
+				{
+				ess.getUser(server.getPlayer(args[0])).sendMessage(user.toggleTeleportEnabled()
+						? _("teleportationEnabled")
+						: _("teleportationDisabled"));
+				}
+		}
+		else 
+		{
 		user.sendMessage(user.toggleTeleportEnabled()
 						 ? _("teleportationEnabled")
 						 : _("teleportationDisabled"));
+		}
 	}
 }
