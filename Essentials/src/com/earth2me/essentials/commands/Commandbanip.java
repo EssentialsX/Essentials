86c0767833e6ea77296e9271d4e93a91e5be1063
KHobbits <rob@khobbits.co.uk>
2013-03-02 17:08:22 +0000

    Fix /banip so it compiles, also throw it through a DRYer.

--- Essentials/src/com/earth2me/essentials/commands/Commandbanip.java
+++ Essentials/src/com/earth2me/essentials/commands/Commandbanip.java
@@ -2,7 +2,6 @@ package com.earth2me.essentials.commands;
 
 import com.earth2me.essentials.Console;
 import static com.earth2me.essentials.I18n._;
-import com.earth2me.essentials.OfflinePlayer;
 import com.earth2me.essentials.User;
 import java.util.logging.Level;
 import org.bukkit.Server;
@@ -25,30 +24,32 @@ public class Commandbanip extends EssentialsCommand
 			throw new NotEnoughArgumentsException();
 		}
 
-		final User player = ess.getUser(args[0]);
+		final String senderName = sender instanceof Player ? ((Player)sender).getDisplayName() : Console.NAME;
+		final String ipAddress;
 
+		final User player = ess.getUser(args[0]);
 		if (player == null)
 		{
-			ess.getServer().banIP(args[0]);
-			sender.sendMessage(_("banIpAddress"), senderName, args[0]);
+			ipAddress = args[0];
 		}
 		else
 		{
-			final String ipAddress = player.getLastLoginAddress();
+			ipAddress = player.getLastLoginAddress();
 			if (ipAddress.length() == 0)
 			{
 				throw new Exception(_("playerNotFound"));
 			}
-			ess.getServer().banIP(ipAddress);
-			final String senderName = sender instanceof Player ? ((Player)sender).getDisplayName() : Console.NAME;
-			server.getLogger().log(Level.INFO, _("banIpAddress", senderName, ipAddress));
-			for (Player onlinePlayer : server.getOnlinePlayers())
+		}
+		
+		ess.getServer().banIP(ipAddress);
+		server.getLogger().log(Level.INFO, _("playerbanIpAddress", senderName, ipAddress));
+
+		for (Player onlinePlayer : server.getOnlinePlayers())
+		{
+			final User onlineUser = ess.getUser(onlinePlayer);
+			if (onlinePlayer == sender || onlineUser.isAuthorized("essentials.ban.notify"))
 			{
-				final User player = ess.getUser(onlinePlayer);
-				if (onlinePlayer == sender || player.isAuthorized("essentials.ban.notify"))
-				{
-					sender.sendMessage(_("banIpAddress", senderName, ipAddress));
-				}
+				sender.sendMessage(_("playerBanIpAddress", senderName, ipAddress));
 			}
 		}
 	}
