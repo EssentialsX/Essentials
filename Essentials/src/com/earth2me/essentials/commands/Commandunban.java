ee7b50a0149e9b61e9335a285bbb80a660a82af4
Skylexia-PC <skyla@skylexia.com>
2013-03-10 03:22:35 -0400

    [Feature] Broadcast unbans to online staff.

--- Essentials/src/com/earth2me/essentials/commands/Commandunban.java
+++ Essentials/src/com/earth2me/essentials/commands/Commandunban.java
@@ -1,10 +1,12 @@
 package com.earth2me.essentials.commands;
 
+import com.earth2me.essentials.Console;
 import static com.earth2me.essentials.I18n._;
 import com.earth2me.essentials.User;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.Server;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 
 public class Commandunban extends EssentialsCommand
@@ -21,25 +23,33 @@ public class Commandunban extends EssentialsCommand
 		{
 			throw new NotEnoughArgumentsException();
 		}
-
+		String name;
 		try
 		{
 			final User user = getPlayer(server, args, 0, true);
+			name = user.getName();
 			user.setBanned(false);
 			user.setBanTimeout(0);
-			sender.sendMessage(_("unbannedPlayer"));
 		}
 		catch (NoSuchFieldException e)
 		{
 			final OfflinePlayer player = server.getOfflinePlayer(args[0]);
-			if (player.isBanned()) 
+			name = player.getName();
+			if (!player.isBanned())
 			{
-				player.setBanned(false);
-				sender.sendMessage(_("unbannedPlayer"));
-				return;
-			}			
-			
-			throw new Exception(_("playerNotFound"), e);
+				throw new Exception(_("playerNotFound"), e);
+			}
+			player.setBanned(false);
+		}
+		
+		final String senderName = sender instanceof Player ? ((Player)sender).getDisplayName() : Console.NAME;
+		for (Player onlinePlayer : server.getOnlinePlayers())
+		{
+			final User onlineUser = ess.getUser(onlinePlayer);
+			if (onlinePlayer == sender || onlineUser.isAuthorized("essentials.ban.notify"))
+			{
+				onlinePlayer.sendMessage(_("playerUnbanned", senderName, name));
+			}
 		}
 	}
 }
