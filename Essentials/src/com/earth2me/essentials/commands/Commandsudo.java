578576e3171d4a3e9d1189ee95982fd575c37d8c
Necrodoom <doomed.war@gmail.com>
2013-03-25 16:11:48 +0200

    [Fix] Sudo allowed on vanished players.

--- Essentials/src/com/earth2me/essentials/commands/Commandsudo.java
+++ Essentials/src/com/earth2me/essentials/commands/Commandsudo.java
@@ -27,7 +27,7 @@ public class Commandsudo extends EssentialsCommand
 			throw new NotEnoughArgumentsException();
 		}
 
-		final User user = getPlayer(server, args, 0);
+		final User user = getPlayer(server, args, 0, true, false);
 		if(args[1].toLowerCase(Locale.ENGLISH).startsWith("c:"))
 		{
 			if (user.isAuthorized("essentials.sudo.exempt") && sender instanceof Player)
