0a0369698339f0d0d8ea82d705366c6df6dd057d
Iaccidentally <coryhuckaby@gmail.com>
2013-04-28 16:23:45 -0400

    [API] Add a warp api, with future 3.x support :: minor cleanup

--- Essentials/src/com/earth2me/essentials/IReplyTo.java
+++ Essentials/src/com/earth2me/essentials/IReplyTo.java
@@ -2,8 +2,17 @@ package com.earth2me.essentials;
 
 import org.bukkit.command.CommandSender;
 
-public interface IReplyTo {
+public interface IReplyTo
+{
+	/**
+	 * Sets the user to reply to
+	 * @param user
+	 */
 	public void setReplyTo(CommandSender user);
 
+	/**
+	 * Gets the user the sender should reply to
+	 * @return
+	 */
 	public CommandSender getReplyTo();
 }
