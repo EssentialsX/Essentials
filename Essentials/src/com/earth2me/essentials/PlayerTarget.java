98e427e831c43a619a57aacc034535c4d5d09213
KHobbits <rob@khobbits.co.uk>
2013-06-08 21:43:42 +0100

    Cleanup ITarget

--- Essentials/src/com/earth2me/essentials/PlayerTarget.java
+++ Essentials/src/com/earth2me/essentials/PlayerTarget.java
@@ -7,22 +7,16 @@ import org.bukkit.entity.Player;
 
 public class PlayerTarget implements ITarget
 {
-	private final Location location;
 	private final String name;
 
 	PlayerTarget(Player entity)
 	{
 		this.name = entity.getName();
-		this.location = null;
 	}
 
 	@Override
 	public Location getLocation()
 	{
-		if (this.name != null)
-		{
-			return Bukkit.getServer().getPlayerExact(name).getLocation();
-		}
-		return location;
+		return Bukkit.getServer().getPlayerExact(name).getLocation();
 	}
 }
