f6fd0bdb32a100c3da67975057bb6410c86a22de
GunfighterJ <joseph.jenniges@gmail.com>
2013-06-30 07:40:24 -0500

    Force reading in UTF-8 from .txt files

--- Essentials/src/com/earth2me/essentials/textreader/TextInput.java
+++ Essentials/src/com/earth2me/essentials/textreader/TextInput.java
@@ -61,7 +61,8 @@ public class TextInput implements IText
 			}
 			if (readFromfile)
 			{
-				final BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+				final Reader reader = new InputStreamReader(new FileInputStream(file), "utf-8");
+				final BufferedReader bufferedReader = new BufferedReader(reader);
 				try
 				{
 					int lineNumber = 0;
