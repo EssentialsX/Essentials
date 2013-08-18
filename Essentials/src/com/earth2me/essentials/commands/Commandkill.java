b8cdd7d1f7955d188add26e42b91c8fb13e54665
KHobbits <rob@khobbits.co.uk>
2013-08-18 21:04:19 +0100

    Fix NPE in /kill when used from console. Thanks Necrodoom

--- Essentials/src/com/earth2me/essentials/commands/Commandkill.java
+++ Essentials/src/com/earth2me/essentials/commands/Commandkill.java
@@ -32,7 +32,7 @@ public class Commandkill extends EssentialsCommand
 		final List<Player> matchedPlayers = server.matchPlayer(args[0]);
 		for (Player matchPlayer : matchedPlayers)
 		{
-			if (ess.getUser(matchPlayer).isAuthorized("essentials.kill.exempt") && !ess.getUser(sender).isAuthorized("essentials.kill.force"))
+			if (sender instanceof Player && ess.getUser(matchPlayer).isAuthorized("essentials.kill.exempt") && !ess.getUser(sender).isAuthorized("essentials.kill.force"))
 			{
 				throw new Exception(_("killExempt", matchPlayer.getDisplayName()));
 			}
