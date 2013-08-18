a06a81c15dac29888661e068e97495748e431701
KHobbits <rob@khobbits.co.uk>
2013-08-19 00:41:21 +0100

    New command /customtext - This command is intended to be used with bukkit.yml aliases to make custom commands.

---
+++ Essentials/src/com/earth2me/essentials/commands/Commandcustomtext.java
@@ -0,0 +1,26 @@
+package com.earth2me.essentials.commands;
+
+import com.earth2me.essentials.textreader.IText;
+import com.earth2me.essentials.textreader.KeywordReplacer;
+import com.earth2me.essentials.textreader.TextInput;
+import com.earth2me.essentials.textreader.TextPager;
+import org.bukkit.Server;
+import org.bukkit.command.CommandSender;
+
+
+public class Commandcustomtext extends EssentialsCommand
+{
+	public Commandcustomtext()
+	{
+		super("customtext");
+	}
+
+	@Override
+	protected void run(final Server server, final CommandSender sender, final String commandLabel, final String[] args) throws Exception
+	{
+		final IText input = new TextInput(sender, "custom", true, ess);
+		final IText output = new KeywordReplacer(input, sender, ess);
+		final TextPager pager = new TextPager(output);
+		pager.showPage(commandLabel, args.length > 0 ? args[0] : null, null, sender);
+	}
+}
