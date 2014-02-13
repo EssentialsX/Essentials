c50f5595126ec808ead7d51c20268a8ac8aaa4ee
KHobbits <rob@khobbits.co.uk>
2014-02-13 22:52:14 +0000

    Allow sending customtext chapters as a parameter instead of reading commandLabel

--- Essentials/src/com/earth2me/essentials/commands/Commandcustomtext.java
+++ Essentials/src/com/earth2me/essentials/commands/Commandcustomtext.java
@@ -5,6 +5,7 @@ import com.earth2me.essentials.textreader.IText;
 import com.earth2me.essentials.textreader.KeywordReplacer;
 import com.earth2me.essentials.textreader.TextInput;
 import com.earth2me.essentials.textreader.TextPager;
+import com.earth2me.essentials.utils.NumberUtil;
 import org.bukkit.Server;
 
 
@@ -21,6 +22,19 @@ public class Commandcustomtext extends EssentialsCommand
 		final IText input = new TextInput(sender, "custom", true, ess);
 		final IText output = new KeywordReplacer(input, sender, ess);
 		final TextPager pager = new TextPager(output);
-		pager.showPage(commandLabel, args.length > 0 ? args[0] : null, null, sender);
+		String chapter = commandLabel;
+		String page;
+
+		if (commandLabel.equalsIgnoreCase("customtext") && args.length > 0 && !NumberUtil.isInt(commandLabel))
+		{
+			chapter = args[0];
+			page = args.length > 1 ? args[1] : null;
+		}
+		else
+		{
+			page = args.length > 0 ? args[0] : null;
+		}
+		
+		pager.showPage(chapter, page, null, sender);
 	}
 }
