fe9fbf585fe35abd2141f94a2b1372b719d118cc
KHobbits <rob@khobbits.co.uk>
2014-04-05 15:33:38 +0100

    Correctly handle line overflow in book pager Fixes a bug with the last book pager tweak.

--- Essentials/src/com/earth2me/essentials/textreader/BookPager.java
+++ Essentials/src/com/earth2me/essentials/textreader/BookPager.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import org.bukkit.Bukkit;
 
 
 public class BookPager
@@ -94,7 +95,10 @@ public class BookPager
 
 					tempLine = pageLine.substring(start, pointer);
 					pageLines.add(tempLine);
-					buildPage(pages, pageLines, forcePageEnd);
+					if (buildPage(pages, pageLines, forcePageEnd))
+					{
+						pageLength = 0;
+					}
 					forcePageEnd = false;
 
 					start = pointer;
@@ -149,7 +153,10 @@ public class BookPager
 			{
 				tempLine = pageLine.substring(start, lineLength);
 				pageLines.add(tempLine);
-				buildPage(pages, pageLines, false);
+				if (buildPage(pages, pageLines, false))
+				{
+					pageLength = 0;
+				}
 			}
 		}
 
@@ -157,7 +164,7 @@ public class BookPager
 		return pages;
 	}
 
-	void buildPage(List<String> pages, List<String> lines, boolean override)
+	boolean buildPage(List<String> pages, List<String> lines, boolean override)
 	{
 		if (override || lines.size() > lineMax)
 		{
@@ -168,6 +175,8 @@ public class BookPager
 			}
 			pages.add(newPage.toString());
 			lines.clear();
+			return true;
 		}
+		return false;
 	}
 }
