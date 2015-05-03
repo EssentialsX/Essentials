20f79f117f36ef6db35c8b0000bab7583ed8d69d
drtshock <drtshock13@gmail.com>
2015-05-03 15:08:32 -0500

    Make sudo a loop command. Adds #23.

--- Essentials/src/com/earth2me/essentials/commands/Commandsudo.java
+++ Essentials/src/com/earth2me/essentials/commands/Commandsudo.java
@@ -1,21 +1,20 @@
 package com.earth2me.essentials.commands;
 
+import com.earth2me.essentials.ChargeException;
 import com.earth2me.essentials.CommandSource;
 import com.earth2me.essentials.User;
+import net.ess3.api.MaxMoneyException;
 import org.bukkit.Server;
 
 import java.util.Locale;
-import java.util.logging.Logger;
 
 import static com.earth2me.essentials.I18n.tl;
 
 
-public class Commandsudo extends EssentialsCommand {
+public class Commandsudo extends EssentialsLoopCommand {
     public Commandsudo() {
         super("sudo");
     }
-
-    private static final Logger LOGGER = Logger.getLogger("Essentials");
 
     @Override
     public void run(final Server server, final CommandSource sender, final String commandLabel, final String[] args) throws Exception {
@@ -23,27 +22,35 @@ public class Commandsudo extends EssentialsCommand {
             throw new NotEnoughArgumentsException();
         }
 
-        final User user = getPlayer(server, sender, args, 0);
-        if (args[1].toLowerCase(Locale.ENGLISH).startsWith("c:")) {
-            if (user.isAuthorized("essentials.sudo.exempt") && sender.isPlayer()) {
-                throw new Exception(tl("sudoExempt"));
-            }
-            user.getBase().chat(getFinalArg(args, 1).substring(2));
-            return;
-        }
         final String[] arguments = new String[args.length - 1];
         if (arguments.length > 0) {
             System.arraycopy(args, 1, arguments, 0, args.length - 1);
         }
 
-        if (user.isAuthorized("essentials.sudo.exempt") && sender.isPlayer()) {
-            throw new Exception(tl("sudoExempt"));
+        final String command = getFinalArg(arguments, 0);
+        boolean multiple = sender.getSender().hasPermission("essentials.sudo.multiple");
+
+        sender.sendMessage(tl("sudoRun", args[0], command, ""));
+        loopOnlinePlayers(server, sender, multiple, multiple, args[0], new String[]{command});
+    }
+
+    @Override
+    protected void updatePlayer(final Server server, final CommandSource sender, final User user, String[] args) throws NotEnoughArgumentsException, PlayerExemptException, ChargeException, MaxMoneyException {
+        if (user.getName().equals(sender.getSender().getName())) {
+            return; // Silently don't do anything.
         }
 
-        final String command = getFinalArg(arguments, 0);
+        if (user.isAuthorized("essentials.sudo.exempt") && sender.isPlayer()) {
+            sender.sendMessage(tl("sudoExempt", user.getName()));
+            return;
+        }
 
-        sender.sendMessage(tl("sudoRun", user.getDisplayName(), command, ""));
+        if (args[0].toLowerCase(Locale.ENGLISH).startsWith("c:")) {
+            user.getBase().chat(getFinalArg(args, 0).substring(2));
+            return;
+        }
 
+        final String command = getFinalArg(args, 0);
         if (command != null && command.length() > 0) {
             class SudoCommandTask implements Runnable {
                 @Override
