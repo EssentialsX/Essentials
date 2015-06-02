b19dbb1b8fbe8bbad70693951e37a754e06d7fd3
vemacs <gentoo@outlook.com>
2015-06-02 13:08:53 -0600

    Make permissions-plugin specific implementations extend Vault, fix build permission checks

--- Essentials/src/com/earth2me/essentials/perm/BPermissions2Handler.java
+++ Essentials/src/com/earth2me/essentials/perm/BPermissions2Handler.java
@@ -1,51 +1,11 @@
 package com.earth2me.essentials.perm;
 
-import de.bananaco.bpermissions.api.ApiLayer;
-import de.bananaco.bpermissions.api.util.CalculableType;
 import org.bukkit.entity.Player;
 
-import java.util.Arrays;
-import java.util.List;
 
-
-public class BPermissions2Handler extends SuperpermsHandler {
-    public BPermissions2Handler() {
-
-    }
-
-    @Override
-    public String getGroup(final Player base) {
-        final List<String> groups = getGroups(base);
-        if (groups == null || groups.isEmpty()) {
-            return null;
-        }
-        return groups.get(0);
-    }
-
-    @Override
-    public List<String> getGroups(final Player base) {
-        final String[] groups = ApiLayer.getGroups(base.getWorld().getName(), CalculableType.USER, base.getName());
-        return Arrays.asList(groups);
-    }
-
-    @Override
-    public boolean inGroup(final Player base, final String group) {
-        return ApiLayer.hasGroupRecursive(base.getWorld().getName(), CalculableType.USER, base.getName(), group);
-    }
-
+public class BPermissions2Handler extends AbstractVaultHandler {
     @Override
     public boolean canBuild(final Player base, final String group) {
         return hasPermission(base, "bPermissions.build");
     }
-
-    @Override
-    public String getPrefix(final Player base) {
-        return ApiLayer.getValue(base.getWorld().getName(), CalculableType.USER, base.getName(), "prefix");
-    }
-
-    @Override
-    public String getSuffix(final Player base) {
-        return ApiLayer.getValue(base.getWorld().getName(), CalculableType.USER, base.getName(), "suffix");
-    }
-
 }
--- Essentials/src/com/earth2me/essentials/perm/SimplyPermsHandler.java
+++ Essentials/src/com/earth2me/essentials/perm/SimplyPermsHandler.java
@@ -1,48 +1,10 @@
 package com.earth2me.essentials.perm;
 
-import net.crystalyx.bukkit.simplyperms.SimplyAPI;
-import net.crystalyx.bukkit.simplyperms.SimplyPlugin;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.Plugin;
 
-import java.util.List;
-
-public class SimplyPermsHandler extends SuperpermsHandler {
-
-    private final transient SimplyAPI api;
-
-    public SimplyPermsHandler(final Plugin plugin) {
-        this.api = ((SimplyPlugin) plugin).getAPI();
-    }
-
-    @Override
-    public String getGroup(final Player base) {
-        final List<String> groups = api.getPlayerGroups(base.getName());
-        if (groups == null || groups.isEmpty()) {
-            return null;
-        }
-        return groups.get(0);
-    }
-
-    @Override
-    public List<String> getGroups(final Player base) {
-        return api.getPlayerGroups(base.getName());
-    }
-
-    @Override
-    public boolean inGroup(final Player base, final String group) {
-        final List<String> groups = api.getPlayerGroups(base.getName());
-        for (String group1 : groups) {
-            if (group1.equalsIgnoreCase(group)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
+public class SimplyPermsHandler extends AbstractVaultHandler {
     @Override
     public boolean canBuild(Player base, String group) {
         return hasPermission(base, "permissions.allow.build");
     }
-
 }
