0a2f1a3dfef64f499b51f3dba89e9594e9faae8c
vemacs <gentoo@outlook.com>
2015-06-02 13:09:54 -0600

    Fix PrivilegesHandler

--- Essentials/src/com/earth2me/essentials/perm/PrivilegesHandler.java
+++ Essentials/src/com/earth2me/essentials/perm/PrivilegesHandler.java
@@ -2,7 +2,7 @@ package com.earth2me.essentials.perm;
 
 import org.bukkit.entity.Player;
 
-public class PrivilegesHandler extends SuperpermsHandler {
+public class PrivilegesHandler extends AbstractVaultHandler {
     @Override
     public boolean canBuild(Player base, String group) {
         return hasPermission(base, "privileges.build");
