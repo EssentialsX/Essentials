891605380ffcc52671f3e6874a28b32f16ffce58
vemacs <d@nkmem.es>
2015-07-28 20:29:17 -0600

    Add /msgtoggle command, untested (implements #89)

---
+++ Essentials/src/com/earth2me/essentials/commands/Commandmsgtoggle.java
@@ -0,0 +1,49 @@
+package com.earth2me.essentials.commands;
+
+import com.earth2me.essentials.CommandSource;
+import com.earth2me.essentials.User;
+import org.bukkit.Server;
+
+import static com.earth2me.essentials.I18n.tl;
+
+
+public class Commandmsgtoggle extends EssentialsToggleCommand {
+    public Commandmsgtoggle() {
+        super("msgtoggle", "essentials.msgtoggle.others");
+    }
+
+    @Override
+    protected void run(final Server server, final CommandSource sender, final String commandLabel, final String[] args) throws Exception {
+        toggleOtherPlayers(server, sender, args);
+    }
+
+    @Override
+    protected void run(final Server server, final User user, final String commandLabel, final String[] args) throws Exception {
+        if (args.length == 1) {
+            Boolean toggle = matchToggleArgument(args[0]);
+            if (toggle == null && user.isAuthorized(othersPermission)) {
+                toggleOtherPlayers(server, user.getSource(), args);
+            } else {
+                togglePlayer(user.getSource(), user, toggle);
+            }
+        } else if (args.length == 2 && user.isAuthorized(othersPermission)) {
+            toggleOtherPlayers(server, user.getSource(), args);
+        } else {
+            togglePlayer(user.getSource(), user, null);
+        }
+    }
+
+    @Override
+    void togglePlayer(CommandSource sender, User user, Boolean enabled) {
+        if (enabled == null) {
+            enabled = !user.isIgnoreMsg();
+        }
+
+        user.setIgnoreMsg(enabled);
+
+        user.sendMessage(enabled ? tl("msgEnabled") : tl("msgDisabled"));
+        if (!sender.isPlayer() || !sender.getPlayer().equals(user.getBase())) {
+            sender.sendMessage(enabled ? tl("msgEnabledFor", user.getDisplayName()) : tl("msgDisabledFor", user.getDisplayName()));
+        }
+    }
+}
