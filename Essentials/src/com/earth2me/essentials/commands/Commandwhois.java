51d43937c1036e902433124e5cefdb73c899a604
Cory Redmond <ace@ac3-servers.eu>
2016-02-21 17:12:10 +0000

    Let people see what proxy the player is coming from.

--- Essentials/src/com/earth2me/essentials/commands/Commandwhois.java
+++ Essentials/src/com/earth2me/essentials/commands/Commandwhois.java
@@ -5,16 +5,33 @@ import com.earth2me.essentials.User;
 import com.earth2me.essentials.craftbukkit.SetExpFix;
 import com.earth2me.essentials.utils.DateUtil;
 import com.earth2me.essentials.utils.NumberUtil;
+import org.bukkit.Bukkit;
 import org.bukkit.Server;
+import org.bukkit.entity.Player;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.InetSocketAddress;
 import java.util.Locale;
 
 import static com.earth2me.essentials.I18n.tl;
 
 
 public class Commandwhois extends EssentialsCommand {
+
+    private Field bungeeField = null;
+
     public Commandwhois() {
         super("whois");
+
+        try {
+            Class spigotConfigClass = Class.forName( "org.spigotmc.SpigotConfig" );
+            this.bungeeField = spigotConfigClass.getDeclaredField("bungee");
+        } catch (ClassNotFoundException | NoSuchFieldException ex) {
+            // Yeah we don't need this.
+        }
+
     }
 
     @Override
@@ -48,5 +65,37 @@ public class Commandwhois extends EssentialsCommand {
         sender.sendMessage(tl("whoisJail", (user.isJailed() ? user.getJailTimeout() > 0 ? DateUtil.formatDateDiff(user.getJailTimeout()) : tl("true") : tl("false"))));
         sender.sendMessage(tl("whoisMuted", (user.isMuted() ? user.getMuteTimeout() > 0 ? DateUtil.formatDateDiff(user.getMuteTimeout()) : tl("true") : tl("false"))));
 
+        InetSocketAddress addr = getRawAddress(Bukkit.getServer().getPlayer(user.getConfigUUID()));
+        if( isBungeeCord() && addr != null ) {
+            sender.sendMessage(tl("whoisProxyAddress", addr.getAddress().toString()));
+        }
+
     }
+
+    public InetSocketAddress getRawAddress(Player player) {
+        if( player == null ) return null;
+        try {
+            Object obj = Player.class.getDeclaredMethod("spigot").invoke(player);
+
+            if(obj != null) {
+                Method method = obj.getClass().getDeclaredMethod("getRawAddress");
+                method.setAccessible(true);
+                return (InetSocketAddress) method.invoke(obj);
+            }
+        } catch (IllegalAccessException | NoSuchMethodException | InvocationTargetException | ClassCastException e) {
+            // We'll leave this aswell.
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public boolean isBungeeCord() {
+        try {
+            return (boolean) bungeeField.get( null );
+        } catch (IllegalAccessException | ClassCastException e) {
+			e.printStackTrace();
+            return false;
+        }
+    }
+
 }
