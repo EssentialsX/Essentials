8199cc40f41ced7dea7f4b6532525f7d771c9ce3
md678685 <mathew678@hotmail.co.uk>
2018-01-29 18:50:41 +0000

    Plugins are not providers
    Deregister the specific EssEco provider, not just every single Vault provider ever because that's utterly ridiculous and stupid and why would anyone do that *glares at self*

--- Essentials/src/com/earth2me/essentials/vault/VaultHookManager.java
+++ Essentials/src/com/earth2me/essentials/vault/VaultHookManager.java
@@ -2,8 +2,11 @@ package com.earth2me.essentials.vault;
 
 import com.earth2me.essentials.Essentials;
 import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.economy.plugins.Economy_Essentials;
 import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.ServicePriority;
+import org.bukkit.plugin.ServicesManager;
 
 /**
  * Responsible for registering EssentialsX's own Vault hook, instead of relying on
@@ -12,12 +15,20 @@ import org.bukkit.plugin.ServicePriority;
 public class VaultHookManager {
 
     /**
-     * Deregister Vault's default hooks, including the legacy EssentialsEco hook.
-     * This may not always be desired behaviour.
+     * Deregister Vault's default legacy EssentialsEco hook.
      */
-    public void unhookLegacy(Essentials ess) {
-        Plugin vaultPlugin = ess.getServer().getPluginManager().getPlugin("Vault");
-        ess.getServer().getServicesManager().unregister(Economy.class, vaultPlugin);
+    public boolean unhookLegacy(Essentials ess) {
+        final Plugin vaultPlugin = ess.getServer().getPluginManager().getPlugin("Vault");
+        final ServicesManager sm = ess.getServer().getServicesManager();
+
+        for (RegisteredServiceProvider rsp : sm.getRegistrations(vaultPlugin)) {
+            if (rsp.getProvider() instanceof Economy_Essentials) {
+                sm.unregister(rsp.getProvider());
+                return true;
+            }
+        }
+
+        return false;
     }
 
     /**
