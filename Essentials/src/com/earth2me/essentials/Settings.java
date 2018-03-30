f93206830119a56878d4b6efd6f8d7bbac2bafbb
loper12 <DtheExp@outlook.com>
2018-03-30 09:43:16 +0100

    Revert "Change line 495 to match the function."
    This reverts commit 15df1c604cbfed867ed1a72d6fd10c3f866d2ed3.

--- Essentials/src/com/earth2me/essentials/Settings.java
+++ Essentials/src/com/earth2me/essentials/Settings.java
@@ -492,7 +492,7 @@ public class Settings implements net.ess3.api.ISettings {
         cancelAfkOnMove = _cancelAfkOnMove();
         cancelAfkOnChat = _cancelAfkOnChat();
         cancelAfkOnDisconnect = _cancelAfkOnDisconnect();
-        afkBroadcastOnDisconnect = _afkBroadcastOnDisconnect();
+        afkBroadcastOnDisconnect = _afkMessageOnDisconnect();
         afkBroadcastOnMutedChat = _afkBroadcastOnMutedChat();
         getFreezeAfkPlayers = _getFreezeAfkPlayers();
         afkListName = _getAfkListName();
