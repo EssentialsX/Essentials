4295fad5ce4987689ccf72dfe872395c9856af6a
Xeyame <34656654+Xeyame@users.noreply.github.com>
2018-04-02 19:00:55 +0200

    Combine 2 checks

--- Essentials/src/com/earth2me/essentials/messaging/SimpleMessageRecipient.java
+++ Essentials/src/com/earth2me/essentials/messaging/SimpleMessageRecipient.java
@@ -121,10 +121,8 @@ public class SimpleMessageRecipient implements IMessageRecipient {
         User user = getUser(this);
         boolean afk = false;
         if (user != null) {
-            if (user.isIgnoreMsg())  {
-              if (sender instanceof IUser && !((IUser) sender).isAuthorized("essentials.msgtoggle.bypass")) { // Don't ignore console and senders with permission
+              if (user.isIgnoreMsg() && sender instanceof IUser && !((IUser) sender).isAuthorized("essentials.msgtoggle.bypass")) { // Don't ignore console and senders with permission
                   return MessageResponse.MESSAGES_IGNORED;
-              }
             }
             afk = user.isAfk();
             // Check whether this recipient ignores the sender, only if the sender is not the console.
