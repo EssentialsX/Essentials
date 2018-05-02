b50cf766757653227ce5ff3c24b7115d18809dec
montlikadani <montlikada@gmail.com>
2018-05-02 17:52:10 +0200

    Converting ping-pong to player ping

--- Essentials/src/com/earth2me/essentials/commands/Commandping.java
+++ Essentials/src/com/earth2me/essentials/commands/Commandping.java
@@ -3,22 +3,46 @@ package com.earth2me.essentials.commands;
 import com.earth2me.essentials.CommandSource;
 import com.earth2me.essentials.utils.FormatUtil;
 import org.bukkit.Server;
+import org.bukkit.entity.Player;
 
 import static com.earth2me.essentials.I18n.tl;
 
-// This command can be used to echo messages to the users screen, mostly useless but also an #EasterEgg
+import java.lang.reflect.Field;
+
 public class Commandping extends EssentialsCommand {
     public Commandping() {
         super("ping");
     }
 
     @Override
-    public void run(final Server server, final CommandSource sender, final String commandLabel, final String[] args) throws Exception {
+    public void run(final Server server, final User user final CommandSource sender, final String commandLabel, final String[] args) throws Exception {
         if (args.length < 1) {
-
-            sender.sendMessage(tl("pong"));
+            final String ping = String.valueOf(getPing(user));
+            user.sendMessage(tl("playerPing", ping));
         } else {
-            sender.sendMessage(FormatUtil.replaceFormat(getFinalArg(args, 0)));
+            user.sendMessage(FormatUtil.replaceFormat(getFinalArg(args, 0)));
         }
+    }
+
+    private int getPing(Player p) {
+    	int pingInt = 0;
+    	Object nmsPlayer = getNMSPlayer(p);
+    	try {
+    		Field ping = nmsPlayer.getClass().getField("ping");
+    		ping.setAccessible(true);
+		    pingInt = ping.getInt(nmsPlayer);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    	return pingInt;
+    }
+
+    private static Object getNMSPlayer(Player p) {
+    	try {
+    		return p.getClass().getMethod("getHandle", new Class[0]).invoke(p, new Object[0]);
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    	}
+    	return null;
     }
 }
