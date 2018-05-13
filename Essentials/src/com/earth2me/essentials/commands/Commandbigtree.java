ed813a3c9df214b63e932537f9ac72ec3efd8315
montlikadani <montlikada@gmail.com>
2018-05-13 14:26:22 +0200

    Update Commandbigtree.java

--- Essentials/src/com/earth2me/essentials/commands/Commandbigtree.java
+++ Essentials/src/com/earth2me/essentials/commands/Commandbigtree.java
@@ -27,8 +27,6 @@ public class Commandbigtree extends EssentialsCommand {
             tree = TreeType.BIG_TREE;
         } else if (args.length > 0 && args[0].equalsIgnoreCase("jungle")) {
             tree = TreeType.JUNGLE;
-        } else if (args.length > 0 && args[0].equalsIgnoreCase("acacia")) {
-            tree = TreeType.ACACIA;
         } else if (args.length > 0 && args[0].equalsIgnoreCase("darkoak")) {
             tree = TreeType.DARK_OAK;
         } else {
@@ -48,7 +46,7 @@ public class Commandbigtree extends EssentialsCommand {
     @Override
     protected List<String> getTabCompleteOptions(Server server, User user, String commandLabel, String[] args) {
         if (args.length == 1) {
-            return Lists.newArrayList("redwood", "tree", "jungle", "acacia", "darkoak");
+            return Lists.newArrayList("redwood", "tree", "jungle", "darkoak");
         } else {
             return Collections.emptyList();
         }
