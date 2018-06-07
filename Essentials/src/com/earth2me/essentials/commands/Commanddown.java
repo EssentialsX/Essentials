3ae1d338b2e28064469f45a161636b7300cf8bce
AllTheMegahertz <markvadeika@gmail.com>
2018-06-06 20:30:37 -0400

    Added /down command

---
+++ Essentials/src/com/earth2me/essentials/commands/Commanddown.java
@@ -0,0 +1,34 @@
+package com.earth2me.essentials.commands;
+
+import com.earth2me.essentials.Trade;
+import com.earth2me.essentials.User;
+import com.earth2me.essentials.utils.LocationUtil;
+import org.bukkit.Location;
+import org.bukkit.Server;
+import org.bukkit.event.player.PlayerTeleportEvent;
+
+import static com.earth2me.essentials.I18n.tl;
+
+public class Commanddown extends EssentialsCommand {
+    public Commanddown() {
+        super("down");
+    }
+
+    @Override
+    public void run(final Server server, final User user, final String commandLabel, final String[] args) throws Exception {
+        final int topX = user.getLocation().getBlockX();
+        final int topZ = user.getLocation().getBlockZ();
+        final float pitch = user.getLocation().getPitch();
+        final float yaw = user.getLocation().getYaw();
+
+        for (int y = user.getLocation().getBlockY() - 3; y > 0; y--) {
+            if (!LocationUtil.isBlockUnsafe(user.getWorld(), topX, y, topZ)) {
+                Location loc = new Location(user.getWorld(), topX, y, topZ, yaw, pitch);
+                user.getTeleport().teleport(loc, new Trade(this.getName(), ess), PlayerTeleportEvent.TeleportCause.COMMAND);
+                user.sendMessage(tl("teleportDown", loc.getWorld().getName(), loc.getBlockX(), loc.getBlockY(), loc.getBlockZ()));
+                return;
+            }
+        }
+
+    }
+}
