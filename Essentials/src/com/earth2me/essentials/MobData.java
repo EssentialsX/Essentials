8fc1d82e76490ac68f99aca1bfbbbd6b448d82d9
montlikadani <montlikada@gmail.com>
2018-10-22 17:26:11 +0200

    Update MobData.java

--- Essentials/src/com/earth2me/essentials/MobData.java
+++ Essentials/src/com/earth2me/essentials/MobData.java
@@ -142,7 +142,7 @@ public enum MobData {
     private String matched;
 
     public static LinkedHashMap<String, MobData> getPossibleData(final Entity spawned, boolean publicOnly) {
-        LinkedHashMap<String, MobData> mobList = new LinkedHashMap<String, MobData>();
+        LinkedHashMap<String, MobData> mobList = new LinkedHashMap<>();
         for (MobData data : MobData.values()) {
             if (data.type instanceof EntityType && spawned.getType().equals(data.type) && ((publicOnly && data.isPublic) || !publicOnly)) {
                 mobList.put(data.nickname.toLowerCase(Locale.ENGLISH), data);
@@ -155,7 +155,7 @@ public enum MobData {
     }
 
     public static List<String> getValidHelp(final Entity spawned) {
-        List<String> output = new ArrayList<String>();
+        List<String> output = new ArrayList<>();
         LinkedHashMap<String, MobData> posData = getPossibleData(spawned, true);
 
         for (MobData data : posData.values()) {
