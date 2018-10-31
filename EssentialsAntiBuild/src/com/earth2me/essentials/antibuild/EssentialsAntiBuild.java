1b367bc6fef973ebe92cfe72b62828af63eb64d2
Mark <markvadeika@gmail.com>
2018-10-31 08:47:44 -0400

    Updated AntiBuild to use new item system

--- EssentialsAntiBuild/src/com/earth2me/essentials/antibuild/EssentialsAntiBuild.java
+++ EssentialsAntiBuild/src/com/earth2me/essentials/antibuild/EssentialsAntiBuild.java
@@ -1,5 +1,7 @@
 package com.earth2me.essentials.antibuild;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -11,7 +13,7 @@ import java.util.Map;
 
 public class EssentialsAntiBuild extends JavaPlugin implements IAntiBuild {
     private final transient Map<AntiBuildConfig, Boolean> settingsBoolean = new EnumMap<AntiBuildConfig, Boolean>(AntiBuildConfig.class);
-    private final transient Map<AntiBuildConfig, List<Integer>> settingsList = new EnumMap<AntiBuildConfig, List<Integer>>(AntiBuildConfig.class);
+    private final transient Map<AntiBuildConfig, List<Material>> settingsList = new EnumMap<AntiBuildConfig, List<Material>>(AntiBuildConfig.class);
     private transient EssentialsConnect ess = null;
 
     @Override
@@ -28,9 +30,9 @@ public class EssentialsAntiBuild extends JavaPlugin implements IAntiBuild {
     }
 
     @Override
-    public boolean checkProtectionItems(final AntiBuildConfig list, final int id) {
-        final List<Integer> itemList = settingsList.get(list);
-        return itemList != null && !itemList.isEmpty() && itemList.contains(id);
+    public boolean checkProtectionItems(final AntiBuildConfig list, final Material type) {
+        final List<Material> itemList = settingsList.get(list);
+        return itemList != null && !itemList.isEmpty() && itemList.contains(type);
     }
 
     @Override
@@ -44,7 +46,7 @@ public class EssentialsAntiBuild extends JavaPlugin implements IAntiBuild {
     }
 
     @Override
-    public Map<AntiBuildConfig, List<Integer>> getSettingsList() {
+    public Map<AntiBuildConfig, List<Material>> getSettingsList() {
         return settingsList;
     }
 
