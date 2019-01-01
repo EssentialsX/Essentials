ae7a404afe202b1bf436cbe5773d0b05a697474d
triagonal <harry@triagonal.xyz>
2019-01-01 19:19:23 +1100

    disallow setting bed homes in non-overworld dimensions

--- Essentials/src/com/earth2me/essentials/EssentialsPlayerListener.java
+++ Essentials/src/com/earth2me/essentials/EssentialsPlayerListener.java
@@ -13,6 +13,7 @@ import org.bukkit.BanList;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
@@ -600,7 +601,7 @@ public class EssentialsPlayerListener implements Listener {
             case RIGHT_CLICK_BLOCK:
                 if (!event.isCancelled() && event.getClickedBlock().getType() == Material.BED_BLOCK && ess.getSettings().getUpdateBedAtDaytime()) {
                     User player = ess.getUser(event.getPlayer());
-                    if (player.isAuthorized("essentials.sethome.bed")) {
+                    if (player.isAuthorized("essentials.sethome.bed") && player.getWorld().getEnvironment().equals(World.Environment.NORMAL)) {
                         player.getBase().setBedSpawnLocation(event.getClickedBlock().getLocation());
                         player.sendMessage(tl("bedSet", player.getLocation().getWorld().getName(), player.getLocation().getBlockX(), player.getLocation().getBlockY(), player.getLocation().getBlockZ()));
                     }
