44e101cbb391592f137f1274952bc25d6d162bf0
darbyjack <admin@glaremasters.me>
2019-02-27 20:50:13 -0600

    Started on a base for the EssentialsDiscord module

--- EssentialsXDiscord/src/com/earth2me/essentials/discord/EssentialsDiscord.java
+++ EssentialsXDiscord/src/com/earth2me/essentials/discord/EssentialsDiscord.java
@@ -1,7 +1,39 @@
 package com.earth2me.essentials.discord;
 
+import com.earth2me.essentials.IConf;
 import com.earth2me.essentials.IEssentialsModule;
+import com.earth2me.essentials.metrics.Metrics;
+import net.ess3.api.IEssentials;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class EssentialsDiscord extends JavaPlugin implements IEssentialsModule {
+import java.util.logging.Level;
+
+import static com.earth2me.essentials.I18n.tl;
+
+public class EssentialsDiscord extends JavaPlugin implements IEssentialsModule, IConf {
+
+    private transient Metrics metrics = null;
+
+    @Override
+    public void onEnable() {
+        final PluginManager pm = getServer().getPluginManager();
+        final IEssentials ess = (IEssentials) pm.getPlugin("Essentials");
+        if (!this.getDescription().getVersion().equals(ess.getDescription().getVersion())) {
+            getLogger().log(Level.WARNING, tl("versionMismatchAll"));
+        }
+
+        if (!ess.isEnabled()) {
+            this.setEnabled(false);
+            return;
+        }
+
+        final EssentialsDiscordListener discordListener = new EssentialsDiscordListener(getDataFolder(), ess);
+        pm.registerEvents(discordListener, this);
+
+
+        if (metrics == null) {
+            metrics = new Metrics(this);
+        }
+    }
 }
