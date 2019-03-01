32afcde66b7b2f635d6f223657f12b55430a2ecc
md678685 <mathew678@hotmail.co.uk>
2019-03-01 11:51:15 +0000

    Remove Lombok
    Remove the (already minimal) usage of Lombok. This will ease development for people who don't already have their IDE set up for Lombok support.
    
    It's also totally unnecessary - it took less than 60 seconds to generate the code needed to replace Lombok annotations in Jails and Spawns.

--- Essentials/src/com/earth2me/essentials/settings/Jails.java
+++ Essentials/src/com/earth2me/essentials/settings/Jails.java
@@ -2,15 +2,39 @@ package com.earth2me.essentials.settings;
 
 import com.earth2me.essentials.storage.MapValueType;
 import com.earth2me.essentials.storage.StorageObject;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
 import org.bukkit.Location;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 
-@Data @EqualsAndHashCode(callSuper = false) public class Jails implements StorageObject {
+public class Jails implements StorageObject {
     @MapValueType(Location.class)
     private Map<String, Location> jails = new HashMap<String, Location>();
+
+    public Map<String, Location> getJails() {
+        return jails;
+    }
+
+    public void setJails(Map<String, Location> jails) {
+        this.jails = jails;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Jails jails1 = (Jails) o;
+        return Objects.equals(jails, jails1.jails);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(jails);
+    }
 }
