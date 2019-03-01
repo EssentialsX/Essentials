29962bb9df3b440af95516cfe21beeb6f852a2a7
DoNotSpamPls <7570108+DoNotSpamPls@users.noreply.github.com>
2019-03-01 20:11:15 +0200

    Remove Lombok usages in the plugin

--- Essentials/src/com/earth2me/essentials/settings/Jails.java
+++ Essentials/src/com/earth2me/essentials/settings/Jails.java
@@ -2,15 +2,35 @@ package com.earth2me.essentials.settings;
 
 import com.earth2me.essentials.storage.MapValueType;
 import com.earth2me.essentials.storage.StorageObject;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
 import org.bukkit.Location;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 
-@Data @EqualsAndHashCode(callSuper = false) public class Jails implements StorageObject {
+public class Jails implements StorageObject {
     @MapValueType(Location.class)
-    private Map<String, Location> jails = new HashMap<String, Location>();
+    private Map<String, Location> jails = new HashMap<>();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Jails jails1 = (Jails) o;
+        return Objects.equals(jails, jails1.jails);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(jails);
+    }
+
+    public Map<String, Location> getJails() {
+        return jails;
+    }
+
+    public void setJails(Map<String, Location> jails) {
+        this.jails = jails;
+    }
 }
--- Essentials/src/com/earth2me/essentials/settings/Spawns.java
+++ Essentials/src/com/earth2me/essentials/settings/Spawns.java
@@ -2,14 +2,34 @@ package com.earth2me.essentials.settings;
 
 import com.earth2me.essentials.storage.MapValueType;
 import com.earth2me.essentials.storage.StorageObject;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
 import org.bukkit.Location;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
-@Data @EqualsAndHashCode(callSuper = false) public class Spawns implements StorageObject {
+public class Spawns implements StorageObject {
     @MapValueType(Location.class)
     private Map<String, Location> spawns = new HashMap<>();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Spawns spawns1 = (Spawns) o;
+        return Objects.equals(spawns, spawns1.spawns);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(spawns);
+    }
+
+    public Map<String, Location> getSpawns() {
+        return spawns;
+    }
+
+    public void setSpawns(Map<String, Location> spawns) {
+        this.spawns = spawns;
+    }
 }
