761fd5fff79ca26d0ca1846f7b3815a403f508eb
md678685 <mathew678@hotmail.co.uk>
2019-03-08 11:22:54 +0000

    Add message field to AfkStatusChangeEvent

--- Essentials/src/net/ess3/api/events/AfkStatusChangeEvent.java
+++ Essentials/src/net/ess3/api/events/AfkStatusChangeEvent.java
@@ -4,7 +4,20 @@ import net.ess3.api.IUser;
 
 
 public class AfkStatusChangeEvent extends StatusChangeEvent {
+    private String message = null;
+
+    // TODO: refactor AFK code to pass include /afk message here
+    public AfkStatusChangeEvent(IUser affected, boolean value, String message) {
+        super(affected, affected, value);
+        this.message = message;
+    }
+
+    @Deprecated
     public AfkStatusChangeEvent(IUser affected, boolean value) {
         super(affected, affected, value);
+    }
+
+    public String getMessage() {
+        return message;
     }
 }
