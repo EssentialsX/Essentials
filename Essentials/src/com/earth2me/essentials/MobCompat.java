7790f8818be5b375210ddc7db380c6155226d295
md678685 <mathew678@hotmail.co.uk>
2019-05-09 15:12:51 +0100

    Add Villager variants, pt 1

--- Essentials/src/com/earth2me/essentials/MobCompat.java
+++ Essentials/src/com/earth2me/essentials/MobCompat.java
@@ -8,16 +8,10 @@ import org.bukkit.entity.Villager;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.Arrays;
 
 public class MobCompat {
 
     public static final EntityType CAT = EnumUtil.getEntityType("CAT", "OCELOT");
-
-    private static Class catClass = null;
-    private static Class catTypeClass = null;
-    private static Method catSetTypeMethod = null;
-    private static Boolean isNewCat = null;
 
     public enum CatType {
         SIAMESE("SIAMESE", "SIAMESE_CAT"),
@@ -42,6 +36,60 @@ public class MobCompat {
         }
     }
 
+    public enum VillagerProfession {
+        NONE("FARMER", "FARMER", "NONE"),
+        ARMORER("BLACKSMITH", "ARMORER"),
+        BUTCHER("FARMER", "BUTCHER"),
+        CARTOGRAPHER("LIBRARIAN", "CARTOGRAPHER"),
+        CLERIC("PRIEST", "CLERIC"),
+        FARMER("FARMER", "FARMER"),
+        FISHERMAN("FARMER", "FISHERMAN"),
+        FLETCHER("FARMER", "FLETCHER"),
+        LEATHERWORKER("BUTCHER", "LEATHERWORKER"),
+        LIBRARIAN("LIBRARIAN", "LIBRARIAN"),
+        MASON(null, null, "MASON"),
+        NITWIT("NITWIT", "NITWIT"),
+        SHEPHERD("FARMER", "SHEPHERD"),
+        TOOLSMITH("BLACKSMITH", "TOOL_SMITH", "TOOLSMITH"),
+        WEAPONSMITH("BLACKSMITH", "WEAPON_SMITH", "WEAPONSMITH")
+        ;
+
+        private String oldProfession;
+        private String oldCareer;
+        private String newProfession;
+
+        VillagerProfession(final String oldProfession, final String career) {
+            this.oldProfession = oldProfession;
+            this.oldCareer = career;
+            this.newProfession = career;
+        }
+
+        VillagerProfession(final String oldProfession, final String oldCareer, final String newProfession) {
+            this.oldProfession = oldProfession;
+            this.oldCareer = oldCareer;
+            this.newProfession = newProfession;
+        }
+
+        private Villager.Profession asEnum() {
+            return EnumUtil.valueOf(Villager.Profession.class, newProfession, oldProfession);
+        }
+    }
+
+    public enum VillagerType {
+        DESERT,
+        JUNGLE,
+        PLAINS,
+        SAVANNA,
+        SNOWY,
+        SWAMP,
+        TAIGA
+    }
+
+    private static Class catClass = null;
+    private static Class catTypeClass = null;
+    private static Method catSetTypeMethod = null;
+    private static Boolean isNewCat = null;
+
     public static void setCatType(final Entity entity, final CatType type) {
         if (isNewCat == null) {
             try {
@@ -65,10 +113,60 @@ public class MobCompat {
         }
     }
 
-    private static Villager.Profession getVillagerProfession(String... names) {
-        // Add nitwit as a default in case we're on older versions
-        names = Arrays.asList(names, "NITWIT").toArray(new String[0]);
-        return EnumUtil.valueOf(Villager.Profession.class, names);
+    private static Boolean isNewVillager = null;
+    private static Class villagerCareerClass = null;
+    private static Method villagerSetCareerMethod = null;
+    private static Class villagerTypeClass = null;
+    private static Method villagerSetTypeMethod = null;
+
+
+    private static void checkVillagerEnums() {
+        try {
+            villagerCareerClass = Class.forName("org.bukkit.entity.Villager.Career");
+            villagerSetCareerMethod = Villager.class.getDeclaredMethod("setCareer", villagerCareerClass);
+            isNewVillager = false;
+        } catch (ClassNotFoundException | NoSuchMethodException e) {
+            try {
+                villagerTypeClass = Class.forName("org.bukkit.entity.Villager.Type");
+                villagerSetTypeMethod = Villager.class.getDeclaredMethod("setVillagerType", villagerTypeClass);
+                isNewVillager = true;
+            } catch (ClassNotFoundException | NoSuchMethodException e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+
+    public static void setVillagerProfession(final Entity entity, final VillagerProfession profession) {
+        if (isNewVillager == null) {
+            checkVillagerEnums();
+        }
+
+        if (isNewVillager) {
+            ((Villager) entity).setProfession(profession.asEnum());
+        } else {
+            ((Villager) entity).setProfession(profession.asEnum());
+            try {
+                villagerSetCareerMethod.invoke(entity, EnumUtil.valueOf(villagerCareerClass, profession.oldCareer));
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void setVillagerType(final Entity entity, final VillagerType type) {
+        if (isNewVillager == null) {
+            checkVillagerEnums();
+        }
+
+        if (!isNewVillager) {
+            return;
+        }
+
+        try {
+            villagerSetTypeMethod.invoke(entity, EnumUtil.valueOf(villagerTypeClass, type.name()));
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
     }
 
 }
