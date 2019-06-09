e513e159187d7e2385cbe85f998443ed92e50141
montlikadani <montlikada@gmail.com>
2019-06-09 17:59:46 +0200

    Update StateChangeEvent.java

--- Essentials/src/net/ess3/api/events/StateChangeEvent.java
+++ Essentials/src/net/ess3/api/events/StateChangeEvent.java
@@ -2,11 +2,15 @@ package net.ess3.api.events;
 
 import net.ess3.api.IUser;
 import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
 
 /**
  * This handles common boilerplate for other StateChangeEvents
  */
-public class StateChangeEvent extends BaseEvent implements Cancellable {
+public class StateChangeEvent extends Event implements Cancellable {
+    private static final HandlerList handlers = new HandlerList();
     private boolean cancelled = false;
     IUser affected;
     IUser controller;
@@ -29,6 +33,15 @@ public class StateChangeEvent extends BaseEvent implements Cancellable {
 
     public IUser getController() {
         return controller;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
     }
 
     @Override
