c4ac18ee2feee86ef4964491d3f74726b472b339
scpketer <scpketer3@gmail.com>
2019-07-03 10:53:34 +0400

    Implement disposal sign enhancement #2628

--- Essentials/src/com/earth2me/essentials/signs/SignDisposal.java
+++ Essentials/src/com/earth2me/essentials/signs/SignDisposal.java
@@ -1,5 +1,7 @@
 package com.earth2me.essentials.signs;
 
+import com.earth2me.essentials.ChargeException;
+import com.earth2me.essentials.Trade;
 import com.earth2me.essentials.User;
 import net.ess3.api.IEssentials;
 
@@ -12,8 +14,22 @@ public class SignDisposal extends EssentialsSign {
     }
 
     @Override
-    protected boolean onSignInteract(final ISign sign, final User player, final String username, final IEssentials ess) {
-        player.getBase().openInventory(ess.getServer().createInventory(player.getBase(), 36, tl("disposal")));
+    protected boolean onSignInteract(final ISign sign, final User player, final String username, final IEssentials ess) throws SignException, ChargeException {
+        if (!sign.getLine(3).isEmpty()) {
+            Trade charge = getTrade(sign, 3, ess);
+            charge.isAffordableFor(player);
+        }
+
+        StringBuilder titleBuilder = new StringBuilder();
+
+        titleBuilder.append(sign.getLine(1));
+        titleBuilder.append(sign.getLine(2));
+
+        if (titleBuilder.length() == 0) {
+            titleBuilder.append(tl("disposal"));
+        }
+
+        player.getBase().openInventory(ess.getServer().createInventory(player.getBase(), 36, titleBuilder.toString()));
         return true;
     }
 }
