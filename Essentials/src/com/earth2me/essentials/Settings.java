37a249f193b72dde567b4887206ada7bdd32023a
darbyjack <admin@glaremasters.me>
2019-08-21 15:52:52 -0500

    .x: SPAWN NO HOME OPTION

--- Essentials/src/com/earth2me/essentials/Settings.java
+++ Essentials/src/com/earth2me/essentials/Settings.java
@@ -1623,4 +1623,9 @@ public class Settings implements net.ess3.api.ISettings {
     public double getMaxProjectileSpeed() {
         return maxProjectileSpeed;
     }
+
+    @Override
+    public boolean isSpawnIfNoHome() {
+        return config.getBoolean("spawn-if-no-home", false);
+    }
 }
