1ba7569bab024e340a3b1174513ebf9c1de8921a
pop4959 <pop4959@gmail.com>
2019-10-02 16:12:32 -0700

    Implement max mute time

--- Essentials/src/com/earth2me/essentials/ISettings.java
+++ Essentials/src/com/earth2me/essentials/ISettings.java
@@ -238,6 +238,8 @@ public interface ISettings extends IConf {
 
     void setEssentialsChatActive(boolean b);
 
+    long getMaxMute();
+
     long getMaxTempban();
 
     Map<String, Object> getListGroupConfig();
