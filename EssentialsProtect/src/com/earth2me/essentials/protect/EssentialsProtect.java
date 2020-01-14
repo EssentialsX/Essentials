EssentialsX/Essentials

commit 2549ed830f902aea992abfad93d3c257db6a79aa

Enable control of entity transformations in EssentialsProtect (#2836)
This allows users to prevent any of the following transformations:

- Creeper charging
- Villager infected by zombie villagers
- Villager being cured
- Villagers turning into witches
- Pigs turning into zombie pigmen
- Zombies turning into Drowned (and husks turning into Zombies)
- Mooshrooms switching colors

Configurations are in a new subsection, `protect.prevent.transformation`. All options are disabled by default.

---

* Allow for control of transformations

* Undid removal of villager checking

* Undid removal of villager checking

* Documentation for transformation configs

Co-Authored-By: md678685 <1917406+md678685@users.noreply.github.com>

* Removed duplicate configuration lines

* Updated info about husks drowning

* Changed config names

Co-authored-by: md678685 <1917406+md678685@users.noreply.github.com>

EssentialsProtect/src/com/earth2me/essentials/protect/EssentialsProtectEntityListener.java
@@ -171,6 +171,29 @@ public class EssentialsProtectEntityListener implements Listener {
     }
 
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onEntityTransform(final EntityTransformEvent event) {
+        final Entity entity = event.getEntity();
+        final EntityTransformEvent.TransformReason reason = event.getTransformReason();
+        if (reason == EntityTransformEvent.TransformReason.INFECTION && prot.getSettingBool(ProtectConfig.prevent_villager_infection)) {
+            event.setCancelled(true);
+        } else if (reason == EntityTransformEvent.TransformReason.CURED && prot.getSettingBool(ProtectConfig.prevent_villager_cure)) {
+            event.setCancelled(true);
+        } else if (reason == EntityTransformEvent.TransformReason.LIGHTNING) {
+            if (entity instanceof Villager && prot.getSettingBool(ProtectConfig.prevent_villager_to_witch)) {
+                event.setCancelled(true);
+            } else if (entity instanceof Pig && prot.getSettingBool(ProtectConfig.prevent_pig_transformation)) {
+                event.setCancelled(true);
+            } else if (entity instanceof Creeper && prot.getSettingBool(ProtectConfig.prevent_creeper_charge)) {
+                event.setCancelled(true);
+            } else if (entity instanceof MushroomCow && prot.getSettingBool(ProtectConfig.prevent_mooshroom_switching)) {
+                event.setCancelled(true);
+            }
+        } else if (reason == EntityTransformEvent.TransformReason.DROWNED && prot.getSettingBool(ProtectConfig.prevent_zombie_drowning)) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onCreatureSpawn(final CreatureSpawnEvent event) {
         if (event.getEntity() instanceof Player) {
             return;

Essentials/src/config.yml
@@ -758,6 +758,22 @@ protect:
     # Monsters won't follow players.
     # permission essentials.protect.entitytarget.bypass disables this.
     entitytarget: false
+    # Prevent certain transformations.
+    transformation:
+      # Prevent creepers becoming charged when struck by lightning.
+      charged-creeper: false
+      # Prevent villagers becoming zombie villagers.
+      zombie-villager: false
+      # Prevent zombie villagers being cured.
+      villager: false
+      # Prevent villagers becoming witches when struck by lightning.
+      witch: false
+      # Prevent pigs becoming zombie pigmen when struck by lightning.
+      zombie-pigman: false
+      # Prevent zombies turning into drowneds, and husks turning into zombies.
+      drowned: false
+      # Prevent mooshrooms changing colour when struck by lightning.
+      mooshroom: false
     # Prevent the spawning of creatures.
     spawn:
       creeper: false

EssentialsProtect/src/com/earth2me/essentials/protect/ProtectConfig.java
@@ -42,7 +42,14 @@ public enum ProtectConfig {
     prevent_villager_death("protect.prevent.villager-death", false),
     prevent_enderdragon_blockdmg("protect.prevent.enderdragon-blockdamage", true),
     prevent_entitytarget("protect.prevent.entitytarget", false),
-    enderdragon_fakeexplosions("protect.enderdragon-fakeexplosions", false);
+    enderdragon_fakeexplosions("protect.enderdragon-fakeexplosions", false),
+    prevent_creeper_charge("protect.prevent.transformation.charged-creeper", false),
+    prevent_villager_infection("protect.prevent.transformation.zombie-villager", false),
+    prevent_villager_cure("protect.prevent.transformation.villager", false),
+    prevent_villager_to_witch("protect.prevent.transformation.witch", false),
+    prevent_pig_transformation("protect.prevent.transformation.zombie-pigman", false),
+    prevent_zombie_drowning("protect.prevent.transformation.drowned", false),
+    prevent_mooshroom_switching("protect.prevent.transformation.mooshroom", false);
     private final String configName;
     private final String defValueString;
     private final boolean defValueBoolean;

commit 2e818ac7090626a6682b19f2b8ce6fe385dbc170

Properly register entity transform event depending on version (#2942)
Fixes the problem mentioned in #2931 which was introduced in #2836. This is caused as a result of EntityTransformEvent being a relatively new event introduced in Spigot [856adc3e6af](https://hub.spigotmc.org/stash/projects/SPIGOT/repos/bukkit/commits/856adc3e6afc3e9c0495ac27e38b96b144e54b1e), which was after the update to 1.13.2 in Spigot [f311182396b](https://hub.spigotmc.org/stash/projects/SPIGOT/repos/bukkit/commits/f311182396bd74b106efebc926ea4d0b92cfba80).

Closes #2931.

EssentialsProtect/src/com/earth2me/essentials/protect/EssentialsProtect.java
@@ -1,6 +1,7 @@
 package com.earth2me.essentials.protect;
 
 import com.earth2me.essentials.metrics.Metrics;
+import com.earth2me.essentials.utils.VersionUtil;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
@@ -48,6 +49,11 @@ public class EssentialsProtect extends JavaPlugin implements IProtect {
 
         final EssentialsProtectEntityListener entityListener = new EssentialsProtectEntityListener(this);
         pm.registerEvents(entityListener, this);
+
+        if (VersionUtil.getServerBukkitVersion().isHigherThan(VersionUtil.v1_13_2_R01)) {
+            final EssentialsProtectEntityListener1_13_2_R1 entityListener1_13_2_r1 = new EssentialsProtectEntityListener1_13_2_R1(this);
+            pm.registerEvents(entityListener1_13_2_r1, this);
+        }
 
         final EssentialsProtectWeatherListener weatherListener = new EssentialsProtectWeatherListener(this);
         pm.registerEvents(weatherListener, this);

EssentialsProtect/src/com/earth2me/essentials/protect/EssentialsProtectEntityListener.java
@@ -171,29 +171,6 @@ public class EssentialsProtectEntityListener implements Listener {
     }
 
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-    public void onEntityTransform(final EntityTransformEvent event) {
-        final Entity entity = event.getEntity();
-        final EntityTransformEvent.TransformReason reason = event.getTransformReason();
-        if (reason == EntityTransformEvent.TransformReason.INFECTION && prot.getSettingBool(ProtectConfig.prevent_villager_infection)) {
-            event.setCancelled(true);
-        } else if (reason == EntityTransformEvent.TransformReason.CURED && prot.getSettingBool(ProtectConfig.prevent_villager_cure)) {
-            event.setCancelled(true);
-        } else if (reason == EntityTransformEvent.TransformReason.LIGHTNING) {
-            if (entity instanceof Villager && prot.getSettingBool(ProtectConfig.prevent_villager_to_witch)) {
-                event.setCancelled(true);
-            } else if (entity instanceof Pig && prot.getSettingBool(ProtectConfig.prevent_pig_transformation)) {
-                event.setCancelled(true);
-            } else if (entity instanceof Creeper && prot.getSettingBool(ProtectConfig.prevent_creeper_charge)) {
-                event.setCancelled(true);
-            } else if (entity instanceof MushroomCow && prot.getSettingBool(ProtectConfig.prevent_mooshroom_switching)) {
-                event.setCancelled(true);
-            }
-        } else if (reason == EntityTransformEvent.TransformReason.DROWNED && prot.getSettingBool(ProtectConfig.prevent_zombie_drowning)) {
-            event.setCancelled(true);
-        }
-    }
-
-    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onCreatureSpawn(final CreatureSpawnEvent event) {
         if (event.getEntity() instanceof Player) {
             return;

EssentialsProtect/src/com/earth2me/essentials/protect/EssentialsProtectEntityListener1_13_2_R1.java
@@ -1,0 +1,45 @@
+package com.earth2me.essentials.protect;
+
+import net.ess3.api.IEssentials;
+import org.bukkit.entity.Creeper;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.MushroomCow;
+import org.bukkit.entity.Pig;
+import org.bukkit.entity.Villager;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityTransformEvent;
+
+public class EssentialsProtectEntityListener1_13_2_R1 implements Listener {
+    private final IProtect prot;
+    private final IEssentials ess;
+
+    EssentialsProtectEntityListener1_13_2_R1(final IProtect prot) {
+        this.prot = prot;
+        this.ess = prot.getEssentialsConnect().getEssentials();
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onEntityTransform(final EntityTransformEvent event) {
+        final Entity entity = event.getEntity();
+        final EntityTransformEvent.TransformReason reason = event.getTransformReason();
+        if (reason == EntityTransformEvent.TransformReason.INFECTION && prot.getSettingBool(ProtectConfig.prevent_villager_infection)) {
+            event.setCancelled(true);
+        } else if (reason == EntityTransformEvent.TransformReason.CURED && prot.getSettingBool(ProtectConfig.prevent_villager_cure)) {
+            event.setCancelled(true);
+        } else if (reason == EntityTransformEvent.TransformReason.LIGHTNING) {
+            if (entity instanceof Villager && prot.getSettingBool(ProtectConfig.prevent_villager_to_witch)) {
+                event.setCancelled(true);
+            } else if (entity instanceof Pig && prot.getSettingBool(ProtectConfig.prevent_pig_transformation)) {
+                event.setCancelled(true);
+            } else if (entity instanceof Creeper && prot.getSettingBool(ProtectConfig.prevent_creeper_charge)) {
+                event.setCancelled(true);
+            } else if (entity instanceof MushroomCow && prot.getSettingBool(ProtectConfig.prevent_mooshroom_switching)) {
+                event.setCancelled(true);
+            }
+        } else if (reason == EntityTransformEvent.TransformReason.DROWNED && prot.getSettingBool(ProtectConfig.prevent_zombie_drowning)) {
+            event.setCancelled(true);
+        }
+    }
+}