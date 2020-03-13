3e69b402d02fa1a11d710c24919b4e5a6b46b73a
JRoy <joshroy126@gmail.com>
2020-03-13 10:29:34 -0400

    Improve MuteStatusChangeEvent

--- Essentials/src/net/ess3/api/events/MuteStatusChangeEvent.java
+++ Essentials/src/net/ess3/api/events/MuteStatusChangeEvent.java
@@ -4,7 +4,35 @@ import net.ess3.api.IUser;
 
 
 public class MuteStatusChangeEvent extends StatusChangeEvent {
+    private Long timestamp;
+    private String reason;
+
+    @Deprecated
     public MuteStatusChangeEvent(IUser affected, IUser controller, boolean value) {
+        this(affected, controller, value, null, null);
+    }
+
+    public MuteStatusChangeEvent(IUser affected, IUser controller, boolean value, Long timestamp, String reason) {
         super(affected, controller, value);
+        this.timestamp = timestamp;
+        if (reason == null) {
+            this.reason = null;
+        } else {
+            this.reason = reason.isEmpty() ? null : reason;
+        }
+    }
+
+    /**
+     * @return Returns null if the timestamp is unknown due to usage of deprecated api; Otherwise, the timestamp with 0 indicating permanent.
+     */
+    public Long getTimestamp() {
+        return timestamp;
+    }
+
+    /**
+     * @return Returns null if no reason is provided or due to the usage of a deprecated api; Otherwise, the reason.
+     */
+    public String getReason() {
+        return reason;
     }
 }
