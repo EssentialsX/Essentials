45d7e1ec209eccf385c2ed2506de01677d9f3438
JRoy <joshroy126@gmail.com>
2020-03-31 22:08:08 -0400

    Cleanup

--- Essentials/src/com/earth2me/essentials/Kit.java
+++ Essentials/src/com/earth2me/essentials/Kit.java
@@ -152,9 +152,12 @@ public class Kit {
 
             boolean spew = false;
             final boolean allowUnsafe = ess.getSettings().allowUnsafeEnchantments();
+            final boolean currencyIsSuffix = ess.getSettings().getCurrencySuffix();
+            final String currencySymbol = ess.getSettings().getCurrencySymbol();
             for (String kitItem : output.getLines()) {
-                if (!ess.getSettings().getCurrencySuffix() ? kitItem.startsWith(ess.getSettings().getCurrencySymbol()) : kitItem.endsWith(ess.getSettings().getCurrencySymbol())) {
-                    BigDecimal value = ess.getSettings().getCurrencySuffix() ? new BigDecimal(kitItem.substring(0, ess.getSettings().getCurrencySymbol().length()).trim()) : new BigDecimal(kitItem.substring(ess.getSettings().getCurrencySymbol().length()).trim());
+                if (!currencyIsSuffix ? kitItem.startsWith(ess.getSettings().getCurrencySymbol()) : kitItem.endsWith(ess.getSettings().getCurrencySymbol())) {
+                    final String valueString = currencyIsSuffix ? kitItem.substring(0, currencySymbol.length()) : kitItem.substring(currencySymbol.length());
+                    BigDecimal value = new BigDecimal(valueString.trim());
                     Trade t = new Trade(value, ess);
                     t.pay(user, OverflowType.DROP);
                     continue;
