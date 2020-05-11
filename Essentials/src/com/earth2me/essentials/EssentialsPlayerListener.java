df3dc450d6ca0d315f65a9541cacc220a3b75041
Destitution-SC <43219594+Destitution-SC@users.noreply.github.com>
2020-05-12 00:30:07 +0200

    Update EssentialsPlayerListener.java

--- Essentials/src/com/earth2me/essentials/EssentialsPlayerListener.java
+++ Essentials/src/com/earth2me/essentials/EssentialsPlayerListener.java
@@ -19,6 +19,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.inventory.ClickType;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
