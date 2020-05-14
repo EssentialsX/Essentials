c36424633dfb7c43f05bbcfe6d05bff7dfde38f4
JRoy <joshroy126@gmail.com>
2020-05-14 17:07:06 -0400

    Start maven structure

---
+++ EssentialsXDiscord/src/com/earth2me/essentials/discord/EssentialsXDiscord.java
@@ -0,0 +1,7 @@
+package com.earth2me.essentials.discord;
+
+import com.earth2me.essentials.IEssentialsModule;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class EssentialsDiscord extends JavaPlugin implements IEssentialsModule {
+}
