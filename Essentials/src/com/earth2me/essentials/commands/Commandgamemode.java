6fda5d3209c3916c7df93113a42a65dfd3ad5e04
JRoy <joshroy126@gmail.com>
2020-06-01 01:04:58 -0400

    Make gamemode command a loop command

--- Essentials/src/com/earth2me/essentials/commands/Commandgamemode.java
+++ Essentials/src/com/earth2me/essentials/commands/Commandgamemode.java
@@ -1,11 +1,11 @@
 package com.earth2me.essentials.commands;
 
 import com.earth2me.essentials.CommandSource;
+import com.earth2me.essentials.IUser;
 import com.earth2me.essentials.User;
 import com.google.common.collect.ImmutableList;
 import org.bukkit.GameMode;
 import org.bukkit.Server;
-import org.bukkit.entity.Player;
 
 import java.util.Collections;
 import java.util.List;
@@ -14,22 +14,19 @@ import java.util.Locale;
 import static com.earth2me.essentials.I18n.tl;
 
 
-public class Commandgamemode extends EssentialsCommand {
+public class Commandgamemode extends EssentialsLoopCommand {
     public Commandgamemode() {
         super("gamemode");
     }
 
     @Override
     protected void run(final Server server, final CommandSource sender, final String commandLabel, final String[] args) throws Exception {
-        GameMode gameMode;
         if (args.length == 0) {
             throw new NotEnoughArgumentsException();
         } else if (args.length == 1) {
-            gameMode = matchGameMode(commandLabel);
-            gamemodeOtherPlayers(server, sender, gameMode, args[0]);
+            loopOnlinePlayersConsumer(server, sender, false, true, args[0], user -> setUserGamemode(sender, matchGameMode(commandLabel), user));
         } else if (args.length == 2) {
-            gameMode = matchGameMode(args[0].toLowerCase(Locale.ENGLISH));
-            gamemodeOtherPlayers(server, sender, gameMode, args[1]);
+            loopOnlinePlayersConsumer(server, sender, false, true, args[1], user -> setUserGamemode(sender, matchGameMode(commandLabel), user));
         }
 
     }
@@ -40,16 +37,14 @@ public class Commandgamemode extends EssentialsCommand {
         if (args.length == 0) {
             gameMode = matchGameMode(commandLabel);
         } else if (args.length > 1 && args[1].trim().length() > 2 && user.isAuthorized("essentials.gamemode.others")) {
-            gameMode = matchGameMode(args[0].toLowerCase(Locale.ENGLISH));
-            gamemodeOtherPlayers(server, user.getSource(), gameMode, args[1]);
+            loopOnlinePlayersConsumer(server, user.getSource(), false, true, args[1], player -> setUserGamemode(user.getSource(), matchGameMode(args[0].toLowerCase(Locale.ENGLISH)), player));
             return;
         } else {
             try {
                 gameMode = matchGameMode(args[0].toLowerCase(Locale.ENGLISH));
             } catch (NotEnoughArgumentsException e) {
                 if (user.isAuthorized("essentials.gamemode.others")) {
-                    gameMode = matchGameMode(commandLabel);
-                    gamemodeOtherPlayers(server, user.getSource(), gameMode, args[0]);
+                    loopOnlinePlayersConsumer(server, user.getSource(), false, true, args[0], player -> setUserGamemode(user.getSource(), matchGameMode(commandLabel), player));
                     return;
                 }
                 throw new NotEnoughArgumentsException();
@@ -60,7 +55,7 @@ public class Commandgamemode extends EssentialsCommand {
             gameMode = user.getBase().getGameMode() == GameMode.SURVIVAL ? GameMode.CREATIVE : user.getBase().getGameMode() == GameMode.CREATIVE ? GameMode.ADVENTURE : GameMode.SURVIVAL;
         }
 
-        if (!canChangeToMode(user, gameMode)) {
+        if (isProhibitedChange(user, gameMode)) {
             user.sendMessage(tl("cantGamemode", gameMode.name()));
             return;
         }
@@ -69,36 +64,23 @@ public class Commandgamemode extends EssentialsCommand {
         user.sendMessage(tl("gameMode", tl(user.getBase().getGameMode().toString().toLowerCase(Locale.ENGLISH)), user.getDisplayName()));
     }
 
-    private void gamemodeOtherPlayers(final Server server, final CommandSource sender, final GameMode gameMode, final String name) throws NotEnoughArgumentsException, PlayerNotFoundException {
-        if (name.trim().length() < 2 || gameMode == null) {
+    private void setUserGamemode(final CommandSource sender, final GameMode gameMode, final User user) throws NotEnoughArgumentsException {
+        if (gameMode == null) {
             throw new NotEnoughArgumentsException(tl("gameModeInvalid"));
         }
 
-        if (sender.isPlayer() && !canChangeToMode(ess.getUser(sender.getPlayer()), gameMode)) {
+        if (sender.isPlayer() && isProhibitedChange(sender.getUser(ess), gameMode)) {
             sender.sendMessage(tl("cantGamemode", gameMode.name()));
             return;
         }
 
-        boolean skipHidden = sender.isPlayer() && !ess.getUser(sender.getPlayer()).canInteractVanished();
-        boolean foundUser = false;
-        final List<Player> matchedPlayers = server.matchPlayer(name);
-        for (Player matchPlayer : matchedPlayers) {
-            final User player = ess.getUser(matchPlayer);
-            if (skipHidden && player.isHidden(sender.getPlayer()) && !sender.getPlayer().canSee(matchPlayer)) {
-                continue;
-            }
-            foundUser = true;
-            player.getBase().setGameMode(gameMode);
-            sender.sendMessage(tl("gameMode", tl(player.getBase().getGameMode().toString().toLowerCase(Locale.ENGLISH)), player.getDisplayName()));
-        }
-        if (!foundUser) {
-            throw new PlayerNotFoundException();
-        }
+        user.getBase().setGameMode(gameMode);
+        sender.sendMessage(tl("gameMode", tl(gameMode.toString().toLowerCase(Locale.ENGLISH)), user.getDisplayName()));
     }
 
     // essentials.gamemode will let them change to any but essentials.gamemode.survival would only let them change to survival.
-    private boolean canChangeToMode(User user, GameMode to) {
-        return user.isAuthorized("essentials.gamemode.all") || user.isAuthorized("essentials.gamemode." + to.name().toLowerCase());
+    private boolean isProhibitedChange(IUser user, GameMode to) {
+        return user != null && !user.isAuthorized("essentials.gamemode.all") && !user.isAuthorized("essentials.gamemode." + to.name().toLowerCase());
     }
 
     private GameMode matchGameMode(String modeString) throws NotEnoughArgumentsException {
@@ -109,11 +91,9 @@ public class Commandgamemode extends EssentialsCommand {
             mode = GameMode.SURVIVAL;
         } else if (modeString.equalsIgnoreCase("gma") || modeString.equalsIgnoreCase("egma") || modeString.contains("advent") || modeString.equalsIgnoreCase("2") || modeString.equalsIgnoreCase("a")) {
             mode = GameMode.ADVENTURE;
-        } else if (modeString.equalsIgnoreCase("gmt") || modeString.equalsIgnoreCase("egmt") || modeString.contains("toggle") || modeString.contains("cycle") || modeString.equalsIgnoreCase("t")) {
-            mode = null;
         } else if (modeString.equalsIgnoreCase("gmsp") || modeString.equalsIgnoreCase("egmsp") || modeString.contains("spec") || modeString.equalsIgnoreCase("3") || modeString.equalsIgnoreCase("sp")) {
             mode = GameMode.SPECTATOR;
-        } else {
+        } else if (!modeString.equalsIgnoreCase("gmt") && !modeString.equalsIgnoreCase("egmt") && !modeString.contains("toggle") && !modeString.contains("cycle") && !modeString.equalsIgnoreCase("t")) {
             throw new NotEnoughArgumentsException();
         }
         return mode;
@@ -158,5 +138,10 @@ public class Commandgamemode extends EssentialsCommand {
         } else {
             return Collections.emptyList();
         }
+    }
+
+    @Override
+    protected void updatePlayer(Server server, CommandSource sender, User user, String[] args) {
+
     }
 }
