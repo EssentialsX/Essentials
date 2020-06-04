7d3560089116e516543a6c78311e8d68e29b0d66
aski6 <aski6@gmx.com>
2020-06-04 12:52:57 +0100

    Remove debug print statement.

--- Essentials/src/com/earth2me/essentials/UserData.java
+++ Essentials/src/com/earth2me/essentials/UserData.java
@@ -553,7 +553,6 @@ public abstract class UserData extends PlayerExtension implements IConf {
 
     public boolean isIgnoredPlayer(IUser user) {
 	UUID uuid = user.getBase().getUniqueId();
-	System.out.println(uuid);
 	//If there is a timeout stored for the player, check if expired, if so set ingored status to false.
 	Long timeout = getIgnoredPlayerTimeout(uuid);
 	if (timeout != null) {
