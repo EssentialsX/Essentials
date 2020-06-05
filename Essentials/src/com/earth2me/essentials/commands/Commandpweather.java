3a6a2eae7dd99e253216d93b407b990931255787
JRoy <joshroy126@gmail.com>
2020-06-05 15:21:31 -0400

    Refactor ptime and pweather
    These were the worst commands i've seen

--- Essentials/src/com/earth2me/essentials/commands/Commandpweather.java
+++ Essentials/src/com/earth2me/essentials/commands/Commandpweather.java
@@ -1,27 +1,21 @@
 package com.earth2me.essentials.commands;
 
-import com.google.common.collect.Lists;
-
 import com.earth2me.essentials.CommandSource;
+import com.earth2me.essentials.IUser;
 import com.earth2me.essentials.User;
+import com.google.common.collect.Lists;
 import org.bukkit.Server;
 import org.bukkit.WeatherType;
-import org.bukkit.entity.Player;
 
 import java.util.*;
 
 import static com.earth2me.essentials.I18n.tl;
+import static com.earth2me.essentials.commands.Commandptime.getAliases;
 
-
-public class Commandpweather extends EssentialsCommand {
-    public static final Set<String> getAliases = new HashSet<>();
+public class Commandpweather extends EssentialsLoopCommand {
     public static final Map<String, WeatherType> weatherAliases = new HashMap<>();
 
     static {
-        getAliases.add("get");
-        getAliases.add("list");
-        getAliases.add("show");
-        getAliases.add("display");
         weatherAliases.put("sun", WeatherType.CLEAR);
         weatherAliases.put("clear", WeatherType.CLEAR);
         weatherAliases.put("storm", WeatherType.DOWNFALL);
@@ -34,123 +28,74 @@ public class Commandpweather extends EssentialsCommand {
 
     @Override
     public void run(final Server server, final CommandSource sender, final String commandLabel, final String[] args) throws Exception {
-        // Which Players(s) / Users(s) are we interested in?
-        String userSelector = null;
-        if (args.length == 2) {
-            userSelector = args[1];
-        }
-        Set<User> users = getUsers(server, sender, userSelector);
-
-        if (args.length == 0) {
-            getUsersWeather(sender, users);
-            return;
-        }
-
-        if (getAliases.contains(args[0])) {
-            getUsersWeather(sender, users);
-            return;
-        }
-
-        if (sender.isPlayer()) {
-            User user = ess.getUser(sender.getPlayer());
-            if (user != null && (!users.contains(user) || users.size() > 1) && !user.isAuthorized("essentials.pweather.others")) {
-                user.sendMessage(tl("pWeatherOthersPermission"));
+        if (args.length == 0 || getAliases.contains(args[0].toLowerCase())) {
+            if (args.length > 1) { // /pweather get md_5 || /pweather get *
+                if (args[1].equals("*") || args[1].equals("**")) {
+                    sender.sendMessage(tl("pWeatherPlayers"));
+                }
+                loopOnlinePlayersConsumer(server, sender, false, true, args[1], player -> getUserWeather(sender, player));
                 return;
             }
-        }
 
-        setUsersWeather(sender, users, args[0].toLowerCase());
-    }
-
-    /**
-     * Used to get the time and inform
-     */
-    private void getUsersWeather(final CommandSource sender, final Collection<User> users) {
-        if (users.size() > 1) {
-            sender.sendMessage(tl("pWeatherPlayers"));
-        }
-
-        for (User user : users) {
-            if (user.getBase().getPlayerWeather() == null) {
-                sender.sendMessage(tl("pWeatherNormal", user.getName()));
-            } else {
-                sender.sendMessage(tl("pWeatherCurrent", user.getName(), user.getBase().getPlayerWeather().toString().toLowerCase(Locale.ENGLISH)));
-            }
-        }
-    }
-
-    /**
-     * Used to set the time and inform of the change
-     */
-    private void setUsersWeather(final CommandSource sender, final Collection<User> users, final String weatherType) throws Exception {
-
-        final StringBuilder msg = new StringBuilder();
-        for (User user : users) {
-            if (msg.length() > 0) {
-                msg.append(", ");
-            }
-
-            msg.append(user.getName());
-        }
-
-        if (weatherType.equalsIgnoreCase("reset")) {
-            for (User user : users) {
-                user.getBase().resetPlayerWeather();
-            }
-
-            sender.sendMessage(tl("pWeatherReset", msg));
-        } else {
-            if (!weatherAliases.containsKey(weatherType)) {
-                throw new NotEnoughArgumentsException(tl("pWeatherInvalidAlias"));
-            }
-
-            for (User user : users) {
-                user.getBase().setPlayerWeather(weatherAliases.get(weatherType));
-            }
-            sender.sendMessage(tl("pWeatherSet", weatherType, msg.toString()));
-        }
-    }
-
-    /**
-     * Used to parse an argument of the type "users(s) selector"
-     */
-    private Set<User> getUsers(final Server server, final CommandSource sender, final String selector) throws Exception {
-        final Set<User> users = new TreeSet<>(new UserNameComparator());
-        // If there is no selector we want the sender itself. Or all users if sender isn't a user.
-        if (selector == null) {
-            if (sender.isPlayer()) {
-                final User user = ess.getUser(sender.getPlayer());
-                users.add(user);
-            } else {
-                for (User user : ess.getOnlineUsers()) {
-                    users.add(user);
+            if (args.length == 1 || sender.isPlayer()) { // /pweather get
+                if (sender.isPlayer()) {
+                    getUserWeather(sender, sender.getUser(ess));
+                    return;
                 }
+                throw new NotEnoughArgumentsException(); // We cannot imply the target for console
             }
-            return users;
-        }
 
-        // Try to find the user with name = selector
-        User user = null;
-        final List<Player> matchedPlayers = server.matchPlayer(selector);
-        if (!matchedPlayers.isEmpty()) {
-            user = ess.getUser(matchedPlayers.get(0));
-        }
-
-        if (user != null) {
-            users.add(user);
-        }
-        // If that fails, Is the argument something like "*" or "all"?
-        else if (selector.equalsIgnoreCase("*") || selector.equalsIgnoreCase("all")) {
-            for (User u : ess.getOnlineUsers()) {
-                users.add(u);
+            // Default to showing the weather of all online users for console when no arguments are provided
+            if (ess.getOnlinePlayers().size() > 1) {
+                sender.sendMessage(tl("pWeatherPlayers"));
+            }
+            for (User player : ess.getOnlineUsers()) {
+                getUserWeather(sender, player);
             }
         }
-        // We failed to understand the world target...
-        else {
-            throw new PlayerNotFoundException();
+
+        if (args.length > 1 && !sender.isAuthorized("essentials.pweather.others", ess) && !args[1].equalsIgnoreCase(sender.getSelfSelector())) {
+            sender.sendMessage(tl("pWeatherOthersPermission"));
+            return;
         }
 
-        return users;
+        String weather = args[0].toLowerCase();
+        if (!weatherAliases.containsKey(weather) && !weather.equalsIgnoreCase("reset")) {
+            throw new NotEnoughArgumentsException(tl("pWeatherInvalidAlias"));
+        }
+
+        StringJoiner joiner = new StringJoiner(",");
+        loopOnlinePlayersConsumer(server, sender, false, true, args.length > 1 ? args[1] : sender.getSelfSelector(), player -> {
+            setUserWeather(player, weather);
+            joiner.add(player.getName());
+        });
+
+        if (weather.equalsIgnoreCase("reset")) {
+            sender.sendMessage(tl("pWeatherReset", joiner.toString()));
+            return;
+        }
+
+        sender.sendMessage(tl("pWeatherSet", weather, joiner.toString()));
+    }
+
+    private void getUserWeather(CommandSource sender, IUser user) {
+        if (user == null) {
+            return;
+        }
+
+        if (user.getBase().getPlayerWeather() == null) {
+            sender.sendMessage(tl("pWeatherNormal", user.getName()));
+            return;
+        }
+        sender.sendMessage(tl("pWeatherCurrent", user.getName(), user.getBase().getPlayerWeather().toString().toLowerCase(Locale.ENGLISH)));
+    }
+
+    private void setUserWeather(User user, String weatherType) {
+        if (weatherType.equalsIgnoreCase("reset")) {
+            user.getBase().resetPlayerWeather();
+            return;
+        }
+        user.getBase().setPlayerWeather(weatherAliases.get(weatherType));
     }
 
     @Override
@@ -162,5 +107,9 @@ public class Commandpweather extends EssentialsCommand {
         } else {
             return Collections.emptyList();
         }
+    }
+
+    @Override
+    protected void updatePlayer(Server server, CommandSource sender, User user, String[] args) {
     }
 }
