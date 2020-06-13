2edbace62136538b19c36819919ce408ceba4c26
pop4959 <pop4959@gmail.com>
2020-06-13 10:34:12 -0700

    Apply review changes

--- Essentials/src/com/earth2me/essentials/Essentials.java
+++ Essentials/src/com/earth2me/essentials/Essentials.java
@@ -410,16 +410,9 @@ public class Essentials extends JavaPlugin implements net.ess3.api.IEssentials {
         i18n.updateLocale(settings.getLocale());
         for (String commandName : this.getDescription().getCommands().keySet()) {
             Command command = this.getCommand(commandName);
-            if (command == null) {
-                continue;
-            }
-            String tlDescription = tl(commandName + "CommandDescription");
-            if (!tlDescription.isEmpty()) {
-                command.setDescription(tlDescription);
-            }
-            String tlUsage = tl(commandName + "CommandUsage");
-            if (!tlUsage.isEmpty()) {
-                command.setUsage(tlUsage);
+            if (command != null) {
+                command.setDescription(tl(commandName + "CommandDescription"));
+                command.setUsage(tl(commandName + "CommandUsage"));
             }
         }
 
