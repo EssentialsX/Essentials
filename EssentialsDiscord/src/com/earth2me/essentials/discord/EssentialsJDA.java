e104ef5aa91b8f8b2803e1ba02fe2d9bf0b6fd89
pop4959 <pop4959@gmail.com>
2020-06-21 11:57:22 -0700

    Process mentions (#4)

--- EssentialsDiscord/src/com/earth2me/essentials/discord/EssentialsJDA.java
+++ EssentialsDiscord/src/com/earth2me/essentials/discord/EssentialsJDA.java
@@ -3,14 +3,20 @@ package com.earth2me.essentials.discord;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.Emote;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.entities.TextChannel;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.event.HandlerList;
 
 import javax.security.auth.login.LoginException;
+import java.util.Objects;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.regex.Pattern;
 
 import static com.earth2me.essentials.I18n.tl;
 
@@ -19,6 +25,7 @@ public class EssentialsJDA {
     private static final Logger logger = Logger.getLogger("EssentialsDiscord");
 
     private JDA jda = null;
+    private Guild guild;
     private final EssentialsDiscord plugin;
 
     public EssentialsJDA(EssentialsDiscord plugin) {
@@ -41,6 +48,13 @@ public class EssentialsJDA {
                 .awaitReady();
         this.setActivity(plugin.getSettings().getStatusActivity(), plugin.getSettings().getStatusMessage());
         logger.log(Level.INFO, "Successfully logged in as " + jda.getSelfUser().getAsTag());
+        try {
+            this.guild = Objects.requireNonNull(this.jda.getGuildById(plugin.getSettings().getGuildId()));
+        } catch (NullPointerException e) {
+            logger.severe("No guild configured!");
+            Bukkit.getPluginManager().disablePlugin(plugin);
+            return;
+        }
         Bukkit.getServer().getPluginManager().registerEvents(new BukkitListener(plugin, this), plugin);
     }
 
@@ -54,8 +68,28 @@ public class EssentialsJDA {
             logger.log(Level.WARNING, tl("discordInvalidChannelId"));
             return;
         }
-        String messageStripped = ChatColor.stripColor(message);
-        channel.sendMessage(messageStripped).queue(success -> {
+        String msg = ChatColor.stripColor(message)
+                .replaceAll("@here", "@\u200bhere")
+                .replaceAll("@everyone", "@\u200beveryone");
+        if (msg.contains("#")) {
+            for (TextChannel textChannel : guild.getTextChannels()) {
+                msg = msg.replaceAll("#" + textChannel.getName(), textChannel.getAsMention());
+            }
+        }
+        if (Pattern.compile(":.+:").matcher(msg).find()) {
+            for (Emote emote : guild.getEmotes()) {
+                msg = msg.replaceAll(":" + emote.getName() + ":", emote.getAsMention());
+            }
+        }
+        if (msg.contains("@")) {
+            for (Role role : guild.getRoles()) {
+                msg = msg.replaceAll("@" + role.getName(), role.getAsMention());
+            }
+            for (Member member : guild.getMembers()) {
+                msg = msg.replaceAll("@" + member.getEffectiveName(), member.getAsMention());
+            }
+        }
+        channel.sendMessage(msg).queue(success -> {
         }, fail -> {
             if (plugin.getParent().getSettings().isDebug()) {
                 fail.printStackTrace();
