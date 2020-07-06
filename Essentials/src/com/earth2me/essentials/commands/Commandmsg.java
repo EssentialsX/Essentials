9681933ec23db6c62ac785519d1b06d4b8309dfe
Josh Roy <10731363+JRoy@users.noreply.github.com>
2020-07-06 14:52:51 -0400

    Add mute expiry time in mute messages (#3329)
    Co-authored-by: MD <1917406+md678685@users.noreply.github.com>
    
    Closes #1211.

--- Essentials/src/com/earth2me/essentials/commands/Commandmsg.java
+++ Essentials/src/com/earth2me/essentials/commands/Commandmsg.java
@@ -1,16 +1,16 @@
 package com.earth2me.essentials.commands;
 
-import static com.earth2me.essentials.I18n.tl;
-
 import com.earth2me.essentials.CommandSource;
 import com.earth2me.essentials.Console;
 import com.earth2me.essentials.User;
 import com.earth2me.essentials.messaging.IMessageRecipient;
+import com.earth2me.essentials.utils.DateUtil;
 import com.earth2me.essentials.utils.FormatUtil;
-
 import org.bukkit.Server;
 
 import java.util.List;
+
+import static com.earth2me.essentials.I18n.tl;
 
 public class Commandmsg extends EssentialsLoopCommand {
 
@@ -29,7 +29,11 @@ public class Commandmsg extends EssentialsLoopCommand {
         if (sender.isPlayer()) {
             User user = ess.getUser(sender.getPlayer());
             if (user.isMuted()) {
-                throw new Exception(user.hasMuteReason() ? tl("voiceSilencedReason", user.getMuteReason()) : tl("voiceSilenced"));
+                String dateDiff = user.getMuteTimeout() > 0 ? DateUtil.formatDateDiff(user.getMuteTimeout()) : null;
+                if (dateDiff == null) {
+                    throw new Exception(user.hasMuteReason() ? tl("voiceSilencedReason", user.getMuteReason()) : tl("voiceSilenced"));
+                }
+                throw new Exception(user.hasMuteReason() ? tl("voiceSilencedReasonTime", dateDiff, user.getMuteReason()) : tl("voiceSilencedTime", dateDiff));
             }
             message = FormatUtil.formatMessage(user, "essentials.msg", message);
             canWildcard = user.isAuthorized("essentials.msg.multiple");
