b0cfe64338bf12a3ea875a1644fa5eee9cf79df1
Josh Roy <10731363+JRoy@users.noreply.github.com>
2020-10-05 08:03:52 -0400

    Apply suggestions from code review
    Co-authored-by: MD <1917406+md678685@users.noreply.github.com>

--- providers/BaseProviders/src/net/ess3/provider/ContainerProvider.java
+++ providers/BaseProviders/src/net/ess3/provider/ContainerProvider.java
@@ -6,10 +6,15 @@ import org.bukkit.inventory.InventoryView;
 public interface ContainerProvider extends Provider {
 
     InventoryView openAnvil(Player player);
+
     InventoryView openCartographyTable(Player player);
+
     InventoryView openGrindstone(Player player);
+
     InventoryView openLoom(Player player);
+
     InventoryView openSmithingTable(Player player);
+
     InventoryView openStonecutter(Player player);
 
 }
