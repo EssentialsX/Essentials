5cd6a2175c2a73bd3eb65837510a42dd62d37dba
JRoy <joshroy126@gmail.com>
2020-11-16 11:54:33 -0500

    Convert project to Gradle

---
+++ Essentials/src/main/java/com/earth2me/essentials/utils/DescParseTickFormat.java
@@ -0,0 +1,254 @@
+package com.earth2me.essentials.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Set;
+import java.util.TimeZone;
+
+import static com.earth2me.essentials.I18n.tl;
+
+/**
+ * This utility class is used for converting between the ingame time in ticks to ingame time as a friendly string. Note
+ * that the time is INGAME.
+ * <p>
+ * http://www.minecraftwiki.net/wiki/Day/night_cycle
+ *
+ * @author Olof Larsson
+ */
+public final class DescParseTickFormat {
+    // ============================================
+    // First some information vars. TODO: Should this be in a config file?
+    // --------------------------------------------
+    public static final Map<String, Integer> nameToTicks = new LinkedHashMap<>();
+    public static final Set<String> resetAliases = new HashSet<>();
+    public static final int ticksAtMidnight = 18000;
+    public static final int ticksPerDay = 24000;
+    public static final int ticksPerHour = 1000;
+    public static final double ticksPerMinute = 1000d / 60d;
+    public static final double ticksPerSecond = 1000d / 60d / 60d;
+    private static final SimpleDateFormat SDFTwentyFour = new SimpleDateFormat("HH:mm", Locale.ENGLISH);
+    private static final SimpleDateFormat SDFTwelve = new SimpleDateFormat("h:mm aa", Locale.ENGLISH);
+
+    static {
+        SDFTwentyFour.setTimeZone(TimeZone.getTimeZone("GMT"));
+        SDFTwelve.setTimeZone(TimeZone.getTimeZone("GMT"));
+
+        nameToTicks.put("sunrise", 23000);
+        nameToTicks.put("dawn", 23000);
+
+        nameToTicks.put("daystart", 0);
+        nameToTicks.put("day", 0);
+
+        nameToTicks.put("morning", 1000);
+
+        nameToTicks.put("midday", 6000);
+        nameToTicks.put("noon", 6000);
+
+        nameToTicks.put("afternoon", 9000);
+
+        nameToTicks.put("sunset", 12000);
+        nameToTicks.put("dusk", 12000);
+        nameToTicks.put("sundown", 12000);
+        nameToTicks.put("nightfall", 12000);
+
+        nameToTicks.put("nightstart", 14000);
+        nameToTicks.put("night", 14000);
+
+        nameToTicks.put("midnight", 18000);
+
+        resetAliases.add("reset");
+        resetAliases.add("normal");
+        resetAliases.add("default");
+    }
+
+    private DescParseTickFormat() {
+    }
+
+    // ============================================
+    public static long parse(String desc) throws NumberFormatException {
+        // Only look at alphanumeric and lowercase and : for 24:00
+        desc = desc.toLowerCase(Locale.ENGLISH).replaceAll("[^A-Za-z0-9:]", "");
+
+        // Detect ticks format
+        try {
+            return parseTicks(desc);
+        } catch (final NumberFormatException ignored) {
+        }
+
+        // Detect 24-hour format
+        try {
+            return parse24(desc);
+        } catch (final NumberFormatException ignored) {
+        }
+
+        // Detect 12-hour format
+        try {
+            return parse12(desc);
+        } catch (final NumberFormatException ignored) {
+        }
+
+        // Detect aliases
+        try {
+            return parseAlias(desc);
+        } catch (final NumberFormatException ignored) {
+        }
+
+        // Well we failed to understand...
+        throw new NumberFormatException();
+    }
+
+    public static long parseTicks(String desc) throws NumberFormatException {
+        if (!desc.matches("^[0-9]+ti?c?k?s?$")) {
+            throw new NumberFormatException();
+        }
+
+        desc = desc.replaceAll("[^0-9]", "");
+
+        return Long.parseLong(desc) % 24000;
+    }
+
+    public static long parse24(String desc) throws NumberFormatException {
+        if (!desc.matches("^[0-9]{2}[^0-9]?[0-9]{2}$")) {
+            throw new NumberFormatException();
+        }
+
+        desc = desc.toLowerCase(Locale.ENGLISH).replaceAll("[^0-9]", "");
+
+        if (desc.length() != 4) {
+            throw new NumberFormatException();
+        }
+
+        final int hours = Integer.parseInt(desc.substring(0, 2));
+        final int minutes = Integer.parseInt(desc.substring(2, 4));
+
+        return hoursMinutesToTicks(hours, minutes);
+    }
+
+    public static long parse12(String desc) throws NumberFormatException {
+        if (!desc.matches("^[0-9]{1,2}([^0-9]?[0-9]{2})?(pm|am)$")) {
+            throw new NumberFormatException();
+        }
+
+        int hours = 0;
+        int minutes = 0;
+
+        desc = desc.toLowerCase(Locale.ENGLISH);
+        final String parsetime = desc.replaceAll("[^0-9]", "");
+
+        if (parsetime.length() > 4) {
+            throw new NumberFormatException();
+        }
+
+        if (parsetime.length() == 4) {
+            hours += Integer.parseInt(parsetime.substring(0, 2));
+            minutes += Integer.parseInt(parsetime.substring(2, 4));
+        } else if (parsetime.length() == 3) {
+            hours += Integer.parseInt(parsetime.substring(0, 1));
+            minutes += Integer.parseInt(parsetime.substring(1, 3));
+        } else if (parsetime.length() == 2) {
+            hours += Integer.parseInt(parsetime.substring(0, 2));
+        } else if (parsetime.length() == 1) {
+            hours += Integer.parseInt(parsetime.substring(0, 1));
+        } else {
+            throw new NumberFormatException();
+        }
+
+        if (desc.endsWith("pm") && hours != 12) {
+            hours += 12;
+        }
+
+        if (desc.endsWith("am") && hours == 12) {
+            hours -= 12;
+        }
+
+        return hoursMinutesToTicks(hours, minutes);
+    }
+
+    public static long hoursMinutesToTicks(final int hours, final int minutes) {
+        long ret = ticksAtMidnight;
+        ret += hours * ticksPerHour;
+
+        ret += (minutes / 60.0) * ticksPerHour;
+
+        ret %= ticksPerDay;
+        return ret;
+    }
+
+    public static long parseAlias(final String desc) throws NumberFormatException {
+        final Integer ret = nameToTicks.get(desc);
+        if (ret == null) {
+            throw new NumberFormatException();
+        }
+
+        return ret;
+    }
+
+    public static boolean meansReset(final String desc) {
+        return resetAliases.contains(desc);
+    }
+
+    // ============================================
+    public static String format(final long ticks) {
+        return tl("timeFormat", format24(ticks), format12(ticks), formatTicks(ticks));
+    }
+
+    public static String formatTicks(final long ticks) {
+        return (ticks % ticksPerDay) + " ticks";
+    }
+
+    public static String format24(final long ticks) {
+        synchronized (SDFTwentyFour) {
+            return formatDateFormat(ticks, SDFTwentyFour);
+        }
+    }
+
+    public static String format12(final long ticks) {
+        synchronized (SDFTwelve) {
+            return formatDateFormat(ticks, SDFTwelve);
+        }
+    }
+
+    public static String formatDateFormat(final long ticks, final SimpleDateFormat format) {
+        final Date date = ticksToDate(ticks);
+        return format.format(date);
+    }
+
+    public static Date ticksToDate(long ticks) {
+        // Assume the server time starts at 0. It would start on a day.
+        // But we will simulate that the server started with 0 at midnight.
+        ticks = ticks - ticksAtMidnight + ticksPerDay;
+
+        // How many ingame days have passed since the server start?
+        final long days = ticks / ticksPerDay;
+        ticks -= days * ticksPerDay;
+
+        // How many hours on the last day?
+        final long hours = ticks / ticksPerHour;
+        ticks -= hours * ticksPerHour;
+
+        // How many minutes on the last day?
+        final long minutes = (long) Math.floor(ticks / ticksPerMinute);
+        final double dticks = ticks - minutes * ticksPerMinute;
+
+        // How many seconds on the last day?
+        final long seconds = (long) Math.floor(dticks / ticksPerSecond);
+
+        // Now we create an english GMT calendar (We wan't no daylight savings)
+        final Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("GMT"), Locale.ENGLISH);
+        cal.setLenient(true);
+
+        // And we set the time to 0! And append the time that passed!
+        cal.set(0, Calendar.JANUARY, 1, 0, 0, 0);
+        cal.add(Calendar.DAY_OF_YEAR, (int) days);
+        cal.add(Calendar.HOUR_OF_DAY, (int) hours);
+        cal.add(Calendar.MINUTE, (int) minutes);
+        cal.add(Calendar.SECOND, (int) seconds + 1); // To solve rounding errors.
+
+        return cal.getTime();
+    }
+}
---
+++ EssentialsProtect/src/main/java/com/earth2me/essentials/protect/EssentialsProtectEntityListener.java
@@ -0,0 +1,258 @@
+package com.earth2me.essentials.protect;
+
+import com.earth2me.essentials.User;
+import net.ess3.api.IEssentials;
+import org.bukkit.entity.Creeper;
+import org.bukkit.entity.EnderCrystal;
+import org.bukkit.entity.EnderDragon;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Fireball;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
+import org.bukkit.entity.SmallFireball;
+import org.bukkit.entity.TNTPrimed;
+import org.bukkit.entity.Villager;
+import org.bukkit.entity.Wither;
+import org.bukkit.entity.WitherSkull;
+import org.bukkit.entity.minecart.ExplosiveMinecart;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.EntityBreakDoorEvent;
+import org.bukkit.event.entity.EntityChangeBlockEvent;
+import org.bukkit.event.entity.EntityDamageByBlockEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.event.entity.EntityTargetEvent;
+import org.bukkit.event.entity.EntityTargetEvent.TargetReason;
+import org.bukkit.event.entity.ExplosionPrimeEvent;
+import org.bukkit.event.hanging.HangingBreakByEntityEvent;
+import org.bukkit.event.hanging.HangingBreakEvent;
+
+import java.util.Locale;
+
+public class EssentialsProtectEntityListener implements Listener {
+    private final IProtect prot;
+    private final IEssentials ess;
+
+    EssentialsProtectEntityListener(final IProtect prot) {
+        this.prot = prot;
+        this.ess = prot.getEssentialsConnect().getEssentials();
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onEntityDamage(final EntityDamageEvent event) {
+        final Entity target = event.getEntity();
+
+        if (target instanceof Villager && prot.getSettingBool(ProtectConfig.prevent_villager_death)) {
+            event.setCancelled(true);
+            return;
+        }
+
+        User user = null;
+        if (target instanceof Player) {
+            user = ess.getUser((Player) target);
+        }
+
+        final DamageCause cause = event.getCause();
+
+        if (event instanceof EntityDamageByBlockEvent) {
+            if (prot.getSettingBool(ProtectConfig.disable_contactdmg) && cause == DamageCause.CONTACT && !(target instanceof Player && shouldBeDamaged(user, "contact"))) {
+                event.setCancelled(true);
+                return;
+            }
+            if (prot.getSettingBool(ProtectConfig.disable_lavadmg) && cause == DamageCause.LAVA && !(target instanceof Player && shouldBeDamaged(user, "lava"))) {
+                event.setCancelled(true);
+                return;
+            }
+            if (prot.getSettingBool(ProtectConfig.prevent_tnt_explosion) && cause == DamageCause.BLOCK_EXPLOSION && !(target instanceof Player && shouldBeDamaged(user, "tnt"))) {
+                event.setCancelled(true);
+                return;
+            }
+        }
+
+        if (event instanceof EntityDamageByEntityEvent) {
+            final EntityDamageByEntityEvent edEvent = (EntityDamageByEntityEvent) event;
+            final Entity eAttack = edEvent.getDamager();
+
+            User attacker = null;
+            if (eAttack instanceof Player) {
+                attacker = ess.getUser((Player) eAttack);
+            }
+
+            //Creeper explode prevention
+            if (eAttack instanceof Creeper && prot.getSettingBool(ProtectConfig.prevent_creeper_playerdmg) && !(target instanceof Player && shouldBeDamaged(user, "creeper"))) {
+                event.setCancelled(true);
+                return;
+            }
+
+            if ((event.getEntity() instanceof Fireball || event.getEntity() instanceof SmallFireball) && prot.getSettingBool(ProtectConfig.prevent_fireball_playerdmg) && !(target instanceof Player && shouldBeDamaged(user, "fireball"))) {
+                event.setCancelled(true);
+                return;
+            }
+
+            if (event.getEntity() instanceof WitherSkull && prot.getSettingBool(ProtectConfig.prevent_witherskull_playerdmg) && !(target instanceof Player && shouldBeDamaged(user, "witherskull"))) {
+                event.setCancelled(true);
+                return;
+            }
+
+            if (eAttack instanceof TNTPrimed && prot.getSettingBool(ProtectConfig.prevent_tnt_playerdmg) && !(target instanceof Player && shouldBeDamaged(user, "tnt"))) {
+                event.setCancelled(true);
+                return;
+            }
+
+            if (eAttack instanceof ExplosiveMinecart && prot.getSettingBool(ProtectConfig.prevent_tntminecart_playerdmg) && !(target instanceof Player && shouldBeDamaged(user, "tnt-minecart"))) {
+                event.setCancelled(true);
+                return;
+            }
+
+            // PVP Settings
+            if (target instanceof Player && eAttack instanceof Player && prot.getSettingBool(ProtectConfig.disable_pvp) && !user.getName().equalsIgnoreCase(attacker.getName()) && (!user.isAuthorized("essentials.protect.pvp") || !attacker.isAuthorized("essentials.protect.pvp"))) {
+                event.setCancelled(true);
+                return;
+            }
+
+            if (edEvent.getDamager() instanceof Projectile && target instanceof Player && ((prot.getSettingBool(ProtectConfig.disable_projectiles) && !shouldBeDamaged(user, "projectiles")) || (((Projectile) edEvent.getDamager()).getShooter() instanceof Player && prot.getSettingBool(ProtectConfig.disable_pvp) && (!user.isAuthorized("essentials.protect.pvp") || !ess.getUser((Player) ((Projectile) edEvent.getDamager()).getShooter()).isAuthorized("essentials.protect.pvp"))))) {
+                event.setCancelled(true);
+                return;
+            }
+        }
+
+        if (target instanceof Player) {
+            if (cause == DamageCause.FALL && prot.getSettingBool(ProtectConfig.disable_fall) && !shouldBeDamaged(user, "fall")) {
+                event.setCancelled(true);
+                return;
+            }
+
+            if (cause == DamageCause.SUFFOCATION && prot.getSettingBool(ProtectConfig.disable_suffocate) && !shouldBeDamaged(user, "suffocation")) {
+                event.setCancelled(true);
+                return;
+            }
+            if ((cause == DamageCause.FIRE || cause == DamageCause.FIRE_TICK) && prot.getSettingBool(ProtectConfig.disable_firedmg) && !shouldBeDamaged(user, "fire")) {
+                event.setCancelled(true);
+                return;
+            }
+            if (cause == DamageCause.DROWNING && prot.getSettingBool(ProtectConfig.disable_drown) && !shouldBeDamaged(user, "drowning")) {
+                event.setCancelled(true);
+                return;
+            }
+            if (cause == DamageCause.LIGHTNING && prot.getSettingBool(ProtectConfig.disable_lightning) && !shouldBeDamaged(user, "lightning")) {
+                event.setCancelled(true);
+                return;
+            }
+            if (cause == DamageCause.WITHER && prot.getSettingBool(ProtectConfig.disable_wither) && !shouldBeDamaged(user, "wither")) {
+                event.setCancelled(true);
+            }
+        }
+    }
+
+    private boolean shouldBeDamaged(final User user, final String type) {
+        return user.isAuthorized("essentials.protect.damage.".concat(type)) && !user.isAuthorized("essentials.protect.damage.disable");
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onEntityExplode(final EntityExplodeEvent event) {
+        if (event.getEntity() == null) {
+            return;
+        }
+        final Entity entity = event.getEntity();
+        final int maxHeight = ess.getSettings().getProtectCreeperMaxHeight();
+
+        if (entity instanceof EnderDragon && prot.getSettingBool(ProtectConfig.prevent_enderdragon_blockdmg)) {
+            event.setCancelled(true);
+            if (prot.getSettingBool(ProtectConfig.enderdragon_fakeexplosions)) {
+                event.getLocation().getWorld().createExplosion(event.getLocation(), 0F);
+            }
+            return;
+        }
+        if (entity instanceof Wither && prot.getSettingBool(ProtectConfig.prevent_wither_spawnexplosion)) {
+            event.setCancelled(true);
+        } else if (entity instanceof Creeper && (prot.getSettingBool(ProtectConfig.prevent_creeper_explosion) || prot.getSettingBool(ProtectConfig.prevent_creeper_blockdmg) || (maxHeight >= 0 && event.getLocation().getBlockY() > maxHeight))) {
+            //Nicccccccccce plaaacccccccccce..
+            event.setCancelled(true);
+            event.getLocation().getWorld().createExplosion(event.getLocation(), 0F);
+        } else if (entity instanceof TNTPrimed && prot.getSettingBool(ProtectConfig.prevent_tnt_explosion)) {
+            event.setCancelled(true);
+
+        } else if (entity instanceof Fireball && prot.getSettingBool(ProtectConfig.prevent_fireball_explosion)) {
+            event.setCancelled(true);
+
+        } else if ((entity instanceof WitherSkull) && prot.getSettingBool(ProtectConfig.prevent_witherskull_explosion)) {
+            event.setCancelled(true);
+        } else if ((entity instanceof ExplosiveMinecart) && prot.getSettingBool(ProtectConfig.prevent_tntminecart_explosion)) {
+            event.setCancelled(true);
+        } else if (entity instanceof EnderCrystal && prot.getSettingBool(ProtectConfig.prevent_ender_crystal_explosion)) {
+            event.setCancelled(true);
+        }
+
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onCreatureSpawn(final CreatureSpawnEvent event) {
+        if (event.getEntity() instanceof Player) {
+            return;
+        }
+        final EntityType creature = event.getEntityType();
+        if (creature == null) {
+            return;
+        }
+        final String creatureName = creature.toString().toLowerCase(Locale.ENGLISH);
+        if (creatureName.isEmpty()) {
+            return;
+        }
+        if (ess.getSettings().getProtectPreventSpawn(creatureName)) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onEntityTarget(final EntityTargetEvent event) {
+        if (!(event.getTarget() instanceof Player)) {
+            return;
+        }
+        final User user = ess.getUser((Player) event.getTarget());
+        if ((event.getReason() == TargetReason.CLOSEST_PLAYER || event.getReason() == TargetReason.TARGET_ATTACKED_ENTITY || event.getReason() == TargetReason.TARGET_ATTACKED_NEARBY_ENTITY || event.getReason() == TargetReason.RANDOM_TARGET || event.getReason() == TargetReason.DEFEND_VILLAGE || event.getReason() == TargetReason.TARGET_ATTACKED_OWNER || event.getReason() == TargetReason.OWNER_ATTACKED_TARGET) && prot.getSettingBool(ProtectConfig.prevent_entitytarget) && !user.isAuthorized("essentials.protect.entitytarget.bypass")) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onExplosionPrime(final ExplosionPrimeEvent event) {
+        if ((event.getEntity() instanceof Fireball || event.getEntity() instanceof SmallFireball) && prot.getSettingBool(ProtectConfig.prevent_fireball_fire)) {
+            event.setFire(false);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onEntityChangeBlock(final EntityChangeBlockEvent event) {
+        if (event.getEntityType() == EntityType.ENDERMAN && prot.getSettingBool(ProtectConfig.prevent_enderman_pickup)) {
+            event.setCancelled(true);
+            return;
+        }
+        if (event.getEntityType() == EntityType.WITHER && prot.getSettingBool(ProtectConfig.prevent_wither_blockreplace)) {
+            event.setCancelled(true);
+        }
+
+        if (event.getEntityType() == EntityType.SHEEP && prot.getSettingBool(ProtectConfig.prevent_sheep_eat_grass)) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onDoorBreak(final EntityBreakDoorEvent event) {
+        if (event.getEntityType() == EntityType.ZOMBIE && prot.getSettingBool(ProtectConfig.prevent_zombie_door_break)) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onPaintingBreak(final HangingBreakByEntityEvent event) {
+        if ((event.getCause() == HangingBreakEvent.RemoveCause.ENTITY) && ((event.getRemover() instanceof Creeper) && prot.getSettingBool(ProtectConfig.prevent_creeper_explosion) || (((event.getRemover() instanceof Fireball) || (event.getRemover() instanceof SmallFireball)) && prot.getSettingBool(ProtectConfig.prevent_fireball_explosion)) || ((event.getRemover() instanceof TNTPrimed) && prot.getSettingBool(ProtectConfig.prevent_tnt_explosion)) || ((event.getRemover() instanceof WitherSkull) && prot.getSettingBool(ProtectConfig.prevent_witherskull_explosion)))) {
+            event.setCancelled(true);
+        }
+    }
+}
