e1e98f504851c8464c549da0b337a71d120b6a06
Olivia <chew@chew.pw>
2020-12-11 10:38:10 -0600

    Add /warpinfo command (#3820)
    Co-authored-by: Josh Roy <10731363+JRoy@users.noreply.github.com>
    Co-authored-by: MD <1917406+mdcfe@users.noreply.github.com>

---
+++ Essentials/src/main/java/com/earth2me/essentials/commands/Commandwarpinfo.java
@@ -0,0 +1,47 @@
+package com.earth2me.essentials.commands;
+
+import com.earth2me.essentials.CommandSource;
+import org.bukkit.Location;
+import org.bukkit.Server;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static com.earth2me.essentials.I18n.tl;
+
+public class Commandwarpinfo extends EssentialsCommand {
+
+    public Commandwarpinfo() {
+        super("warpinfo");
+    }
+
+    @Override
+    public void run(final Server server, final CommandSource sender, final String commandLabel, final String[] args) throws Exception {
+        if (args.length == 0) {
+            throw new NotEnoughArgumentsException();
+        }
+        final String name = args[0];
+        final Location loc = ess.getWarps().getWarp(name);
+        sender.sendMessage(tl("warpInfo", name));
+        sender.sendMessage(tl("whoisLocation", loc.getWorld().getName(), loc.getBlockX(), loc.getBlockY(), loc.getBlockZ()));
+    }
+
+    @Override
+    protected List<String> getTabCompleteOptions(final Server server, final CommandSource sender, final String commandLabel, final String[] args) {
+        if (args.length == 1) {
+            if (ess.getSettings().getPerWarpPermission() && sender.isPlayer()) {
+                final List<String> list = new ArrayList<>();
+                for (String curWarp : ess.getWarps().getList()) {
+                    if (sender.isAuthorized("essentials.warps." + curWarp, ess)) {
+                        list.add(curWarp);
+                    }
+                }
+                return list;
+            }
+            return new ArrayList<>(ess.getWarps().getList());
+        } else {
+            return Collections.emptyList();
+        }
+    }
+}
