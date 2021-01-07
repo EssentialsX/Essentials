0fd75e78c03688b8f82da050bbb9f0c578486725
JRoy <joshroy126@gmail.com>
2021-01-07 09:56:26 -0500

    Add fake service manager for tests

--- Essentials/src/test/java/com/earth2me/essentials/FakeServer.java
+++ Essentials/src/test/java/com/earth2me/essentials/FakeServer.java
@@ -56,6 +56,8 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginLoader;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.RegisteredListener;
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.ServicePriority;
 import org.bukkit.plugin.ServicesManager;
 import org.bukkit.plugin.UnknownDependencyException;
 import org.bukkit.plugin.messaging.Messenger;
@@ -355,7 +357,57 @@ public class FakeServer implements Server {
 
     @Override
     public ServicesManager getServicesManager() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return new ServicesManager() {
+            @Override
+            public <T> void register(Class<T> service, T provider, Plugin plugin, ServicePriority priority) {
+
+            }
+
+            @Override
+            public void unregisterAll(Plugin plugin) {
+
+            }
+
+            @Override
+            public void unregister(Class<?> service, Object provider) {
+
+            }
+
+            @Override
+            public void unregister(Object provider) {
+
+            }
+
+            @Override
+            public <T> T load(Class<T> service) {
+                return null;
+            }
+
+            @Override
+            public <T> RegisteredServiceProvider<T> getRegistration(Class<T> service) {
+                return null;
+            }
+
+            @Override
+            public List<RegisteredServiceProvider<?>> getRegistrations(Plugin plugin) {
+                return null;
+            }
+
+            @Override
+            public <T> Collection<RegisteredServiceProvider<T>> getRegistrations(Class<T> service) {
+                return null;
+            }
+
+            @Override
+            public Collection<Class<?>> getKnownServices() {
+                return null;
+            }
+
+            @Override
+            public <T> boolean isProvidedFor(Class<T> service) {
+                return false;
+            }
+        };
     }
 
     @Override
