b0573bcfb0162e72949d219523dd69bbf5533a89
JRoy <joshroy126@gmail.com>
2021-01-23 18:40:45 -0500

    Add Service API

---
+++ Essentials/src/main/java/net/essentialsx/api/v2/services/EssentialsService.java
@@ -0,0 +1,12 @@
+package net.essentialsx.api.v2.services;
+
+/**
+ * Provides basic information about a service registered by EssentialsX.
+ */
+public interface EssentialsService {
+    /**
+     * Gets the name of this service.
+     * @return The name of the service.
+     */
+    String getName();
+}
