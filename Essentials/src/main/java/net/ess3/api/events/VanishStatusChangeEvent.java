6f0596099e581d903ae15daba8ea70a66a6e3776
DrBot7 <46543473+DrBot7@users.noreply.github.com>
2021-01-29 22:07:16 -0600

    api

--- Essentials/src/main/java/net/ess3/api/events/VanishStatusChangeEvent.java
+++ Essentials/src/main/java/net/ess3/api/events/VanishStatusChangeEvent.java
@@ -3,6 +3,8 @@ package net.ess3.api.events;
 import net.ess3.api.IUser;
 import org.bukkit.event.HandlerList;
 
+import static com.earth2me.essentials.I18n.tl;
+
 /**
  * Fired when a player's vanish status changes due to the /vanish command.
  * <p>
@@ -11,6 +13,8 @@ import org.bukkit.event.HandlerList;
  */
 public class VanishStatusChangeEvent extends StatusChangeEvent {
     private static final HandlerList handlers = new HandlerList();
+    private String fakeJoinMessage = null;
+    private String fakeLeaveMessage = null;
 
     public VanishStatusChangeEvent(final IUser affected, final IUser controller, final boolean value) {
         super(affected, controller, value);
@@ -23,5 +27,35 @@ public class VanishStatusChangeEvent extends StatusChangeEvent {
 
     public static HandlerList getHandlerList() {
         return handlers;
+    }
+
+    public boolean isFakeJoinMessage() {
+        return fakeJoinMessage != null;
+    }
+
+    public String getFakeJoinMessage() {
+        if (isFakeJoinMessage()) {
+            return tl("unvanishBroadcast", affected.getName(), affected.getDisplayName());
+        }
+        return null;
+    }
+
+    public void setFakeJoinMessage(String message) {
+        fakeJoinMessage = message;
+    }
+
+    public boolean isFakeLeaveMessage() {
+        return fakeLeaveMessage != null;
+    }
+
+    public String getFakeLeaveMessage() {
+        if (isFakeLeaveMessage()) {
+            return tl("vanishBroadcast", affected.getName(), affected.getDisplayName());
+        }
+        return null;
+    }
+
+    public void setFakeLeaveMessage(String message) {
+        fakeLeaveMessage = message;
     }
 }
