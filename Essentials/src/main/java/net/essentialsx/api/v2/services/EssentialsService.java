f8c3be7b58ac42196be358909763daf2dc79175c
JRoy <joshroy126@gmail.com>
2021-02-02 10:28:31 -0500

    Remove EssentialsService#getName

--- Essentials/src/main/java/net/essentialsx/api/v2/services/EssentialsService.java
+++ Essentials/src/main/java/net/essentialsx/api/v2/services/EssentialsService.java
@@ -4,9 +4,4 @@ package net.essentialsx.api.v2.services;
  * Provides basic information about a service registered by EssentialsX.
  */
 public interface EssentialsService {
-    /**
-     * Gets the name of this service.
-     * @return The name of the service.
-     */
-    String getName();
 }
