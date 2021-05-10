44356d0502ef89761026931e5dc796d4aca651fe
Josh Roy <10731363+JRoy@users.noreply.github.com>
2021-05-10 15:26:16 -0400

    Fix checkstyle

--- Essentials/src/main/java/com/earth2me/essentials/commands/EssentialsCommand.java
+++ Essentials/src/main/java/com/earth2me/essentials/commands/EssentialsCommand.java
@@ -23,7 +23,6 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.MissingResourceException;
-import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.logging.Level;
 import java.util.logging.Logger;
