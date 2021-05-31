bbcb0b55270842d5f345b470397fed3b1bd5d43f
Josh Roy <10731363+JRoy@users.noreply.github.com>
2021-05-31 18:52:08 -0400

    Remove unused interface

--- Essentials/src/main/java/net/ess3/api/ISpawnStorage.java
+++
@@ -1,9 +0,0 @@
-package net.ess3.api;
-
-import com.earth2me.essentials.IConf;
-
-/**
- * This interface contains basic API functions for Essentials Spawn.
- */
-public interface ISpawnStorage extends IConf {
-}
