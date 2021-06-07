59a6b182a00400296a192ab09db453b736f3b77c
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2021-06-07 02:47:03 -0700

    Clean up build scripts using convention plugins and add run tasks using the Run Paper Gradle plugin
    Each module has it's own run task (i.e. './gradlew :EssentialsXChat:runServer'), which will run itself and the main EssX module.
    
    The parent has a 'runServer' task which has a property to specify which modules are included (ex. './gradlew :runServer -PrunModules=chat,spawn'), as well as a 'runAll' task which includes all modules.

---
+++ build-logic/src/main/groovy/GitUtil.groovy
@@ -0,0 +1,54 @@
+import net.kyori.indra.git.IndraGitExtension
+import org.eclipse.jgit.lib.Ref
+import org.eclipse.jgit.lib.Repository
+import org.eclipse.jgit.revwalk.RevWalk
+import org.gradle.api.Project
+
+final class GitUtil {
+    private GitUtil() {
+    }
+
+    static int commitsSinceLastTag(Project project) {
+        def indraGit = project.extensions.findByType(IndraGitExtension.class)
+        if (indraGit == null || !indraGit.isPresent() || indraGit.tags().isEmpty()) {
+            return -1
+        }
+        def tags = indraGit.tags()
+        def depth = 0
+        def walk = new RevWalk(indraGit.git().getRepository())
+        def commit = walk.parseCommit(indraGit.commit())
+        while (true) {
+            for (tag in tags) {
+                if (walk.parseCommit(tag.getLeaf().getObjectId()) == commit) {
+                    walk.dispose()
+                    // todo: is this call to close needed?
+                    // 'close() called when useCnt is already zero for Repository[Essentials/.git]'
+                    indraGit.git().close()
+                    return depth
+                }
+            }
+            depth++
+            commit = walk.parseCommit(commit.getParents()[0])
+        }
+    }
+
+    static String headBranchName(Project project) {
+        def indraGit = project.extensions.findByType(IndraGitExtension.class)
+        if (System.getenv("GITHUB_HEAD_REF") != null && !System.getenv("GITHUB_HEAD_REF").isEmpty()) {
+            return System.getenv("GITHUB_HEAD_REF")
+        } else if (System.getenv("GITHUB_REF") != null && !System.getenv("GITHUB_REF").isEmpty()) {
+            return System.getenv("GITHUB_REF").replaceFirst("refs/heads/", "")
+        }
+
+        if (!indraGit.isPresent()) {
+            return "detached-head"
+        }
+
+        Ref ref = indraGit.git().getRepository().exactRef('HEAD')?.target
+        if (ref == null) {
+            return "detached-head"
+        }
+
+        return Repository.shortenRefName(ref.name)
+    }
+}
