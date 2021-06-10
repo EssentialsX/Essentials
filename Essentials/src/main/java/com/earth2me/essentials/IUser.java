7d305e5c22ca7807da09b90c0af770c53ff36671
Josh Roy <10731363+JRoy@users.noreply.github.com>
2021-06-10 19:45:21 -0400

    Update Essentials/src/main/java/com/earth2me/essentials/IUser.java
    Co-authored-by: MD <1917406+mdcfe@users.noreply.github.com>

--- Essentials/src/main/java/com/earth2me/essentials/IUser.java
+++ Essentials/src/main/java/com/earth2me/essentials/IUser.java
@@ -238,7 +238,7 @@ public interface IUser {
 
     /**
      * Gets information about the most recent tpa request in the tpa queue of this {@link IUser}.
-     *
+     * <p>
      * The TPA Queue is Last-In-First-Out queue which stores all the active pending teleport
      * requests of this {@link IUser}. Timeout calculations are also done during the
      * iteration process of this method, ensuring that teleport requests made past the timeout
