eef6e4e06bfc9ac882cf719332f9c6a74806154b
Josh Roy <10731363+JRoy@users.noreply.github.com>
2021-06-11 10:46:48 -0400

    Add ice commmand #EasterEgg

---
+++ Essentials/src/main/java/com/earth2me/essentials/commands/Commandice.java
@@ -0,0 +1,47 @@
+package com.earth2me.essentials.commands;
+
+import com.earth2me.essentials.ChargeException;
+import com.earth2me.essentials.CommandSource;
+import com.earth2me.essentials.User;
+import com.earth2me.essentials.utils.VersionUtil;
+import net.ess3.api.IUser;
+import net.ess3.api.MaxMoneyException;
+import org.bukkit.Server;
+
+import static com.earth2me.essentials.I18n.tl;
+
+public class Commandice extends EssentialsLoopCommand {
+    public Commandice() {
+        super("ice");
+    }
+
+    @Override
+    protected void run(Server server, CommandSource sender, String commandLabel, String[] args) throws Exception {
+        if (VersionUtil.getServerBukkitVersion().isLowerThan(VersionUtil.v1_17_R01)) {
+            sender.sendMessage(tl("unsupportedFeature"));
+            return;
+        }
+
+        if (args.length == 0 && !sender.isPlayer()) {
+            throw new NotEnoughArgumentsException();
+        }
+
+        if (args.length > 0 && sender.isAuthorized("essentials.ice.others", ess)) {
+            loopOnlinePlayers(server, sender, false, true, args[0], null);
+            return;
+        }
+        //noinspection ConstantConditions
+        freezePlayer(sender.getUser(ess));
+    }
+
+    @Override
+    protected void updatePlayer(Server server, CommandSource sender, User user, String[] args) throws NotEnoughArgumentsException, PlayerExemptException, ChargeException, MaxMoneyException {
+        freezePlayer(user);
+        user.sendMessage(tl("iceOther"));
+    }
+
+    private void freezePlayer(final IUser user) {
+        user.getBase().setFreezeTicks(user.getBase().getMaxFreezeTicks());
+        user.sendMessage(tl("ice"));
+    }
+}
