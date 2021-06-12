0b847d68bf6db6139c040ed03d593fe03bfbd4ae
Debug <49997488+DebugOk@users.noreply.github.com>
2021-06-12 03:30:53 +0200

    Removed an import
    I added that because I thought it was needed.
    
    Spoiler alert, it wasn't.

--- Essentials/src/main/java/com/earth2me/essentials/commands/Commandbalancetop.java
+++ Essentials/src/main/java/com/earth2me/essentials/commands/Commandbalancetop.java
@@ -14,7 +14,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
-//import java.math.BigDecimal;
 import java.util.concurrent.CompletableFuture;
 
 import static com.earth2me.essentials.I18n.tl;
