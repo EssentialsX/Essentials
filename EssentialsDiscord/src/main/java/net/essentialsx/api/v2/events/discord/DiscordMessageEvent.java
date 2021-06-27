7a96843279f3ee5e9e83c947d810ed5ce908a630
Josh Roy <10731363+JRoy@users.noreply.github.com>
2021-06-27 00:12:24 -0400

    Group mention changes

--- EssentialsDiscord/src/main/java/net/essentialsx/api/v2/events/discord/DiscordMessageEvent.java
+++ EssentialsDiscord/src/main/java/net/essentialsx/api/v2/events/discord/DiscordMessageEvent.java
@@ -24,7 +24,7 @@ public class DiscordMessageEvent extends Event implements Cancellable {
     /**
      * @param type               The message type/destination of this event.
      * @param message            The raw message content of this event.
-     * @param allowGroupMentions If the message should allow the pinging of channels, users, or emotes.
+     * @param allowGroupMentions Whether or not the message should allow the pinging of roles, users, or emotes.
      */
     public DiscordMessageEvent(final MessageType type, final String message, final boolean allowGroupMentions) {
         this(type, message, allowGroupMentions, null, null, null);
@@ -33,7 +33,7 @@ public class DiscordMessageEvent extends Event implements Cancellable {
     /**
      * @param type               The message type/destination of this event.
      * @param message            The raw message content of this event.
-     * @param allowGroupMentions If the message should allow the pinging of channels, users, or emotes.
+     * @param allowGroupMentions Whether or not the message should allow the pinging of roles, users, or emotes.
      * @param avatarUrl          The avatar URL to use for this message (if supported) or null to use the default bot avatar.
      * @param name               The name to use for this message (if supported) or null to use the default bot name.
      * @param uuid               The UUID of the player which caused this event or null if this wasn't a player triggered event.
