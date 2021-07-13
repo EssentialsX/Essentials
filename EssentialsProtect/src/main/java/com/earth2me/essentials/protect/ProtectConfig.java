11b84ea6add49306977829d8bc1688fd2f6cf13a
hicupalot <53972884+hicupalot@users.noreply.github.com>
2021-07-13 10:22:28 +0100

    Update ProtectConfig.java

--- EssentialsProtect/src/main/java/com/earth2me/essentials/protect/ProtectConfig.java
+++ EssentialsProtect/src/main/java/com/earth2me/essentials/protect/ProtectConfig.java
@@ -61,6 +61,8 @@ public enum ProtectConfig {
     prevent_pig_transformation("protect.prevent.transformation.zombie-pigman", false),
     prevent_zombie_drowning("protect.prevent.transformation.drowned", false),
     prevent_mooshroom_switching("protect.prevent.transformation.mooshroom", false);
+    lingering_potion_particles("protect.prevent.lingering.effects",false);
+
     private final String configName;
     private final String defValueString;
     private final boolean defValueBoolean;
