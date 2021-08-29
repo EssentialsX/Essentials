e556424a63b602c858a5b903e2b6b389e24e9318
MD <1917406+mdcfe@users.noreply.github.com>
2021-08-29 21:27:52 +0100

    shut

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -10,5 +10,5 @@ repositories {
 dependencies {
     implementation("net.kyori", "indra-common", "2.0.6")
     implementation("gradle.plugin.com.github.jengelman.gradle.plugins", "shadow", "7.0.0")
-    implementation("xyz.jpenilla", "run-paper", "1.0.2")
+    implementation("xyz.jpenilla", "run-paper", "1.0.4")
 }
