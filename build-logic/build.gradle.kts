aafaf94ab4c08d6d4b7ee61f67c1ec29abfe19c7
Josh Roy <10731363+JRoy@users.noreply.github.com>
2021-12-28 15:06:12 -0500

    Bump run-paper

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -9,5 +9,5 @@ repositories {
 dependencies {
     implementation("net.kyori", "indra-common", "2.0.6")
     implementation("gradle.plugin.com.github.jengelman.gradle.plugins", "shadow", "7.0.0")
-    implementation("xyz.jpenilla", "run-paper", "1.0.4")
+    implementation("xyz.jpenilla", "run-paper", "1.0.6")
 }
