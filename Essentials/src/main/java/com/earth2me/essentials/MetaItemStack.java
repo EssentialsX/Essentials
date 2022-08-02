1bcb8df15b9143dfe95378bb806718e9d5e31647
David Acevedo <19176326+SavageAvocado@users.noreply.github.com>
2022-08-02 19:02:49 -0400

    Removed stray "org.bukkit.entity.Player" import.

--- Essentials/src/main/java/com/earth2me/essentials/MetaItemStack.java
+++ Essentials/src/main/java/com/earth2me/essentials/MetaItemStack.java
@@ -18,7 +18,6 @@ import org.bukkit.Material;
 import org.bukkit.block.Banner;
 import org.bukkit.block.banner.PatternType;
 import org.bukkit.enchantments.Enchantment;
-import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BannerMeta;
