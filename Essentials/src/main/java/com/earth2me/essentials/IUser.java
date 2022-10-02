0e9276ae433dea30233ceb66f26fd61cf15618dd
Nassim Jahnke <nassim@njahnke.dev>
2022-10-02 10:52:49 +0200

    Add renamehome command

--- Essentials/src/main/java/com/earth2me/essentials/IUser.java
+++ Essentials/src/main/java/com/earth2me/essentials/IUser.java
@@ -155,6 +155,8 @@ public interface IUser {
 
     void delHome(String name) throws Exception;
 
+    void renameHome(String name, String newName) throws Exception;
+
     boolean hasHome();
 
     Location getLastLocation();
