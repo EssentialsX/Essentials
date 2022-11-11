6e931b2b3b620dc7fc98222ff18a7e76979bb43f
Josh Roy <10731363+JRoy@users.noreply.github.com>
2022-11-11 18:28:55 -0500

    Fix ConsoleInjector not getting reinjecting on reload (#5149)

--- EssentialsDiscord/src/main/java/net/essentialsx/discord/JDADiscordService.java
+++ EssentialsDiscord/src/main/java/net/essentialsx/discord/JDADiscordService.java
@@ -392,7 +392,7 @@ public class JDADiscordService implements DiscordService, IEssentialsModule {
 
         shutdownConsoleRelay(false);
         consoleWebhook = DiscordUtil.getWebhookClient(webhookId, webhookToken, jda.getHttpClient());
-        if (injector == null) {
+        if (injector == null || injector.isRemoved()) {
             injector = new ConsoleInjector(this);
             injector.start();
         }
