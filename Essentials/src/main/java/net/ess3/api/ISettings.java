51ce90ff3a8e5ce9e4300ae7fcbe0ad30dcb2306
Cole Riggle <coleriggle@gmail.com>
2022-12-11 18:24:07 -0500

    Introduce new money-caps-multiple into config and Settings

--- Essentials/src/main/java/net/ess3/api/ISettings.java
+++ Essentials/src/main/java/net/ess3/api/ISettings.java
@@ -1,7 +1,17 @@
 package net.ess3.api;
 
+import com.earth2me.essentials.User;
+
+import java.math.BigDecimal;
+import java.util.Set;
+
 /**
  *
  */
 public interface ISettings extends com.earth2me.essentials.ISettings {
+    Set<String> getMultipleMoneyCaps();
+
+    BigDecimal getMoneyCap(User user);
+
+    BigDecimal getMoneyCap(String set);
 }
