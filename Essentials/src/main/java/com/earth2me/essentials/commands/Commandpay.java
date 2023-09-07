46d5f3d2eddb4e5da88d2f14d1545b4a54bd8389
Daniil Z <idanix@list.ru>
2023-09-07 03:10:01 +0300

    Add more strict number format for the pay command

--- Essentials/src/main/java/com/earth2me/essentials/commands/Commandpay.java
+++ Essentials/src/main/java/com/earth2me/essentials/commands/Commandpay.java
@@ -14,10 +14,15 @@ import java.math.BigDecimal;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static com.earth2me.essentials.I18n.tl;
 
 public class Commandpay extends EssentialsLoopCommand {
+    private static final Pattern NUMBER_FORMAT = Pattern.compile("([0_]*[0-9][0-9_'`,]*(?:\\.[0-9]+)?)([kmbt])?");
+    private static final Pattern SANITIZE = Pattern.compile("[^0-9.]");
+
     private static final BigDecimal THOUSAND = new BigDecimal(1000);
     private static final BigDecimal MILLION = new BigDecimal(1_000_000);
     private static final BigDecimal BILLION = new BigDecimal(1_000_000_000);
@@ -33,38 +38,37 @@ public class Commandpay extends EssentialsLoopCommand {
             throw new NotEnoughArgumentsException();
         }
 
-        final String ogStr = args[1];
-
-        if (ogStr.contains("-")) {
+        if (args[1].startsWith("-")) {
             throw new Exception(tl("payMustBePositive"));
         }
 
-        final String sanitizedString = ogStr.replaceAll("[^0-9.]", "");
-
-        if (sanitizedString.isEmpty()) {
-            throw new NotEnoughArgumentsException();
+        final Matcher formatMatcher = NUMBER_FORMAT.matcher(args[1]);
+        if (!formatMatcher.matches()) {
+            throw new Exception(tl("numberRequired"));
         }
-
-        BigDecimal tempAmount = new BigDecimal(sanitizedString);
-        switch (Character.toLowerCase(ogStr.charAt(ogStr.length() - 1))) {
-            case 'k': {
-                tempAmount = tempAmount.multiply(THOUSAND);
-                break;
-            }
-            case 'm': {
-                tempAmount = tempAmount.multiply(MILLION);
-                break;
-            }
-            case 'b': {
-                tempAmount = tempAmount.multiply(BILLION);
-                break;
-            }
-            case 't': {
-                tempAmount = tempAmount.multiply(TRILLION);
-                break;
-            }
-            default: {
-                break;
+        BigDecimal tempAmount = new BigDecimal(SANITIZE.matcher(formatMatcher.group(1)).replaceAll(""));
+        final String modifier = formatMatcher.group(2);
+        if (modifier != null) {
+            switch (modifier) {
+                case "k": {
+                    tempAmount = tempAmount.multiply(THOUSAND);
+                    break;
+                }
+                case "m": {
+                    tempAmount = tempAmount.multiply(MILLION);
+                    break;
+                }
+                case "b": {
+                    tempAmount = tempAmount.multiply(BILLION);
+                    break;
+                }
+                case "t": {
+                    tempAmount = tempAmount.multiply(TRILLION);
+                    break;
+                }
+                default: {
+                    break;
+                }
             }
         }
 
@@ -93,7 +97,7 @@ public class Commandpay extends EssentialsLoopCommand {
                 if (user.isPromptingPayConfirm() && !amount.equals(user.getConfirmingPayments().get(player))) { // checks if exists and if command needs to be repeated.
                     // Used to reset confirmations and inform to confirm when a new pay command has been inserted.
                     if (!informToConfirm.get()) {
-                        // User hasnt been asked to confirm payment to this player, reset all confirmed payments and ask to confirm again.
+                        // User hasn't been asked to confirm payment to this player, reset all confirmed payments and ask to confirm again.
                         // Clear previous confirmations to ensure that a new confirmation message is brought up.
                         user.getConfirmingPayments().clear();
                         informToConfirm.set(true);
