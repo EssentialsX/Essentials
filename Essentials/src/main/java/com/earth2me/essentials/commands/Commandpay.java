27e2b5e54c42fb2678d15f10800078d4d3025ffc
Daniil Z <idanix@list.ru>
2023-09-13 01:01:25 +0300

    Use better amount format pattern

--- Essentials/src/main/java/com/earth2me/essentials/commands/Commandpay.java
+++ Essentials/src/main/java/com/earth2me/essentials/commands/Commandpay.java
@@ -20,7 +20,7 @@ import java.util.regex.Pattern;
 import static com.earth2me.essentials.I18n.tl;
 
 public class Commandpay extends EssentialsLoopCommand {
-    private static final Pattern NUMBER_FORMAT = Pattern.compile("([0-9][0-9_'`,]*(?:\\.[0-9]+)?|\\.[0-9]+)([kmbt])?");
+    private static final Pattern AMOUNT_FORMAT = Pattern.compile("((?:[0-9][_'`,]?)+(?:\\.[0-9]+)?|\\.[0-9]+)([kmbt])?");
     private static final Pattern SANITIZE = Pattern.compile("[^0-9.]");
 
     private static final BigDecimal THOUSAND = new BigDecimal(1000);
@@ -43,7 +43,7 @@ public class Commandpay extends EssentialsLoopCommand {
             throw new Exception(tl("payMustBePositive"));
         }
 
-        final Matcher formatMatcher = NUMBER_FORMAT.matcher(amountStr);
+        final Matcher formatMatcher = AMOUNT_FORMAT.matcher(amountStr);
         if (!formatMatcher.matches()) {
             throw new Exception(tl("numberRequired"));
         }
