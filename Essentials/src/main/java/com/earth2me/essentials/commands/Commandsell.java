b3db135f04cf4e4f1074acea32f5f338ba5fd948
Josh Roy <10731363+JRoy@users.noreply.github.com>
2023-09-17 18:17:05 -0400

    clean up sell a lil bit

--- Essentials/src/main/java/com/earth2me/essentials/commands/Commandsell.java
+++ Essentials/src/main/java/com/earth2me/essentials/commands/Commandsell.java
@@ -118,8 +118,10 @@ public class Commandsell extends EssentialsCommand {
         user.getBase().updateInventory();
         Trade.log("Command", "Sell", "Item", user.getName(), new Trade(ris, ess), user.getName(), new Trade(result, ess), user.getLocation(), user.getMoney(), ess);
         user.giveMoney(result, null, UserBalanceUpdateEvent.Cause.COMMAND_SELL);
-        user.sendTl("itemSold", NumberUtil.displayCurrency(result, ess), amount, is.getType().toString().toLowerCase(Locale.ENGLISH), NumberUtil.displayCurrency(worth, ess));
-        ess.getLogger().log(Level.INFO, tlLiteral("itemSoldConsole", user.getName(), is.getType().toString().toLowerCase(Locale.ENGLISH), NumberUtil.displayCurrency(result, ess), amount, NumberUtil.displayCurrency(worth, ess), user.getDisplayName()));
+        final String typeName = is.getType().toString().toLowerCase(Locale.ENGLISH);
+        final String worthDisplay = NumberUtil.displayCurrency(worth, ess);
+        user.sendTl("itemSold", NumberUtil.displayCurrency(result, ess), amount, typeName, worthDisplay);
+        ess.getLogger().log(Level.INFO, tlLiteral("itemSoldConsole", user.getName(), typeName, NumberUtil.displayCurrency(result, ess), amount, worthDisplay, user.getDisplayName()));
         return result;
     }
 
