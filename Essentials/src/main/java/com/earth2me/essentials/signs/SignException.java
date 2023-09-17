e47969e3efa0c03fda6a6bb0cb600edeee1a6bb0
Josh Roy <10731363+JRoy@users.noreply.github.com>
2023-09-17 18:11:05 -0400

    Clarify SignException tlKey parameter

--- Essentials/src/main/java/com/earth2me/essentials/signs/SignException.java
+++ Essentials/src/main/java/com/earth2me/essentials/signs/SignException.java
@@ -3,12 +3,12 @@ package com.earth2me.essentials.signs;
 import net.ess3.api.TranslatableException;
 
 public class SignException extends TranslatableException {
-    public SignException(final String message, final Object... args) {
-        super(message, args);
+    public SignException(final String tlKey, final Object... args) {
+        super(tlKey, args);
     }
 
-    public SignException(final Throwable cause, final String message, final Object... args) {
-        super(message, args);
+    public SignException(final Throwable cause, final String tlKey, final Object... args) {
+        super(tlKey, args);
         setCause(cause);
     }
 }
