38bd7e96851f86ea100eb25a365745946f408580
Josh Roy <10731363+JRoy@users.noreply.github.com>
2023-09-22 17:17:58 -0400

    lol

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -7,8 +7,7 @@ repositories {
 }
 
 dependencies {
-    implementation("net.kyori", "indra-common", "3.1.1")
+    implementation("net.kyori", "indra-common", "3.1.3")
     implementation("com.github.johnrengelman", "shadow", "8.1.1")
     implementation("xyz.jpenilla", "run-task", "2.1.0")
-    implementation("org.gradlex:java-ecosystem-capabilities:1.3.1")
 }
