835d522c90c4dd8322eed98de36a57892a9abd59
Evidentsinger14 <cdelancey186423@gmail.com>
2024-04-28 19:23:57 -0400

    fixes the fix for 5768

--- Essentials/src/main/java/com/earth2me/essentials/commands/Commandhelpop.java
+++ Essentials/src/main/java/com/earth2me/essentials/commands/Commandhelpop.java
@@ -51,7 +51,10 @@ public class Commandhelpop extends EssentialsCommand {
         final HelpopMessageSendEvent sendEvent = new HelpopMessageSendEvent(from, recipients, message);
         ess.getServer().getPluginManager().callEvent(sendEvent);
 
-        from.sendTl("helpOp", from.getDisplayName(), message);
+        final IUser sender = (IUser) from;
+        if(!recipients.contains(sender)){
+            from.sendTl("helpOp", from.getDisplayName(), message);
+        }
 
         for (IUser recipient : sendEvent.getRecipients()) {
             recipient.sendTl("helpOp", from.getDisplayName(), message);
