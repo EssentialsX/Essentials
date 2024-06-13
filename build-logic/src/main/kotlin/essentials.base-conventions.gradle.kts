2f684f70b5367c707d4528ccf68e2db288c9667c
JRoy <10731363+JRoy@users.noreply.github.com>
2024-06-13 18:06:24 -0400

    Update to Minecraft 1.21

--- build-logic/src/main/kotlin/essentials.base-conventions.gradle.kts
+++ build-logic/src/main/kotlin/essentials.base-conventions.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 val baseExtension = extensions.create<EssentialsBaseExtension>("essentials", project)
 
 val checkstyleVersion = "8.36.2"
-val spigotVersion = "1.20.6-R0.1-SNAPSHOT"
+val spigotVersion = "1.21-R0.1-SNAPSHOT"
 val junit5Version = "5.10.2"
 val mockitoVersion = "3.12.4"
 
