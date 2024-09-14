3e29e04722a39307a40c25e48798a1632ad3cb54
JRoy <10731363+JRoy@users.noreply.github.com>
2024-09-14 15:08:37 -0400

    Fix kittycannon on old versions

--- Essentials/src/main/java/com/earth2me/essentials/commands/Commandkittycannon.java
+++ Essentials/src/main/java/com/earth2me/essentials/commands/Commandkittycannon.java
@@ -2,6 +2,7 @@ package com.earth2me.essentials.commands;
 
 import com.earth2me.essentials.Mob;
 import com.earth2me.essentials.User;
+import com.earth2me.essentials.utils.RegistryUtil;
 import org.bukkit.Location;
 import org.bukkit.Server;
 import org.bukkit.entity.Cat;
@@ -21,8 +22,12 @@ public class Commandkittycannon extends EssentialsCommand {
 
     private static Ocelot spawnOcelot(final Server server, final User user) throws Mob.MobException {
         final Ocelot ocelot = (Ocelot) Mob.OCELOT.spawn(user.getWorld(), server, user.getBase().getEyeLocation());
-        final int i = random.nextInt(Ocelot.Type.values().length);
-        ocelot.setCatType(Ocelot.Type.values()[i]);
+        //noinspection deprecation
+        final Object[] values = RegistryUtil.values(Ocelot.Type.class);
+
+        final int i = random.nextInt(values.length);
+        //noinspection deprecation
+        ocelot.setCatType((Ocelot.Type) values[i]);
         ((Tameable) ocelot).setTamed(true);
         ocelot.setBaby();
         ocelot.setVelocity(user.getBase().getEyeLocation().getDirection().multiply(2));
@@ -31,8 +36,10 @@ public class Commandkittycannon extends EssentialsCommand {
 
     private static Entity spawnCat(final Server server, final User user) throws Mob.MobException {
         final Cat cat = (Cat) Mob.CAT.spawn(user.getWorld(), server, user.getBase().getEyeLocation());
-        final int i = random.nextInt(Cat.Type.values().length);
-        cat.setCatType(Cat.Type.values()[i]);
+        final Object[] values = RegistryUtil.values(Cat.Type.class);
+
+        final int i = random.nextInt(values.length);
+        cat.setCatType((Cat.Type) values[i]);
         cat.setTamed(true);
         cat.setBaby();
         cat.setVelocity(user.getBase().getEyeLocation().getDirection().multiply(2));
