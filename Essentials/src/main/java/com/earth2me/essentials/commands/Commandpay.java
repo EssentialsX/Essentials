73426f42d3bce26716d76c5a571b4855d951de68
JRoy <10731363+JRoy@users.noreply.github.com>
2024-12-30 08:48:07 -0500

    fix old provider method call

--- Essentials/src/main/java/com/earth2me/essentials/commands/Commandpay.java
+++ Essentials/src/main/java/com/earth2me/essentials/commands/Commandpay.java
@@ -43,7 +43,8 @@ public class Commandpay extends EssentialsLoopCommand {
 
         final BigDecimal amount;
         if (ess.getSettings().isPerPlayerLocale()) {
-            amount = NumberUtil.parseStringToBDecimal(ogStr, user.getPlayerLocale(ess.getProviders().get(PlayerLocaleProvider.class).getLocale(user.getBase())));
+            final String playerLocale = ess.provider(PlayerLocaleProvider.class).getLocale(user.getBase());
+            amount = NumberUtil.parseStringToBDecimal(ogStr, user.getPlayerLocale(playerLocale));
         } else {
             amount = NumberUtil.parseStringToBDecimal(ogStr);
         }
