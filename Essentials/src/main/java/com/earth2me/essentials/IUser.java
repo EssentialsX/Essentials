6e6b1a00bda7d487d1fb8cdaa712cf46a323ddff
JRoy <10731363+JRoy@users.noreply.github.com>
2025-01-24 18:23:42 -0500

    Remove deprecated teleport APIs

--- Essentials/src/main/java/com/earth2me/essentials/IUser.java
+++ Essentials/src/main/java/com/earth2me/essentials/IUser.java
@@ -3,7 +3,6 @@ package com.earth2me.essentials;
 import com.earth2me.essentials.api.IAsyncTeleport;
 import com.earth2me.essentials.commands.IEssentialsCommand;
 import com.earth2me.essentials.config.entities.CommandCooldown;
-import net.ess3.api.ITeleport;
 import net.ess3.api.MaxMoneyException;
 import net.ess3.api.events.AfkStatusChangeEvent;
 import net.essentialsx.api.v2.services.mail.MailMessage;
@@ -70,13 +69,9 @@ public interface IUser {
      * @return whether there is a teleport request
      */
     @Deprecated
-    boolean hasOutstandingTeleportRequest();
-
-    /**
-     * @deprecated This API is not asynchronous. Use {@link com.earth2me.essentials.api.IAsyncTeleport IAsyncTeleport} with {@link IUser#getAsyncTeleport()}
-     */
-    @Deprecated
-    ITeleport getTeleport();
+    default boolean hasOutstandingTeleportRequest() {
+        return getNextTpaRequest(false, false, false) != null;
+    }
 
     IAsyncTeleport getAsyncTeleport();
 
