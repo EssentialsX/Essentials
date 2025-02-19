e2b3e8bbbd2b776b3cf32e3183bdcf84b58733e2
Josh Roy <10731363+JRoy@users.noreply.github.com>
2025-02-19 15:49:15 -0500

    Fix startup issue with potion providers
    The LegacyPotionMetaProvider was initializing a map with enums that don't exist on modern versions. Since this was a static variable, this map got initialized when the test method was called, leading to a NoSuchFieldError.
    
    Also improved resilience of these provider-type issues again by catching a Throwable rather than an Exception in the ProviderFactory.

--- providers/1_12Provider/src/main/java/net/ess3/provider/providers/LegacyPotionMetaProvider.java
+++ providers/1_12Provider/src/main/java/net/ess3/provider/providers/LegacyPotionMetaProvider.java
@@ -16,7 +16,7 @@ import java.util.Map;
 
 @ProviderData(description = "1.9-1.20.4 Potion Meta Provider", weight = 1)
 public class LegacyPotionMetaProvider implements PotionMetaProvider {
-    private static final Map<Integer, PotionType> damageValueToType = ImmutableMap.<Integer, PotionType>builder()
+    private final Map<Integer, PotionType> damageValueToType = ImmutableMap.<Integer, PotionType>builder()
         .put(1, PotionType.REGEN)
         .put(2, PotionType.SPEED)
         .put(3, PotionType.FIRE_RESISTANCE)
