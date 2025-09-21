9e13fb7ef3a2ffee3f5343cc5cb329c411998ade
maxcom1 <46265094+maxcom1@users.noreply.github.com>
2025-09-21 16:53:30 +0200

    Add option to limit baltop entries

--- Essentials/src/main/java/com/earth2me/essentials/BalanceTopImpl.java
+++ Essentials/src/main/java/com/earth2me/essentials/BalanceTopImpl.java
@@ -53,7 +53,10 @@ public class BalanceTopImpl implements BalanceTop {
         }
         final LinkedHashMap<UUID, Entry> sortedMap = new LinkedHashMap<>();
         entries.sort((entry1, entry2) -> entry2.getBalance().compareTo(entry1.getBalance()));
+        int entryLimit = ess.getSettings().getBaltopEntryLimit();
+        int i = 0;
         for (Entry entry : entries) {
+            if (entryLimit != -1 && ++i > entryLimit) break;
             sortedMap.put(entry.getUuid(), entry);
         }
         topCache = sortedMap;
