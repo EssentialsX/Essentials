0b58c16bbed19ed897bd80e5cbff8776218ae122
Stackulate <180084054+Stackulate@users.noreply.github.com>
2025-12-08 11:28:14 +0100

    Add v1_21_R6 MDFIVEMAGICLETTER

--- providers/NMSReflectionProvider/src/main/java/net/ess3/nms/refl/ReflUtil.java
+++ providers/NMSReflectionProvider/src/main/java/net/ess3/nms/refl/ReflUtil.java
@@ -23,6 +23,7 @@ public final class ReflUtil {
     public static final NMSVersion V1_19_R1 = NMSVersion.fromString("v1_19_R1");
     public static final NMSVersion V1_19_R2 = NMSVersion.fromString("v1_19_R2");
     public static final NMSVersion V1_20_R4 = NMSVersion.fromString("v1_20_R4");
+    public static final NMSVersion V1_21_R6 = NMSVersion.fromString("v1_21_R6");
     private static final Map<String, Class<?>> classCache = new HashMap<>();
     private static final Table<Class<?>, String, Method> methodCache = HashBasedTable.create();
     private static final Table<Class<?>, MethodParams, Method> methodParamCache = HashBasedTable.create();
