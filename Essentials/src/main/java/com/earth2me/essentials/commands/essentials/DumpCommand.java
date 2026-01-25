a1f827bdbffb1f8550aa8322028b26a6a3590d20
JRoy <10731363+JRoy@users.noreply.github.com>
2026-01-24 19:10:43 -0800

    update dump url

--- Essentials/src/main/java/com/earth2me/essentials/commands/essentials/DumpCommand.java
+++ Essentials/src/main/java/com/earth2me/essentials/commands/essentials/DumpCommand.java
@@ -255,7 +255,7 @@ public class DumpCommand extends EssentialsTreeNode {
             final CompletableFuture<PasteUtil.PasteResult> future = PasteUtil.createPaste(files);
             future.thenAccept(result -> {
                 if (result != null) {
-                    final String dumpUrl = "https://essentialsx.net/dump.html?bytebin=" + result.getPasteId();
+                    final String dumpUrl = "https://essentialsx.net/dump?bytebin=" + result.getPasteId();
                     sender.sendTl("dumpUrl", dumpUrl);
                     // pastes.dev doesn't support deletion keys
                     //sender.sendTl("dumpDeleteKey", result.getDeletionKey());
