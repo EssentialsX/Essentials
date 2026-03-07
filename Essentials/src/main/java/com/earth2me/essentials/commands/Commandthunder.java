555951f10742c44168dc7f331d19734cea5bb108
Théo Dufrene <43671109+itsme-to@users.noreply.github.com>
2026-03-07 14:30:40 +0100

    Add MiniMessage support for thunder enabled / disabled

--- Essentials/src/main/java/com/earth2me/essentials/commands/Commandthunder.java
+++ Essentials/src/main/java/com/earth2me/essentials/commands/Commandthunder.java
@@ -1,6 +1,7 @@
 package com.earth2me.essentials.commands;
 
 import com.earth2me.essentials.User;
+import com.earth2me.essentials.adventure.AdventureUtil;
 import com.google.common.collect.Lists;
 import org.bukkit.Server;
 import org.bukkit.World;
@@ -21,15 +22,17 @@ public class Commandthunder extends EssentialsCommand {
 
         final World world = user.getWorld();
         final boolean setThunder = args[0].equalsIgnoreCase("true");
+        final String status = setThunder ? user.playerTl("enabled") : user.playerTl("disabled");
+
         if (args.length == 1) {
             world.setThundering(setThunder);
-            user.sendTl("thunder", setThunder ? user.playerTl("enabled") : user.playerTl("disabled"));
+            user.sendTl("thunder", AdventureUtil.parsed(status));
             return;
         }
 
         world.setThundering(setThunder);
         world.setThunderDuration(Integer.parseInt(args[1]) * 20);
-        user.sendTl("thunderDuration", setThunder ? user.playerTl("enabled") : user.playerTl("disabled"), Integer.parseInt(args[1]));
+        user.sendTl("thunderDuration", AdventureUtil.parsed(status), Integer.parseInt(args[1]));
     }
 
     @Override
