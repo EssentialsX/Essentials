1d75835c19f241f6d381c7ea590264f8bf003162
MD <1917406+mdcfe@users.noreply.github.com>
2026-03-08 21:37:09 +0000

    Migrate Vault and emoji-java dependencies away from Jitpack

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,10 +13,9 @@ dependencyResolutionManagement {
                 includeGroup("net.md_5")
             }
         }
-        maven("https://jitpack.io") {
+        maven("https://repo.essentialsx.net/releases") {
             content {
-                includeGroup("com.github.milkbowl")
-                includeGroup("com.github.MinnDevelopment")
+                includeGroup("net.essentialsx.deps")
             }
         }
         maven("https://repo.codemc.org/repository/maven-public") {
