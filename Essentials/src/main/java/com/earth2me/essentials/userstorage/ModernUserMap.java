aded02c024a2c0aac0cad3e9d9092b577864c239
JRoy <10731363+JRoy@users.noreply.github.com>
2026-08-03 21:42:22 -0700

    populate cache

--- Essentials/src/main/java/com/earth2me/essentials/userstorage/ModernUserMap.java
+++ Essentials/src/main/java/com/earth2me/essentials/userstorage/ModernUserMap.java
@@ -102,8 +102,10 @@ public class ModernUserMap extends CacheLoader<UUID, User> implements IUserMap {
 
     @Override
     public User getUser(final Player base) {
-        final User cachedUser = userCache.getIfPresent(base.getUniqueId());
+        final UUID uuid = base.getUniqueId();
+        final User cachedUser = userCache.getIfPresent(uuid);
         if (cachedUser != null && cachedUser.getBase() == base) {
+            uuidCache.updateCache(uuid, base.getName());
             return cachedUser;
         }
 
